e7b61710c232045c4a0f9741999f9a105b836e0a
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-05-18 22:36:54 +0000

    *Small change to the way registration keys.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@314 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Data Interface/RegistrationKeys.cs
+++ OpenDental/Data Interface/RegistrationKeys.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography;
 using CodeBase;
+using System.IO;
+using CDT;
 
 namespace OpenDental {
 	///<summary>Used to keep track of which product keys have been assigned to which customers. This class is only used if the program is being run from a distributor installation.</summary>
@@ -34,53 +36,9 @@ namespace OpenDental {
 
 		///<summary>Inserts a new and unique registration key into the database.</summary>
 		public static void Create(RegistrationKey registrationKey){
-			Match m;
 			do{
-				registrationKey.RegKey="ODKEY";
-				Random rand=new Random();
-				for(int i=0;i<16-registrationKey.RegKey.Length;i++){
-					registrationKey.RegKey+=(char)rand.Next(0,255);
-				}
-				//TODO: update with encryption.
-
-				//Key must be generated and stored in db!
-
-
-				DSACryptoServiceProvider dsa=new DSACryptoServiceProvider();
-				dsa.FromXmlString(PrefB.GetString("DistributorKey"));
-				byte[] signedHash=dsa.SignData(Encoding.UTF8.GetBytes(registrationKey.RegKey));
-				MsgBoxCopyPaste msg=new MsgBoxCopyPaste(signedHash.ToString());
-				msg.ShowDialog();
-
-				
-
-				/*
-				 * static String BytesToHexString(byte[] bytes) 
-        {
-            StringBuilder hexString = new StringBuilder(64);
-
-            for (int counter = 0; counter < bytes.Length; counter++) 
-            {
-                hexString.Append(String.Format("{0:X2}", bytes[counter]));
-            }
-            return hexString.ToString();
-        }
-				 */
-				
-
-
-
-
-
-
-
-
-				//Convert the stored key to base64 to both make the key user-readable and to 
-				//ensure that there are no semi-colons in it (otherwise, the semi-colons are removed 
-				//when connecting to an Oracle database, in which case the stored key would be invalid).
-				registrationKey.RegKey=Convert.ToBase64String(Encoding.UTF8.GetBytes(registrationKey.RegKey));
-				m=Regex.Match(registrationKey.RegKey,"^([ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789]*)$");
-			}while(!m.Success || m.Result("$1")!=registrationKey.RegKey || KeyIsInUse(registrationKey.RegKey));
+				registrationKey.RegKey=CDT.Class1.GenerateRandKey();
+			} while(KeyIsInUse(registrationKey.RegKey));
 			string command="INSERT INTO registrationkey (PatNum,RegKey,Note) VALUES ("+
 				"'"+POut.PInt(registrationKey.PatNum)+"',"+
 				"'"+POut.PString(registrationKey.RegKey)+"',"+
