c3322e28a93be07c74bb2cb2b2fb2595d7cad3c7
fcarlier <fcarlier@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-07-10 12:11:23 +0000

    - Don't call dto.GetType() in every "if/else" branch, but rather cache the result;
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@524 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentServer/BusinessLayer.cs
+++ OpenDentServer/BusinessLayer.cs
@@ -9,38 +9,38 @@ namespace OpenDentServer {
 
 		///<summary>DataSet cannot be null.</summary>
 		public static DataSet ProcessQuery(DtoQueryBase dto) {
-			if(dto.GetType()==typeof(DtoGeneralGetTable)) {
+			Type type = dto.GetType();
+
+			if(type==typeof(DtoGeneralGetTable)) {
 				return GeneralB.GetTable(((DtoGeneralGetTable)dto).Command);
 			}
-			else if(dto.GetType()==typeof(DtoGeneralGetTableLow)) {
+			else if(type==typeof(DtoGeneralGetTableLow)) {
 				return GeneralB.GetTableLow(((DtoGeneralGetTableLow)dto).Command);
 			}
-			else if(dto.GetType()==typeof(DtoGeneralGetDataSet)) {
+			else if(type==typeof(DtoGeneralGetDataSet)) {
 				return GeneralB.GetDataSet(((DtoGeneralGetDataSet)dto).Commands);
 			}
-			else if(dto.GetType()==typeof(DtoAccountModuleGetAll)) {
+			else if(type==typeof(DtoAccountModuleGetAll)) {
 				return AccountModuleB.GetAll(((DtoAccountModuleGetAll)dto).PatNum);
 			}
-			else if(dto.GetType()==typeof(DtoChartModuleGetAll)) {
+			else if(type==typeof(DtoChartModuleGetAll)) {
 				return ChartModuleB.GetAll(((DtoChartModuleGetAll)dto).PatNum,((DtoChartModuleGetAll)dto).IsAuditMode);
 			}
-			else if(dto.GetType()==typeof(DtoCovCatRefresh)) {
+			else if(type==typeof(DtoCovCatRefresh)) {
 				return CovCatB.Refresh();
 			}
-			else if(dto.GetType()==typeof(DtoDefRefresh)) {
+			else if(type==typeof(DtoDefRefresh)) {
 				return DefB.Refresh();
 			}
-			else if(dto.GetType()==typeof(DtoPrefRefresh)) {
+			else if(type==typeof(DtoPrefRefresh)) {
 				return PrefB.Refresh();
 			}
-			else if(dto.GetType()==typeof(DtoProcedureRefresh)) {
+			else if(type==typeof(DtoProcedureRefresh)) {
 				return ProcedureB.Refresh(((DtoProcedureRefresh)dto).PatNum);
 			}
-			else if(dto.GetType()==typeof(DtoUserodRefresh)) {
+			else if(type==typeof(DtoUserodRefresh)) {
 				return UserodB.Refresh();
 			}
-
-
 			else{
 				throw new Exception("OpenDentServer.BusinessLayer.ProcessObject(dto) is missing a case for "
 					+dto.GetType().ToString());
@@ -48,76 +48,59 @@ namespace OpenDentServer {
 		}
 
 		public static int ProcessCommand(DtoCommandBase dto) {
-			if(dto.GetType()==typeof(DtoDefInsert)) {
+			Type type = dto.GetType();
+
+			if(type==typeof(DtoDefInsert)) {
 				return DefB.Insert(((DtoDefInsert)dto).DefCur);
 			}
-			else if(dto.GetType()==typeof(DtoDefUpdate)) {
+			else if(type==typeof(DtoDefUpdate)) {
 				return DefB.Update(((DtoDefUpdate)dto).DefCur);
 			}
-			else if(dto.GetType()==typeof(DtoDocumentInsert)) {
+			else if(type==typeof(DtoDocumentInsert)) {
 				return DocumentB.Insert(((DtoDocumentInsert)dto).Doc,((DtoDocumentInsert)dto).PatLF,((DtoDocumentInsert)dto).PatNum);
 			}
-			else if(dto.GetType()==typeof(DtoDocumentUpdate)) {
+			else if(type==typeof(DtoDocumentUpdate)) {
 				return DocumentB.Update(((DtoDocumentUpdate)dto).Doc);
 			}
-			else if(dto.GetType()==typeof(DtoEmailMessageUpdate)) {
+			else if(type==typeof(DtoEmailMessageUpdate)) {
 				return EmailMessageB.Update(((DtoEmailMessageUpdate)dto).Message);
 			}
-			else if(dto.GetType()==typeof(DtoGeneralNonQ)) {
+			else if(type==typeof(DtoGeneralNonQ)) {
 				return GeneralB.NonQ(((DtoGeneralNonQ)dto).Command,((DtoGeneralNonQ)dto).GetInsertID);
 			}
-			else if(dto.GetType()==typeof(DtoLogin)) {
+			else if(type==typeof(DtoLogin)) {
 				return OpenDentalService.Login(((DtoLogin)dto).Database,((DtoLogin)dto).OdUser,((DtoLogin)dto).OdPassHash);
 			}
-			else if(dto.GetType()==typeof(DtoMiscDataMakeABackup)) {
+			else if(type==typeof(DtoMiscDataMakeABackup)) {
 				return MiscDataB.MakeABackup();
 			}
-			else if(dto.GetType()==typeof(DtoProcedureInsert)) {
+			else if(type==typeof(DtoProcedureInsert)) {
 				return ProcedureB.Insert(((DtoProcedureInsert)dto).Proc);
 			}
-			else if(dto.GetType()==typeof(DtoProcedureUpdate)) {
+			else if(type==typeof(DtoProcedureUpdate)) {
 				return ProcedureB.Update(((DtoProcedureUpdate)dto).Proc,((DtoProcedureUpdate)dto).OldProc);
 			}
-			else if(dto.GetType()==typeof(DtoProcedureDelete)) {
+			else if(type==typeof(DtoProcedureDelete)) {
 				return ProcedureB.Delete(((DtoProcedureDelete)dto).ProcNum);
 			}
-			else if(dto.GetType()==typeof(DtoProcedureUpdateAptNum)) {
+			else if(type==typeof(DtoProcedureUpdateAptNum)) {
 				return ProcedureB.UpdateAptNum(((DtoProcedureUpdateAptNum)dto).ProcNum,((DtoProcedureUpdateAptNum)dto).NewAptNum);
 			}
-			else if(dto.GetType()==typeof(DtoProcedureUpdatePlannedAptNum)) {
+			else if(type==typeof(DtoProcedureUpdatePlannedAptNum)) {
 				return ProcedureB.UpdatePlannedAptNum(((DtoProcedureUpdatePlannedAptNum)dto).ProcNum,
 					((DtoProcedureUpdatePlannedAptNum)dto).NewPlannedAptNum);
 			}
-			else if(dto.GetType()==typeof(DtoProcedureUpdatePriority)) {
+			else if(type==typeof(DtoProcedureUpdatePriority)) {
 				return ProcedureB.UpdatePriority(((DtoProcedureUpdatePriority)dto).ProcNum,
 					((DtoProcedureUpdatePriority)dto).NewPriority);
 			}
-			else if(dto.GetType()==typeof(DtoProcedureUpdateFee)) {
+			else if(type==typeof(DtoProcedureUpdateFee)) {
 				return ProcedureB.UpdateFee(((DtoProcedureUpdateFee)dto).ProcNum,((DtoProcedureUpdateFee)dto).NewFee);
 			}
-			
-
-
-
 			else{
 				throw new Exception("OpenDentServer.BusinessLayer.ProcessCommand(dto) is missing a case for "
 					+dto.GetType().ToString());
 			}
 		}
-
-
-			
-			
-
-
-			//switch(dto.DTO_type){
-			//	case "ProcedureInsert":
-
-
-			//}
-
-			//return null;
-		//}
-
 	}
 }
