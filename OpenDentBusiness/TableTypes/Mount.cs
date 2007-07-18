0c52d63f058efa6f8d870012075a68cb0988789f
fcarlier <fcarlier@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-07-18 16:20:17 +0000

    - The TableTypes corresponding to the Imaging module now inherit from DataObjectBase and expose properties instead of fields. They now have "XXXChanged" events. The data access logic itself has not yet been modified. This change has been made using first the "Convert" and then the "Convert2" macros available in the fcarlier branch. - The next step would be to use the Data Access framework for *loading* these objects. That code is relatively straight forward and should work correctly on both MySQL and Oracle. - I'll be making these changes slowly (holidays!) which should have a positive effect on testing: every, small change has some time to be tested by other developers. In case of any bugs, please post on the od forum (developers section).
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@562 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/TableTypes/Mount.cs
+++ OpenDentBusiness/TableTypes/Mount.cs
@@ -1,28 +1,156 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenDental.DataAccess;
 
 namespace OpenDentBusiness {
 	/// <summary>These can be freely deleted, renamed, moved, etc. without affecting any patient info.  mountitem</summary>
-	public class Mount{
+	[DataObject("mount")]
+	public class Mount : DataObjectBase {
 		///<summary>Primary key.</summary>
-		public int MountNum;
+		[DataField("MountNum")]
+		private int mountNum;
+
+		/// <summary>Primary key.</summary>
+		public int MountNum {
+			get { return mountNum; }
+			set { mountNum = value; MarkDirty(); mountNumChanged = true; }
+		}
+
+		bool mountNumChanged;
+
+		public bool MountNumChanged {
+			get { return mountNumChanged; }
+			
+		}
 		///<summary>FK to patient.PatNum</summary>
-		public int PatNum;
+		[DataField("PatNum")]
+		private int patNum;
+
+		/// <summary>FK to patient.PatNum</summary>
+		public int PatNum {
+			get { return patNum; }
+			set { patNum = value; MarkDirty(); patNumChanged = true; }
+		}
+
+		bool patNumChanged;
+
+		public bool PatNumChanged {
+			get { return patNumChanged; }
+			
+		}
 		///<summary>The category enumeration/folder that this mount belongs to in the image module tree-view.</summary>
-		public int DocCategory;
+		[DataField("DocCategory")]
+		private int docCategory;
+
+		/// <summary>The category enumeration/folder that this mount belongs to in the image module tree-view.</summary>
+		public int DocCategory {
+			get { return docCategory; }
+			set { docCategory = value; MarkDirty(); docCategoryChanged = true; }
+		}
+
+		bool docCategoryChanged;
+
+		public bool DocCategoryChanged {
+			get { return docCategoryChanged; }
+			
+		}
 		///<summary>The date at which the mount itself was created. Has no bearing on the creation date of the images the mount houses.</summary>
-		public DateTime DateCreated;
+		[DataField("DateCreated")]
+		private DateTime dateCreated;
+
+		/// <summary>The date at which the mount itself was created. Has no bearing on the creation date of the images the mount houses.</summary>
+		public DateTime DateCreated {
+			get { return dateCreated; }
+			set { dateCreated = value; MarkDirty(); dateCreatedChanged = true; }
+		}
+
+		bool dateCreatedChanged;
+
+		public bool DateCreatedChanged {
+			get { return dateCreatedChanged; }
+			
+		}
 		///<summary>Used to provide a document description in the image module tree-view.</summary>
-		public string Description;
+		[DataField("Description")]
+		private string description;
+
+		/// <summary>Used to provide a document description in the image module tree-view.</summary>
+		public string Description {
+			get { return description; }
+			set { description = value; MarkDirty(); descriptionChanged = true; }
+		}
+
+		bool descriptionChanged;
+
+		public bool DescriptionChanged {
+			get { return descriptionChanged; }
+			
+		}
 		///<summary>To allow the user to enter specific information regarding the exam and tooth numbers, as well as points on interest in the xray images.</summary>
-		public string Note;
+		[DataField("Note")]
+		private string note;
+
+		/// <summary>To allow the user to enter specific information regarding the exam and tooth numbers, as well as points on interest in the xray images.</summary>
+		public string Note {
+			get { return note; }
+			set { note = value; MarkDirty(); noteChanged = true; }
+		}
+
+		bool noteChanged;
+
+		public bool NoteChanged {
+			get { return noteChanged; }
+			
+		}
 		///<summary>Enum:ImageType This is so that an image can be properly associated with the mount in the image module tree-view.</summary>
-		public ImageType ImgType;
+		[DataField("ImgType")]
+		private ImageType imgType;
+
+		/// <summary>Enum:ImageType This is so that an image can be properly associated with the mount in the image module tree-view.</summary>
+		public ImageType ImgType {
+			get { return imgType; }
+			set { imgType = value; MarkDirty(); imgTypeChanged = true; }
+		}
+
+		bool imgTypeChanged;
+
+		public bool ImgTypeChanged {
+			get { return imgTypeChanged; }
+			
+		}
 		///<summary>The static width of the mount, in pixels.</summary>
-		public int Width;
+		[DataField("Width")]
+		private int width;
+
+		/// <summary>The static width of the mount, in pixels.</summary>
+		public int Width {
+			get { return width; }
+			set { width = value; MarkDirty(); widthChanged = true; }
+		}
+
+		bool widthChanged;
+
+		public bool WidthChanged {
+			get { return widthChanged; }
+			
+		}
 		///<summary>The static height of the mount, in pixels.</summary>
-		public int Height;
+		[DataField("Height")]
+		private int height;
+
+		/// <summary>The static height of the mount, in pixels.</summary>
+		public int Height {
+			get { return height; }
+			set { height = value; MarkDirty(); heightChanged = true; }
+		}
+
+		bool heightChanged;
+
+		public bool HeightChanged {
+			get { return heightChanged; }
+			
+		}
 
 		///<summary></summary>
 		public Mount Copy() {
