31e5c50b9784429d943eb022ab16d5b165cc4a4e
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-07-21 20:20:18 +0000

    I had to remove the new unit test project from the solution due to a missing reference to NUnit. Also a bug fix: Blockout Cut-Copy-Paste, the Clear Day button was not functional.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@565 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormBlockoutCutCopyPaste.cs
+++ OpenDental/Forms/FormBlockoutCutCopyPaste.cs
@@ -32,7 +32,7 @@ namespace OpenDental{
 		private static DateTime DateCopyEnd=DateTime.MinValue;
 		public int ApptViewNumCur;
 		private static int ApptViewNumPrevious;
-		private OpenDental.UI.Button button1;
+		private OpenDental.UI.Button butClearDay;
 		public DateTime DateSelected;
 
 		///<summary></summary>
@@ -71,17 +71,17 @@ namespace OpenDental{
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.textClipboard = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
+			this.butCopyWeek = new OpenDental.UI.Button();
+			this.butCopyDay = new OpenDental.UI.Button();
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.butRepeat = new OpenDental.UI.Button();
 			this.label4 = new System.Windows.Forms.Label();
 			this.checkReplace = new System.Windows.Forms.CheckBox();
 			this.textRepeat = new System.Windows.Forms.TextBox();
+			this.butPaste = new OpenDental.UI.Button();
 			this.checkWeekend = new System.Windows.Forms.CheckBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new OpenDental.UI.Button();
-			this.butCopyWeek = new OpenDental.UI.Button();
-			this.butCopyDay = new OpenDental.UI.Button();
-			this.butRepeat = new OpenDental.UI.Button();
-			this.butPaste = new OpenDental.UI.Button();
+			this.butClearDay = new OpenDental.UI.Button();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
@@ -116,6 +116,34 @@ namespace OpenDental{
 			this.label3.Text = "Clipboard Contents";
 			this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
 			// 
+			// butCopyWeek
+			// 
+			this.butCopyWeek.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCopyWeek.Autosize = true;
+			this.butCopyWeek.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCopyWeek.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCopyWeek.CornerRadius = 4F;
+			this.butCopyWeek.Location = new System.Drawing.Point(6,85);
+			this.butCopyWeek.Name = "butCopyWeek";
+			this.butCopyWeek.Size = new System.Drawing.Size(75,24);
+			this.butCopyWeek.TabIndex = 28;
+			this.butCopyWeek.Text = "Copy Week";
+			this.butCopyWeek.Click += new System.EventHandler(this.butCopyWeek_Click);
+			// 
+			// butCopyDay
+			// 
+			this.butCopyDay.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCopyDay.Autosize = true;
+			this.butCopyDay.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCopyDay.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCopyDay.CornerRadius = 4F;
+			this.butCopyDay.Location = new System.Drawing.Point(6,58);
+			this.butCopyDay.Name = "butCopyDay";
+			this.butCopyDay.Size = new System.Drawing.Size(75,24);
+			this.butCopyDay.TabIndex = 27;
+			this.butCopyDay.Text = "Copy Day";
+			this.butCopyDay.Click += new System.EventHandler(this.butCopyDay_Click);
+			// 
 			// groupBox2
 			// 
 			this.groupBox2.Controls.Add(this.butRepeat);
@@ -129,6 +157,20 @@ namespace OpenDental{
 			this.groupBox2.TabIndex = 45;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Paste";
+			// 
+			// butRepeat
+			// 
+			this.butRepeat.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butRepeat.Autosize = true;
+			this.butRepeat.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butRepeat.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butRepeat.CornerRadius = 4F;
+			this.butRepeat.Location = new System.Drawing.Point(6,64);
+			this.butRepeat.Name = "butRepeat";
+			this.butRepeat.Size = new System.Drawing.Size(75,24);
+			this.butRepeat.TabIndex = 30;
+			this.butRepeat.Text = "Repeat";
+			this.butRepeat.Click += new System.EventHandler(this.butRepeat_Click);
 			// 
 			// label4
 			// 
@@ -158,6 +200,20 @@ namespace OpenDental{
 			this.textRepeat.TabIndex = 31;
 			this.textRepeat.Text = "1";
 			// 
+			// butPaste
+			// 
+			this.butPaste.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butPaste.Autosize = true;
+			this.butPaste.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butPaste.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butPaste.CornerRadius = 4F;
+			this.butPaste.Location = new System.Drawing.Point(6,37);
+			this.butPaste.Name = "butPaste";
+			this.butPaste.Size = new System.Drawing.Size(75,24);
+			this.butPaste.TabIndex = 29;
+			this.butPaste.Text = "Paste";
+			this.butPaste.Click += new System.EventHandler(this.butPaste_Click);
+			// 
 			// checkWeekend
 			// 
 			this.checkWeekend.Location = new System.Drawing.Point(32,113);
@@ -176,80 +232,25 @@ namespace OpenDental{
 			this.label1.Text = "Remember that this tool only applies to the visible operatories for the current a" +
     "ppointment view.  It also does not copy to a different operatory.";
 			// 
-			// button1
+			// butClearDay
 			// 
-			this.button1.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.button1.Autosize = true;
-			this.button1.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.button1.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.button1.CornerRadius = 4F;
-			this.button1.Location = new System.Drawing.Point(32,78);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75,24);
-			this.button1.TabIndex = 48;
-			this.button1.Text = "Clear Day";
-			// 
-			// butCopyWeek
-			// 
-			this.butCopyWeek.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butCopyWeek.Autosize = true;
-			this.butCopyWeek.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butCopyWeek.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butCopyWeek.CornerRadius = 4F;
-			this.butCopyWeek.Location = new System.Drawing.Point(6,85);
-			this.butCopyWeek.Name = "butCopyWeek";
-			this.butCopyWeek.Size = new System.Drawing.Size(75,24);
-			this.butCopyWeek.TabIndex = 28;
-			this.butCopyWeek.Text = "Copy Week";
-			this.butCopyWeek.Click += new System.EventHandler(this.butCopyWeek_Click);
-			// 
-			// butCopyDay
-			// 
-			this.butCopyDay.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butCopyDay.Autosize = true;
-			this.butCopyDay.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butCopyDay.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butCopyDay.CornerRadius = 4F;
-			this.butCopyDay.Location = new System.Drawing.Point(6,58);
-			this.butCopyDay.Name = "butCopyDay";
-			this.butCopyDay.Size = new System.Drawing.Size(75,24);
-			this.butCopyDay.TabIndex = 27;
-			this.butCopyDay.Text = "Copy Day";
-			this.butCopyDay.Click += new System.EventHandler(this.butCopyDay_Click);
-			// 
-			// butRepeat
-			// 
-			this.butRepeat.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butRepeat.Autosize = true;
-			this.butRepeat.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butRepeat.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butRepeat.CornerRadius = 4F;
-			this.butRepeat.Location = new System.Drawing.Point(6,64);
-			this.butRepeat.Name = "butRepeat";
-			this.butRepeat.Size = new System.Drawing.Size(75,24);
-			this.butRepeat.TabIndex = 30;
-			this.butRepeat.Text = "Repeat";
-			this.butRepeat.Click += new System.EventHandler(this.butRepeat_Click);
-			// 
-			// butPaste
-			// 
-			this.butPaste.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butPaste.Autosize = true;
-			this.butPaste.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butPaste.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butPaste.CornerRadius = 4F;
-			this.butPaste.Location = new System.Drawing.Point(6,37);
-			this.butPaste.Name = "butPaste";
-			this.butPaste.Size = new System.Drawing.Size(75,24);
-			this.butPaste.TabIndex = 29;
-			this.butPaste.Text = "Paste";
-			this.butPaste.Click += new System.EventHandler(this.butPaste_Click);
+			this.butClearDay.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butClearDay.Autosize = true;
+			this.butClearDay.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butClearDay.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butClearDay.CornerRadius = 4F;
+			this.butClearDay.Location = new System.Drawing.Point(32,78);
+			this.butClearDay.Name = "butClearDay";
+			this.butClearDay.Size = new System.Drawing.Size(75,24);
+			this.butClearDay.TabIndex = 48;
+			this.butClearDay.Text = "Clear Day";
+			this.butClearDay.Click += new System.EventHandler(this.butClearDay_Click);
 			// 
 			// FormBlockoutCutCopyPaste
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
 			this.ClientSize = new System.Drawing.Size(290,383);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.butClearDay);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.checkWeekend);
 			this.Controls.Add(this.groupBox1);
@@ -278,6 +279,14 @@ namespace OpenDental{
 			}
 			FillClipboard();
 			ApptViewNumPrevious=ApptViewNumCur;//remember the appt view for next time.
+		}
+
+		private void butClearDay_Click(object sender,EventArgs e) {
+			if(!MsgBox.Show(this,true,"Clear all blockouts for day?")) {
+				return;
+			}
+			Schedules.ClearBlockoutsForDay(DateSelected);//works for daily or weekly
+			Close();
 		}
 
 		private void FillClipboard(){
@@ -434,6 +443,8 @@ namespace OpenDental{
 			}
 			Close();
 		}
+
+		
 
 
 
