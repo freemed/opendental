f40957e1f3f40eded79ac7fa7894489f2066e640
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-09-12 02:36:57 +0000

    *Added random primary key ranges based on the computer network. Applied to version 5.2.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@760 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/MiscData.cs
+++ OpenDentBusiness/Data Interface/MiscData.cs
@@ -3,19 +3,47 @@ using System.Collections;
 using System.Data;
 using OpenDentBusiness;
 using OpenDental.DataAccess;
+using System.Net;
 
 namespace OpenDental {
 
 	///<summary>Miscellaneous database functions.</summary>
-	internal class MiscData {
+	public class MiscData {
+
+		private static int numComputers=0;
+		private static int myComputerNum=0;//One-based unique computer number index. Used to decide which key-partition to use for this computer.
+		private static int myPartitionStart=0;
+		private static int myPartitionEnd=0;
+
 		///<summary>Generates a random primary key.  Tests to see if that key already exists before returning it for use.  Currently, the range of returned values is greater than 0, and less than or equal to 16777215, the limit for mysql medium int.  This will eventually change to a max of 18446744073709551615.  Then, the return value would have to be a ulong and the mysql type would have to be bigint.</summary>
 		public static int GetKey(string tablename, string field) {
-			Random random = new Random();
-			double rnd = random.NextDouble();
-			while (rnd == 0 || KeyInUse(tablename, field, (int)(rnd * 16777215))) {
-				rnd = random.NextDouble();
+			//Calculate the primary key range for this computer if it has not already calculated.
+			if(numComputers==0 || myComputerNum==0){
+				try{
+					string command="SELECT COUNT(*) FROM computer";
+					DataSet result=GeneralB.GetTable(command);
+					numComputers=PIn.PInt(result.Tables[0].Rows[0][0].ToString());
+					command="SELECT COUNT(*) FROM computer WHERE ComputerNum<=(SELECT ComputerNum FROM computer AS temp WHERE CompName "+
+						"like '"+Dns.GetHostName()+"')";
+					result=GeneralB.GetTable(command);
+					myComputerNum=PIn.PInt(result.Tables[0].Rows[0][0].ToString());
+				}catch{
+					//This computer has not yet been added to the computer table. Generate any old random number as long as it is unique.
+					//This is the first introduction of the computer into the cluster.
+					numComputers=1;
+					myComputerNum=1;
+				}
+				int keymaxval=16777215;
+				int partitionSize=keymaxval/numComputers;//truncation here is good (to avoid partition overflow).
+				myPartitionStart=(myComputerNum-1)*partitionSize+1;
+				myPartitionEnd=myPartitionStart+partitionSize-1;
 			}
-			return (int)(rnd * 16777215);
+			Random random=new Random();
+			int rnd;
+			do{
+				rnd=random.Next(myPartitionStart,myPartitionEnd);
+			}while(rnd==0||KeyInUse(tablename,field,rnd));
+			return rnd;
 		}
 
 		private static bool KeyInUse(string tablename, string field, int keynum) {
