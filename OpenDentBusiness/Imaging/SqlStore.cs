1bc7f386772023b75638c5fa482a3694188658d0
fcarlier <fcarlier@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-10-02 18:01:29 +0000

    - Prototype implementation of SqlStore, not yet tested;
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@887 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Imaging/SqlStore.cs
+++ OpenDentBusiness/Imaging/SqlStore.cs
@@ -4,39 +4,115 @@ using System.Drawing.Imaging;
 using System.Collections.Generic;
 using System.Text;
 using OpenDentBusiness;
+using System.Data;
+using System.IO;
+using OpenDental.DataAccess;
 
 namespace OpenDental.Imaging {
 	public class SqlStore : ImageStoreBase {
 		protected override Bitmap OpenImage(Document doc) {
-			throw new Exception("The method or operation is not implemented.");
+			byte[] buffer = GetBytes(doc);
+			MemoryStream stream = new MemoryStream(buffer);
+			return new Bitmap(stream);
 		}
 
 		protected override byte[] GetBytes(Document doc) {
-			throw new Exception("The method or operation is not implemented.");
+			byte[] buffer;
+			using(IDbConnection connection = DataSettings.GetConnection())
+			using(IDbCommand command = connection.CreateCommand()) {
+				command.CommandText =
+					@"SELECT Data FROM Files WHERE DocNum = ?DocNum";
+
+				IDataParameter docNumParameter = command.CreateParameter();
+				docNumParameter.ParameterName = "DocNum";
+				docNumParameter.Value = doc.DocNum;
+
+				connection.Open();
+				buffer = (byte[])command.ExecuteScalar();
+				connection.Close();
+			}
+			return buffer;
 		}
 
 		protected override void SaveDocument(Document doc, Bitmap image) {
-			throw new Exception("The method or operation is not implemented.");
+			SaveDocument(doc, image, image.RawFormat);
 		}
 
 		protected override void SaveDocument(Document doc, Bitmap image, ImageFormat format) {
-			throw new Exception("The method or operation is not implemented.");
+			using(MemoryStream stream = new MemoryStream()) {
+				image.Save(stream, format);
+				SaveDocument(doc, stream);
+			}
 		}
 
 		protected override void SaveDocument(Document doc, Bitmap image, ImageCodecInfo codec, EncoderParameters encoderParameters) {
-			throw new Exception("The method or operation is not implemented.");
+			using(MemoryStream stream = new MemoryStream()) {
+				image.Save(stream, codec, encoderParameters);
+				SaveDocument(doc, stream);
+			}
 		}
 
 		protected override void SaveDocument(Document doc, string filename) {
-			throw new Exception("The method or operation is not implemented.");
+			using(FileStream stream = new FileStream(filename, FileMode.Open, FileAccess.Read)) {
+				SaveDocument(doc, stream);
+			}
+		}
+
+		private void SaveDocument(Document doc, Stream stream) {
+			// Copy the contents to a byte array
+			int length = (int)stream.Length;
+			byte[] buffer = new byte[length];
+			stream.Read(buffer, 0, length);
+
+			using(IDbConnection connection = DataSettings.GetConnection())
+			using(IDbCommand command = connection.CreateCommand()) {
+				command.CommandText =
+					@"INSERT INTO Files (DocNum, Data, Thumbnail) VALUES (?DocNum, ?Data, NULL)";
+
+				IDataParameter docNumParameter = command.CreateParameter();
+				docNumParameter.ParameterName = "DocNum";
+				docNumParameter.Value = doc.DocNum;
+
+				IDataParameter dataParameter = command.CreateParameter();
+				dataParameter.ParameterName = "Data";
+				dataParameter.Value = buffer;
+
+				connection.Open();
+				command.ExecuteNonQuery();
+				connection.Close();
+			}
 		}
 
 		protected override void DeleteThumbnailImageInternal(Document doc) {
-			throw new Exception("The method or operation is not implemented.");
+			using(IDbConnection connection = DataSettings.GetConnection())
+			using(IDbCommand command = connection.CreateCommand()) {
+				command.CommandText =
+					@"UPDATE Files SET Thumbnail = NULL WHERE DocNum = ?DocNum";
+
+				IDataParameter docNumParameter = command.CreateParameter();
+				docNumParameter.ParameterName = "DocNum";
+				docNumParameter.Value = doc.DocNum;
+
+				connection.Open();
+				command.ExecuteNonQuery();
+				connection.Close();
+			}
 		}
 
 		protected override void DeleteDocument(Document doc) {
-			throw new Exception("The method or operation is not implemented.");
+			using(IDbConnection connection = DataSettings.GetConnection())
+			using(IDbCommand command = connection.CreateCommand()) {
+				command.CommandText =
+					@"DELETE Thumbnail WHERE DocNum = ?DocNum";
+
+				IDataParameter docNumParameter = command.CreateParameter();
+				docNumParameter.ParameterName = "DocNum";
+				docNumParameter.Value = doc.DocNum;
+
+				connection.Open();
+				command.ExecuteNonQuery();
+				connection.Close();
+			}
 		}
 	}
 }
