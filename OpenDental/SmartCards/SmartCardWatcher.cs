6e26d094a11b235ac7e8e51b88ca6ad72605dfeb
fcarlier <fcarlier@098d0599-7f2b-0410-95a5-ad30ac85393a>
2007-10-02 20:26:54 +0000

    - Add missing namespace reference;
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@891 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/SmartCards/SmartCardWatcher.cs
+++ OpenDental/SmartCards/SmartCardWatcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Collections.ObjectModel;
+using OpenDental.SmartCards.Belgium;
 
 namespace OpenDental.SmartCards {
 	public class SmartCardWatcher {
