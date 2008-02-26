cdea174688f856bb6f74cfd72633f7c8d0f69387
wjstarck <wjstarck@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-02-26 00:43:10 +0000

    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@1331 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/User Controls/ContrAnesthesia.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Data;
+using System.Windows.Forms;
+using OpenDentBusiness;
+
+namespace OpenDental
+{
+	///<summary>Summary description for ContrAnesthesia.</summary>
+	public class ContrAnesthesia : System.Windows.Forms.Control{
+		///<summary>Required designer variable.</summary>
+		private System.ComponentModel.Container components = null;
+		///<summary>This data array gets filled when loading an anesthetic. It is altered as the user makes changes, and then the results are saved to the db by reading from this array.</summary>
+		///<summary>The index in Anesthesics.List of the currently selected exam.</summary>
+		private int selectedAnesthetic;
+		//public AnesthesiaExam AnesthesiaExamCur;
+		///<summary>The index in Anesthesics.List of the currently selected anesthetic.</summary>
+		public int SelectedAnesthetic{
+			get{
+				return selectedAnesthetic;
+			}
+			set{
+				selectedAnesthetic=value;
+			}
+		}
+
+		///<summary></summary>
+		protected override Size DefaultSize{
+			get{
+				return new Size(590,665);
+			}
+		}
+
+		///<summary></summary>
+		public ContrAnesthesia(){
+			
+		}
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+		
+		}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
