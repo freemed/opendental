07e530d127fe827846bef7104deb03ffcd20c1be
wjstarck <wjstarck@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-03-05 07:13:25 +0000

    Last form for Anesthetic Record module and changed FormOpenDental
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@1362 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormAnestheticMedsInventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+using OpenDental.UI;
+
+namespace OpenDental.Forms
+{
+	public class FormAnestheticMedsInventory : System.Windows.Forms.Form{
+		
+		public FormAnestheticMedsInventory()
+		{
+			InitializeComponent();
+		}
+	}
+}
