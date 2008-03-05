a936c53b51f91d75f208950d33f865aaffded4de
wjstarck <wjstarck@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-03-05 06:26:49 +0000

    Anesthetic Meds Intake Form for Anesthesia Module
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@1361 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormAnestheticMedsIntake.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace OpenDental.Forms
+{
+	partial class FormAnestheticMedsIntake
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnestheticMedsIntake));
+			this.SuspendLayout();
+			// 
+			// FormAnestheticMedsIntake
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(798, 322);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "FormAnestheticMedsIntake";
+			this.Text = "Aneshetic Medication Intake Form";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ OpenDental/Forms/FormAnestheticMedsIntake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+using OpenDental.UI;
+
+namespace OpenDental.Forms
+{
+	public class FormAnestheticMedsIntake : System.Windows.Forms.Form{
+
+		public FormAnestheticMedsIntake()
+		{
+			InitializeComponent();
+		}
+	}
+}
