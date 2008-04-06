6ab3a66ab66a2e8cf63cfc9816c36f9661ae83bb
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-04-06 15:08:59 +0000

    Cache folder.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@1453 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Cache/aaCache.cs
+++ OpenDentBusiness/Cache/aaCache.cs
@@ -1,2 +1,2 @@
-﻿//There will be a number of classes in this LocalCache folder.  They each will be simple repositories for global static variables.  All classes here will end in C for "Cache".
+﻿//There will be a number of classes in this Cache folder.  They each will be simple repositories for global static variables.  All classes here will end in C for "Cache".
 
