32eb3dbfe20065ae4d97d3f69953daadaee61e11
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-10-24 02:19:03 +0000

    ported back the fix for the anesth module button.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2058 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Main Modules/ContrChart.cs
+++ OpenDental/Main Modules/ContrChart.cs
@@ -2927,7 +2927,7 @@ namespace OpenDental{
 			ToolBarMain.Buttons.Add(new ODToolBarButton(Lan.g(this,"Perio Chart"),2,"","Perio"));
 			//ToolBarMain.Buttons.Add(new ODToolBarButton(ODToolBarButtonStyle.Separator));
 			if(PrefC.GetBoolSilent("EnableAnesthMod",true)){
-			ToolBarMain.Buttons.Add(new ODToolBarButton(Lan.g(this, "Anesthesia"), 3, "", "Anesthesia"));
+				ToolBarMain.Buttons.Add(new ODToolBarButton(Lan.g(this,"Anesthesia"),3,"","Anesthesia"));
 			}
 			//button=new ODToolBarButton(Lan.g(this,"Commlog"),4,"","Commlog");
 			//button.Style=ODToolBarButtonStyle.DropDownButton;
@@ -3045,10 +3045,9 @@ namespace OpenDental{
 				ToolBarMain.Buttons["Rx"].Enabled=false;
 				ToolBarMain.Buttons["LabCase"].Enabled=false;
 				ToolBarMain.Buttons["Perio"].Enabled = false;
-                if (PrefC.GetBoolSilent("EnableAnesthMod", false))
-                {
-                    ToolBarMain.Buttons["Anesthesia"].Enabled = false;
-                }
+				if(PrefC.GetBoolSilent("EnableAnesthMod", false)){
+					ToolBarMain.Buttons["Anesthesia"].Enabled = false;
+				}
 				tabProc.Enabled = false;
 				butAddKey.Enabled=false;
 				butForeignKey.Enabled=false;
@@ -3064,12 +3063,9 @@ namespace OpenDental{
 				ToolBarMain.Buttons["LabCase"].Enabled=true;
 				ToolBarMain.Buttons["Perio"].Enabled = true;
 				//turns off Anesthesia Module button if pref is not checked in Module Setup
-                if (PrefC.GetBoolSilent("EnableAnesthMod", true))
-                {
-                    ToolBarMain.Buttons["Anesthesia"].Enabled = true;
-                }
-           
-
+				if(PrefC.GetBoolSilent("EnableAnesthMod", true)){
+					ToolBarMain.Buttons["Anesthesia"].Enabled=true;
+				}
 				tabProc.Enabled=true;
 				butAddKey.Enabled=true;
 				butForeignKey.Enabled=true;
