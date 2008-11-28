148eeb2e32e300f6aa23a15816054baa2dc412dc
wjstarck <wjstarck@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-11-28 02:57:40 +0000

    Deleting meds properly adjusts medication inventory levels now; deleted some unused methods
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2193 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/AMedications.cs
+++ OpenDentBusiness/Data Interface/AMedications.cs
@@ -19,56 +19,11 @@ namespace OpenDentBusiness
 		///<summary>This data adapter is used for all queries to the database.</summary>
 		static MySqlConnection con;
 		public MySqlCommand cmd;
-		//public double QtyOnHandOld;
-		
+				
 
-		public static DataTable GetAMDataTable() {
-
-			string command = "SELECT AnesthMedName as 'Anesthetic Medication',AnesthDose as 'Dose',DoseTimeStamp as 'Time Stamp' FROM anestheticdata";
-			DataTable table = General.GetTable(command);
-			DataTable AMDataTable = table.Clone();//does not copy any data
-			AMDataTable.TableName = "anesthmedsgiven";
-			for (int i = 0; i < AMDataTable.Columns.Count; i++)
-			{
-				AMDataTable.Columns[i].DataType = typeof(string);
-			}
-			DataRow r;
-			DateTime date;
-			for (int i = 0; i < table.Rows.Count; i++)
-			{
-				r = AMDataTable.NewRow();
-				r["Anesthetic Medication"] = table.Rows[i]["Anesthetic Medication"].ToString();
-				r["Dose"] = table.Rows[i]["Dose"].ToString();
-				r["Time Stamp"] = table.Rows[i]["Time Stamp"].ToString();
-				AMDataTable.Rows.Add(r);
-			}
-			return AMDataTable;
-			}
-			/// <summary>Gets the data from anesthmedsinventory table</summary>
-
-			public static DataTable GetAMInventory(){
-			string command = "SELECT distinct AnesthMedName 'Anesthetic Medication', AnesthHowSupplied as 'How Supplied', QtyOnHand as 'Quantity on hand(mL)' FROM anesthmedsinventory order by AnestheticMedNum desc";
-			DataTable table = General.GetTable(command);
-			DataTable AMDataTable = table.Clone();//does not copy any data
-			AMDataTable.TableName = "anesthmedsinventory";
-			for (int i = 0; i < AMDataTable.Columns.Count; i++)
-			{
-				AMDataTable.Columns[i].DataType = typeof(string);
-			}
-			DataRow r;
-			DateTime date;
-			for (int i = 0; i < table.Rows.Count; i++)
-			{
-				r = AMDataTable.NewRow();
-				r["Anesthetic Medication"] = table.Rows[i]["Anesthetic Medication"].ToString();
-				r["How Supplied"] = table.Rows[i]["How Supplied"].ToString();
-				r["Quantity on hand(mL)"] = table.Rows[i]["Quantity on hand(mL)"].ToString();
-				AMDataTable.Rows.Add(r);
-			}
-			return AMDataTable;
-      }
-      /// <summary>Inserts the selected Anesthetic medication and dose values into the anesthmedsgiven table in the database</summary>
+    ///<summary>Inserts the selected Anesthetic medication and dose values into the anesthmedsgiven table in the database</summary>
 	public static int InsertAnesth_Data(int anestheticRecordNum,string anestheticOpen, string anestheticClose, string surgOpen, string surgClose, string anesthetist, string surgeon, string asst, string circulator, string VSMName, string VSMSerNum, string ASA, string ASA_EModifier, int inhO2, int inhN2O, int O2LMin, int N2OLMin, int RteNasCan, int RteNasHood, int RteETT, int MedRouteIVCath, int MedRouteIVButtFly, int MedRouteIM, int MedRoutePO, int MedRouteNasal, int MedRouteRectal, string IVSite, int IVGauge, int IVSideR, int IVSideL, int IVAtt, string IVF, int IVFVol, int MonBP, int MonSpO2, int MonEtCO2, int MonTemp, int MonPrecordial, int MonEKG, string Notes, int PatWgt, int WgtUnitsLbs, int WgtUnitsKgs, int PatHgt, string EscortName, string EscortCellNum, string EscortRel, string NPOTime, int HgtUnitsIn, int HgtUnitsCm){
+				
 			int recordnum = AnestheticRecords.GetRecordNum(anestheticRecordNum);
 		
 			string command = "INSERT INTO anestheticdata (AnestheticRecordNum,AnesthOpen,AnesthClose,SurgOpen,SurgClose,Anesthetist,Surgeon,Asst,Circulator,VSMName,VSMSerNum,ASA,ASA_EModifier,InhO2,InhN2O,O2LMin,N2OLMin,RteNasCan,RteNasHood,RteETT,MedRouteIVCath,MedRouteIVButtFly,MedRouteIM,MedRoutePO,MedRouteNasal,MedRouteRectal,IVSite,IVGauge,IVSideR,IVSideL,IVAtt,IVF,IVFVol,MonBP,MonSpO2,MonEtCO2,MonTemp,MonPrecordial,MonEKG,Notes,PatWgt,WgtUnitsLbs,WgtUnitsKgs,PatHgt,EscortName,EscortCellNum,EscortRel,NPOTime,HgtUnitsIn,HgtUnitsCm)" +
@@ -128,8 +83,8 @@ namespace OpenDentBusiness
 		}
 
 	/// <summary>Updates changes to the selected Anesthetic Record's data in the database</summary>
-	public static int UpdateAnesth_Data(int anestheticRecordNum, string anestheticOpen, string anestheticClose, string surgOpen, string surgClose, string anesthetist, string surgeon, string asst, string circulator, string VSMName, string VSMSerNum, string ASA, string ASA_EModifier, int inhO2, int inhN2O, int O2LMin, int N2OLMin, int RteNasCan, int RteNasHood, int RteETT, int MedRouteIVCath, int MedRouteIVButtFly, int MedRouteIM, int MedRoutePO, int MedRouteNasal, int MedRouteRectal, string IVSite, int IVGauge, int IVSideR, int IVSideL, int IVAtt, string IVF, int IVFVol, int MonBP, int MonSpO2, int MonEtCO2, int MonTemp, int MonPrecordial, int MonEKG, string Notes, int PatWgt, int WgtUnitsLbs, int WgtUnitsKgs, int PatHgt, string EscortName, string EscortCellNum, string EscortRel, string NPOTime, int HgtUnitsIn, int HgtUnitsCm)
-	{
+	public static int UpdateAnesth_Data(int anestheticRecordNum, string anestheticOpen, string anestheticClose, string surgOpen, string surgClose, string anesthetist, string surgeon, string asst, string circulator, string VSMName, string VSMSerNum, string ASA, string ASA_EModifier, int inhO2, int inhN2O, int O2LMin, int N2OLMin, int RteNasCan, int RteNasHood, int RteETT, int MedRouteIVCath, int MedRouteIVButtFly, int MedRouteIM, int MedRoutePO, int MedRouteNasal, int MedRouteRectal, string IVSite, int IVGauge, int IVSideR, int IVSideL, int IVAtt, string IVF, int IVFVol, int MonBP, int MonSpO2, int MonEtCO2, int MonTemp, int MonPrecordial, int MonEKG, string Notes, int PatWgt, int WgtUnitsLbs, int WgtUnitsKgs, int PatHgt, string EscortName, string EscortCellNum, string EscortRel, string NPOTime, int HgtUnitsIn, int HgtUnitsCm){
+
 		string command = "UPDATE anestheticdata SET "
 				+	" AnesthOpen		='"	+ POut.PString(anestheticOpen) + "' "
 				+	",AnesthClose		='"	+ POut.PString(anestheticClose) + "' "
@@ -207,21 +162,8 @@ namespace OpenDentBusiness
 			string command1 = "UPDATE anesthmedsinventory SET QtyOnHand = '" + qty + "' WHERE AnesthMedName = '" + AMname + "'";
 			General.NonQ(command1);
 		}
-		/// <summary>Inserts the newly added anesthetic medication and How supplied into the anesthmedsgiven table in the database</summary>
-		public static void Insertanesth_howsupplied(string anesth_Medname, string howSupplied){
-			string AMedname = anesth_Medname, HSupplied = howSupplied;
-			if (anesth_Medname.Contains("'"))
-				{
-					AMedname = anesth_Medname.Replace("'", "''");
-				}
-			if (howSupplied.Contains("'"))
-				{
-					HSupplied = howSupplied.Replace("'", "''");
-				}
-			string command = "INSERT INTO anesthmedsinventory(AnesthMedName,AnesthHowSupplied) VALUE('" + AMedname + "','" + HSupplied + "')";
-			General.NonQ(command);
-		}
-		/// <summary>Inserts the newly added anesthetic medication and How supplied into the anesthmedsgiven table in the database</summary>
+
+		/// <summary>Inserts the newly added anesthetic medication and how supplied into the anesthmedsgiven table in the database</summary>
 		public static void InsertAMedDose(string anesth_Medname, double qtyonhandold, double dose, double amtwasted, double qtyonhandnew,  int anestheticRecordNum){
 			string AMName = anesth_Medname;
 			
@@ -257,7 +199,6 @@ namespace OpenDentBusiness
 			{
 				AMName = anesthMedName.Replace("'", "''");
 			}
-
 
 			//update anesthmedsinventory
 
@@ -296,7 +237,6 @@ namespace OpenDentBusiness
 					General.NonQ(command2);
 			}
 		
-
 			//update anesthmedsgiven
 			string command3 = "UPDATE anesthmedsgiven SET "
 				+ " AnesthMedName		='" + POut.PString(AMName) + "' "
@@ -305,8 +245,6 @@ namespace OpenDentBusiness
 				+ ",DoseTimeStamp		='" + POut.PString(Convert.ToString(dosetimestamp)) + "'"
 				+ "WHERE DoseTimeStamp ='" + Convert.ToString(dosetimestamp) + "'" + " AND AnestheticRecordNum = " + anestheticRecordNum;
 			General.NonQ(command3);
-
-
 			
 		}
 
@@ -326,8 +264,7 @@ namespace OpenDentBusiness
 		
 		}
 
-		public static double GetQtyWasted(string anesthMedName, string doseTimeStamp)
-		{
+		public static double GetQtyWasted(string anesthMedName, string doseTimeStamp){
 
 			MySqlCommand command = new MySqlCommand();
 			con = new MySqlConnection(DataSettings.ConnectionString);
@@ -343,31 +280,9 @@ namespace OpenDentBusiness
 
 		}
 
-		/// <summary>Gets the data from anesthmedsgiven table</summary>
-		public static DataTable GetdataForGrid() {
-			string command = "SELECT AnesthMedName as 'Anesthetic Medication', QtyGiven as 'Dose', QtyWasted as 'Dose Wasted',DoseTimeStamp as 'Time Stamp' FROM anesthmedsgiven order by AnestheticMedNum  desc";
-			DataTable table = General.GetTable(command);
-			DataTable AMDataTable = table.Clone();//does not copy any data
-			AMDataTable.TableName = "anesthmedsgiven";
-			for (int i = 0; i < AMDataTable.Columns.Count; i++)
-				{
-					AMDataTable.Columns[i].DataType = typeof(string);
-				}
-			DataRow r;
-			DateTime date;
-			for (int i = 0; i < table.Rows.Count; i++)
-				{
-					r = AMDataTable.NewRow();
-					r["Anesthetic medication"] = table.Rows[i]["Anesthetic medication"].ToString();
-					r["Dose"] = table.Rows[i]["Dose"].ToString();
-					r["Dose Wasted"] = table.Rows[i]["Dose Wasted"].ToString();
-					r["Time Stamp"] = table.Rows[i]["Time Stamp"].ToString();
-					AMDataTable.Rows.Add(r);
-				}
-			return AMDataTable;
-		}
-		/// <summary>Updates the table anesthmedsinventory with the new quantity adjustment</summary>
+		///<summary> Updates the table anesthmedsinventory with the new quantity adjustment</summary>
 		public static void UpdateAMedInvAdj(string anesthMedName, double qty, double qtyOnHand){
+
 			double adjQty = 0.0;
 			adjQty = Convert.ToDouble(qty) + Convert.ToDouble(qtyOnHand);
 
@@ -381,31 +296,7 @@ namespace OpenDentBusiness
 
 		}
 
-		/// <summary>Updates the table anesthmedsinventoryadj</summary>
-		public static void UpdateMedNum(string notes, string adjPos, string aMed, string howSupplied, int oldqty, int rownumber){
-			string notes2 = notes, adjPos2 = adjPos, aMed2 = aMed, howSupplied2 = howSupplied;
-			if (notes.Contains("'"))
-				{
-					notes2 = notes.Replace("'", "''");
-				}
-			if (adjPos.Contains("'"))
-				{
-					adjPos2 = adjPos.Replace("'", "''");
-				}
-			if (aMed.Contains("'"))
-				{
-					aMed2 = aMed.Replace("'", "''");
-				}
-			if (howSupplied.Contains("'"))
-				{
-					howSupplied2 = howSupplied.Replace("'", "''");
-				}
-			string command = "UPDATE anesthmedsinventoryadj SET anestheticMedNum=" + rownumber + " WHERE notes='" + notes2 + "' AND adjpos='" + adjPos2 + "' AND anestheticMedNum=" + rownumber + "";
-			General.NonQ(command);
-		}
-
 		/// <summary>Updates the table anesthmedsinventory</summary>
-
 		public static void UpdateAMed_Adj_Qty(string aMed, string howsupplied, int qtyOnHand, int newQTY){
 
 			string aMed2 = aMed, howsupplied2 = howsupplied;
@@ -421,146 +312,26 @@ namespace OpenDentBusiness
 			General.NonQ(command);
 		}
 		
-		/// <summary>Delete rows from the table anesthmedsgiven</summary>
+		///<summary>Deletes anesthetic meds from the table anesthmedsgiven, and updates inventory accordingly </summary>
 		public static void DeleteAMedDose(string anesthMedName, double  QtyGiven, double QtyWasted, string TimeStamp, int anestheticRecordNum){
-			
-			//get QtyOnHandOld
-			double qtyOnHandOld = GetQtyOnHandOld(anesthMedName, TimeStamp);
-
-			//Update anesthmedsgiven
-			string command = "DELETE FROM anesthmedsgiven WHERE AnesthMedName='" + anesthMedName + "' and QtyGiven=" + QtyGiven + " and DoseTimeStamp='" + TimeStamp.ToString() + "'" + " and AnestheticRecordNum = " + anestheticRecordNum;
-			General.NonQ(command);
-			
-			
-			//Update QtyOnHandOld for remaining meds of this name
-			
-
-			if (GetQtyOnHandOld(anesthMedName, TimeStamp) != 0.0)
-			{
-			double qtyOnHandOld2 = GetQtyOnHandOld(anesthMedName, TimeStamp);
-
-				string command2 = "UPDATE anesthmedsgiven SET "
-				+ " QtyOnHandOld		=	" + POut.PDouble(qtyOnHandOld2) + " "
-				+ "WHERE AnesthMedName ='" + Convert.ToString(anesthMedName) + "'";
-				General.NonQ(command2);
-
+	
 			//Update anesthmedsinventory
-			double AdjQty = Convert.ToDouble(qtyOnHandOld2) + QtyGiven + QtyWasted;
+			double AdjQty = Convert.ToDouble(GetQtyOnHand(anesthMedName)) + QtyGiven + QtyWasted;
 			string command3 = "UPDATE anesthmedsinventory SET "
 					+ " QtyOnHand		=	" + POut.PDouble(AdjQty) + " "
 					+ "WHERE AnesthMedName ='" + Convert.ToString(anesthMedName) + "'";
 			General.NonQ(command3);
 
 
-			}
-		}
-
-		/*public static void DeleteRow(int selectedRow)
-		{
-
-			string command = "DELETE FROM anesthmedsgiven WHERE AnesthMedName='" + anesthMedName + "' and QtyGiven=" + QtyGiven + " and DoseTimeStamp='" + TimeStamp.ToString() + "'";
+			//Update anesthmedsgiven
+			string command = "DELETE FROM anesthmedsgiven WHERE AnesthMedName='" + anesthMedName + "' and QtyGiven=" + QtyGiven + " and DoseTimeStamp='" + TimeStamp.ToString() + "'" + " and AnestheticRecordNum = " + anestheticRecordNum;
 			General.NonQ(command);
-		}*/
-
-		public static void Update(string aMed, string howsupplied, int qtyOnHand, string notes, int oldQty, int medNum){
-
-			string notes2 = notes, aMed2 = aMed, howsupplied2 = howsupplied;
-			if (notes.Contains("'"))
-				{
-					notes2 = notes.Replace("'", "''");
-				}
-			if (aMed.Contains("'"))
-				{
-					aMed2 = aMed.Replace("'", "''");
-				}
-			if (howsupplied.Contains("'"))
-				{
-					howsupplied2 = howsupplied.Replace("'", "''");
-				}
-			int mednum = AMedications.GetMedNum(aMed2,howsupplied2, oldQty);
-			int medadj = AMedications.GetAdjMedNum(mednum, notes);
-
-			if (mednum == medadj && mednum != 0 && medadj != 0)
-				{
-					string command1 = "UPDATE anesthmedsinventoryadj SET AnestheticMedNum=" + AMedications.GetMedNum(aMed2, howsupplied2, qtyOnHand) + " WHERE notes='" + notes2 + "'";
-					General.NonQ(command1);
-				}
-			else
-				{
-					string command1 = "INSERT INTO anesthmedsinventoryadj(AnestheticMedNum) VALUES(" + AMedications.GetMedNum(aMed2, howsupplied2, qtyOnHand) + ")";
-					General.NonQ(command1);
-				}
-			string command = "UPDATE anesthmedsinventoryadj SET Notes='" + notes2 + "' WHERE AnestheticMedNum=" + AMedications.GetMedNum(aMed2, howsupplied2, qtyOnHand);
-			General.NonQ(command);
-		}
-		public static int GetMedNum2(string aMed, double qtyOnHand){
-
-			MySqlCommand command2 = new MySqlCommand();
-			if (con.State == ConnectionState.Open)
-				con.Close();
-			con.Open();
-			command2.CommandText = "SELECT AnestheticMedNum FROM anesthmedsinventory WHERE AnesthMedName= '" + (aMed) + "'";
-			command2.Connection = con;
-			string mednum = Convert.ToString(command2.ExecuteScalar());
-			int medid = 0;
-			if (mednum != null && mednum != "")
-				return medid = Convert.ToInt32(mednum);
-			else
-				return medid;
-		}
-
-		public static int GetMedNum(string aMed, string howsupplied, int qtyOnHand){
-
-			string command = "SELECT AnestheticMedNum FROM anesthmedsinventory WHERE AnesthMedName= '" + aMed + "' and AnesthHowSupplied='" + howsupplied + "' and QtyOnHand=" + qtyOnHand + "";
-			return General.NonQ(command);
-			//int medid = 0;
-			//if (mednum != null && mednum != "")
-			//    return medid = Convert.ToInt32(mednum);
-			//else
-			//    return medid;
-		}
-
-		/// <summary> Gets the anestheticmednum from the anesthmedsinventoryadj table. /// </summary>
-
-		public static int GetAdjMedNum(int mednum, string notes){
-			
-			string command = "SELECT AnestheticMedNum FROM anesthmedsinventoryadj WHERE AnestheticMedNum  =" + mednum + "";
-			return General.NonQ(command);
-			//int medid = 0;
-			//if (mednumadj != null && mednumadj != "")
-			//    return medid = Convert.ToInt32(mednumadj);
-			//else
-			//    return medid;
-		}
-		public static DataTable GetMedNumber(int rownumber){
-			
-			string command = "SELECT AnestheticMedNum FROM anesthmedsinventoryadj WHERE AnestheticMedNum=" + rownumber;
-			DataTable dt = new DataTable();
-			dt = General.GetTable(command);
-			DataTable dtclone = dt.Clone();
-
-			for (int i = 0; i < dtclone.Columns.Count; i++)
-			{
-				dtclone.Columns[i].DataType = typeof(string);
-			}
-			DataRow r;
-			for (int i = 0; i < dt.Rows.Count; i++)
-				{
-					r = dtclone.NewRow();
-					r["AnestheticMedNum"] = dt.Rows[i]["AnestheticMedNum"].ToString();
-					//r["How Supplied"] = dt.Rows[i]["How Supplied"].ToString();
-					//r["Quantity on hand(mLs)"] = dt.Rows[i]["Quantity on hand(mLs)"].ToString();
-					//r["Quantity Adjustment(mLs)"] = "";
-					//r["Notes"] = dt.Rows[i]["Notes"].ToString();
-					dtclone.Rows.Add(r);
-			}
-				return dtclone;
+		
 		}
 
 		/// <summary>Returns QtyOnHand for anesthetic medication inventory adjustment calculations/// </summary>
 
-		public static double GetQtyOnHand(string aMed)
-		{
+		public static double GetQtyOnHand(string aMed){
 
 			MySqlCommand cmd = new MySqlCommand();
 			con = new MySqlConnection(DataSettings.ConnectionString);
@@ -568,7 +339,6 @@ namespace OpenDentBusiness
 			if (con.State == ConnectionState.Open)
 				con.Close();
 			con.Open();
-
 			//if (con.State == ConnectionState.Open) MessageBox.Show("Connection to MySQL opened through OLE DB Provider"); //for testing mySQL connection
 			cmd.CommandText = "SELECT QtyOnHand FROM anesthmedsinventory WHERE AnesthMedName='" + aMed + "'";
 			string QtyOnHand = Convert.ToString(cmd.ExecuteScalar());
@@ -577,34 +347,6 @@ namespace OpenDentBusiness
 			return qtyOnHand;
 		}
 
-			public static double GetQtyOnHandOld(string aMed, string doseTimeStamp){
-
-			MySqlCommand cmd = new MySqlCommand();
-			con = new MySqlConnection(DataSettings.ConnectionString);
-			cmd.Connection = con;
-			if (con.State == ConnectionState.Open)
-				con.Close();
-			con.Open();
-
-			//if (con.State == ConnectionState.Open) MessageBox.Show("Connection to MySQL opened through OLE DB Provider"); //for testing mySQL connection
-			cmd.CommandText = "SELECT QtyOnHandOld FROM anesthmedsgiven WHERE AnesthMedName='" + aMed + "'";  // +"AND DoseTimeStamp='" + doseTimeStamp + "'";
-			string QtyOnHandOld = Convert.ToString(cmd.ExecuteScalar());
-
-			if (QtyOnHandOld == "")
-			{
-				return 0.0;
-			}
-
-			else
-			{	double qtyOnHandOld = Convert.ToDouble(QtyOnHandOld);
-				con.Close();
-				return qtyOnHandOld;
-			}
-
-
-			
-
-		}
 
 	}
 	
