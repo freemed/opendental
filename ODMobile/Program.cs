b09df4bbe4eec391012440a3793e7129ef25254e
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2008-12-22 21:07:23 +0000

    Added ODMobile project.  This may be a problem, in which case, I will remove it.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2269 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ ODMobile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace OpenDentMobile {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[MTAThread]
+		static void Main() {
+			Application.Run(new Form1());
+		}
+	}
+}
