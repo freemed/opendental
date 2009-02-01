e71ba2f40ad5439611ccb02536cf0de745b247d6
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-02-01 19:07:54 +0000

    missing files
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2358 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Bridges/EHG_statements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using OpenDentBusiness;
+
+namespace OpenDental.Bridges {
+	public class EHG_statements {
+		///<summary>Generates all the xml up to the point where the first statement would go.</summary>
+		public static void GeneratePracticeInfo(XmlWriter writer) {
+
+		}
+
+		public static void GenerateOneStatement(XmlWriter writer,Statement stmt,Patient pat,Family fam,DataSet dataSet){
+
+		}
+
+		///<summary>After statements are added, this adds the necessary closing xml elements.</summary>
+		public static void GenerateWrapUp(XmlWriter writer) {
+
+		}
+
+		public static void Send(string data) {
+
+		}
+
+
+	}
+}
