ee507779715704dd5fa413e14605ec7d573b539a
wjstarck <wjstarck@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-03-10 01:53:32 +0000

    Some cleanup
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2445 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Data Interface/AnesthVSData.cs
+++ OpenDental/Data Interface/AnesthVSData.cs
@@ -24,69 +24,6 @@ namespace OpenDental{
 			return new List<AnestheticVSData>(DataObjectFactory<AnestheticVSData>.CreateObjects(command));
 		}
 
-		/*public static void WriteObject(AnesthVSData med){
-
-			DataObjectFactory<AnesthVSData>.WriteObject(med);
-		}*/
-
-		///<summary>Surround with try-catch. Shouldn't need this</summary>
-		/*public static void DeleteObject(AnestheticMedsGiven med){
-
-			//validate that not already in use.
-			string command="SELECT COUNT(*) FROM anesthmedsgiven WHERE DoseTimeStamp="+POut.PString(med.DoseTimeStamp);
-			int count=PIn.PInt(General.GetCount(command));
-            //disabled during development, will probably need to enable for release
-			/*if(count>0) {
-				throw new ApplicationException(Lan.g("AnestheticMeds","Anesthetic Medication is already in use. Not allowed to delete."));
-			}*/
-			/*command="SELECT COUNT(*) FROM anesthmedsgiven WHERE DoseTimeStamp="+POut.PString(med.DoseTimeStamp);
-			count=PIn.PInt(General.GetCount(command));*/
-            //disabled for now...
-			/*if(count>0) {
-				throw new ApplicationException(Lan.g("AnestheticMeds","Anesthetic Medication is already in use. Not allowed to delete."));
-			}*/
-            /*DataObjectFactory<AnestheticMedsGiven>.DeleteObject(med);
-		}*/
-
-		public static string GetName(List<AnestheticMedsGiven> listAnesthMedsGiven, string doseTimeStamp)
-		{
-			for (int i = 0; i < listAnesthMedsGiven.Count; i++)
-			{
-
-				if (listAnesthMedsGiven[i].DoseTimeStamp == doseTimeStamp)
-				{
-
-					return listAnesthMedsGiven[i].AnesthMedName;
-				}
-			}
-			return "";
-		}
-
-		public static string GetQtyGiven(List<AnestheticMedsGiven> listAnesthMedsGiven, string doseTimeStamp){
-			for (int i = 0; i < listAnesthMedsGiven.Count; i++){
-
-				if (listAnesthMedsGiven[i].DoseTimeStamp == doseTimeStamp){
-
-					return listAnesthMedsGiven[i].QtyGiven;
-				}
-			}
-			return "";
-		}
-
-		/// <summary>
-		/// Gets the Anesthetic Record number from the anestheticrecord table.
-		/// </summary>
-		public static int getRecordNum(int patnum)
-		{
-			MySqlCommand command2 = new MySqlCommand();
-			con.Open();
-			command2.CommandText = "SELECT Max(AnestheticRecordNum)  FROM anestheticrecord a, Patient p where a.Patnum = p.Patnum and p.patnum = " + patnum + "";
-			command2.Connection = con;
-			int supplierID = Convert.ToInt32(command2.ExecuteScalar());
-			return supplierID;
-			con.Close();
-		}
-
 
 	}
 
