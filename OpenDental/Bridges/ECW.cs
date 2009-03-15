1871217f4ce00308c67ed408e58bb01b4cf292e5
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-03-15 22:46:40 +0000

    Missing file.  And version changes.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2478 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Bridges/ECW.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenDentBusiness;
+
+namespace OpenDental.Bridges {
+	public class ECW {
+		///<summary>AptNum is always passed in by eCW.  It is used in the logic for setting procedures complete within apt edit window.</summary>
+		public static int AptNum;
+
+		//OD accepts commandline arguments from eCW.  That's handled in FormOpenDental.
+
+		public static void SendHL7(Appointment apt,Patient pat) {
+			OpenDentBusiness.HL7.DFT dft=new OpenDentBusiness.HL7.DFT(apt,pat);
+			HL7Msg msg=new HL7Msg();
+			msg.AptNum=apt.AptNum;
+			msg.HL7Status=HL7MessageStatus.OutPending;//it will be marked outSent by the HL7 service.
+			msg.MsgText=dft.GenerateMessage();
+			HL7Msgs.WriteObject(msg);
+		}
+
+
+
+
+
+	}
+}
