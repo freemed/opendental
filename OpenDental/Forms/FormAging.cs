8bef9a0b6134f5e0cb8793451bd191aa0e822920
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-04-07 02:58:38 +0000

    Aging now runs as of the date in the window rather than as of today.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2558 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormAging.cs
+++ OpenDental/Forms/FormAging.cs
@@ -177,7 +177,7 @@ namespace OpenDental{
 				}
 			}
 			Cursor=Cursors.WaitCursor;
-			Ledgers.ComputeAging(0,DateTime.Today);
+			Ledgers.ComputeAging(0,PIn.PDate(textDateCalc.Text));
 			if(Prefs.UpdateString("DateLastAging",POut.PDate(PIn.PDate(textDateCalc.Text),false))){
 				DataValid.SetInvalid(InvalidType.Prefs);
 			}
