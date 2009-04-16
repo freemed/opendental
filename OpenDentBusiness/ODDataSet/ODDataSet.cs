8b37c585ddeb98c2991f84c71d424bfcce358149
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-04-16 03:35:38 +0000

    debugging new ODDataSet
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2585 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/ODDataSet/ODDataSet.cs
+++ OpenDentBusiness/ODDataSet/ODDataSet.cs
@@ -7,12 +7,35 @@ using System.Reflection;
 using System.Xml;
 
 namespace OpenDentBusiness {
-	/*
+	
 	public class ODDataSet {
 		public ODDataTableCollection Tables;
 
 		public ODDataSet(string xmlData){
 			Tables=new ODDataTableCollection();
+			XmlDocument doc=new XmlDocument();
+			doc.LoadXml(xmlData);
+			ODDataTable currentTable=new ODDataTable();
+			ODDataRow currentRow;
+			XmlNodeList nodesRows=doc.DocumentElement.ChildNodes;
+			for(int i=0;i<nodesRows.Count;i++) {
+			//foreach(XElement elementRow in rootElement.Elements()) {
+				currentRow=new ODDataRow();
+				if(currentTable.Name=="") {
+					currentTable.Name=nodesRows[i].Name;
+				}
+				else if(currentTable.Name!=nodesRows[i].Name) {
+					this.Tables.Add(currentTable);
+					currentTable=new ODDataTable();
+					currentTable.Name=nodesRows[i].Name;
+				}
+				foreach(XmlNode nodeCell in nodesRows[i].ChildNodes) {
+					currentRow.Add(nodeCell.Name,nodeCell.InnerXml);
+				}
+				currentTable.Rows.Add(currentRow);
+			}
+			this.Tables.Add(currentTable);
+			/*
 			XElement rootElement=XElement.Parse(xmlData);
 			ODDataTable currentTable=new ODDataTable();
 			//Dictionary<string,string> currentRow;
@@ -32,7 +55,7 @@ namespace OpenDentBusiness {
 				}
 				currentTable.Rows.Add(currentRow);
 			}
-			this.Tables.Add(currentTable);
+			this.Tables.Add(currentTable);*/
 		}
 	}
 
@@ -52,7 +75,7 @@ namespace OpenDentBusiness {
       }
 		}
 	}
-	*/
+	
 	
 
 	
