f9929a2def5a357088294369f87dd5721908e023
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-04-22 18:22:29 +0000

    Starting to add object[] serialization code.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2613 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/DtoObject.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace OpenDentBusiness {
+	///<summary>Packages any object with a TypeName so that it can be serialized and deserialized better.</summary>
+	public class DtoObject:IXmlSerializable {
+		///<summary>Fully qualified name, including the namespace but not the assembly.  Examples: System.Int32, OpenDentBusiness.Patient, OpenDentBusiness.Patient[], List&lt;OpenDentBusiness.Patient&gt;.  When the xml element is created for the Obj, the namespace is not included.  So this field properly stores it.</summary>
+		public string TypeName;
+		///<summary>The actual object.</summary>
+		public object Obj;
+
+		///<summary>Empty constructor as required by IXmlSerializable</summary>
+		public DtoObject() {
+		}
+
+		///<summary>This is the constructor that should be used normally because it automatically creates the TypeName.</summary>
+		public DtoObject(object obj) {
+			Obj=obj;
+			Type type=obj.GetType();
+			//This will eventually become much more complex:
+			//Arrays automatically become "ArrayOf..." and serialize just fine, with TypeName=...[]
+			//Lists:
+			if(type.IsGenericType) {
+				Type listType=type.GetGenericArguments()[0];
+				TypeName="List<"+listType.FullName+">";
+			}
+			else {
+				TypeName=type.FullName;
+			}
+		}
+
+		public void WriteXml(XmlWriter writer) {
+			/* we want the result to look like this:
+			<TypeName>Patient</TypeName>
+			<Obj>
+				<Patient>
+					<LName>Smith</LName>
+					<PatNum>22</PatNum>
+					<IsGuar>True</IsGuar>
+				</Patient>
+			</Obj>
+			*/
+			writer.WriteStartElement("TypeName");
+			writer.WriteString(TypeName);
+			writer.WriteEndElement();//TypeName
+			writer.WriteStartElement("Obj");
+			Type type=null;
+			type=Obj.GetType();
+			XmlSerializer serializer = new XmlSerializer(type);
+			serializer.Serialize(writer,Obj);
+			writer.WriteEndElement();//Obj
+		}
+
+		public void ReadXml(XmlReader reader) {
+			//personName = reader.ReadString();
+			reader.ReadToFollowing("TypeName");
+			reader.ReadStartElement("TypeName");
+			TypeName=reader.ReadString();
+			reader.ReadEndElement();//TypeName
+			reader.ReadToFollowing("Obj");
+			reader.ReadStartElement("Obj");
+			while(reader.NodeType!=XmlNodeType.Element) {
+				reader.Read();//gets rid of whitespace if in debug mode.
+			}
+			string strObj=reader.ReadOuterXml();
+			//now get the reader to the correct location
+			while(reader.NodeType!=XmlNodeType.EndElement) {
+				reader.Read();
+			}
+			reader.ReadEndElement();//Obj
+			while(reader.NodeType!=XmlNodeType.EndElement) {
+				reader.Read();
+			}
+			reader.ReadEndElement();//DtoObject
+			Type type=null;
+			if(TypeName.StartsWith("List<")) {
+				Type typeGen=Type.GetType(TypeName.Substring(5,TypeName.Length-6));
+				Type typeList=typeof(List<>);
+				type=typeList.MakeGenericType(typeGen);
+			}
+			else {
+				type=Type.GetType(TypeName);
+			}
+			XmlSerializer serializer = new XmlSerializer(type);
+			XmlReader reader2=XmlReader.Create(new StringReader(strObj));
+			Obj=serializer.Deserialize(reader2);
+		}
+
+		///<summary>Required by IXmlSerializable</summary>
+		public XmlSchema GetSchema() {
+			return (null);
+		}
+
+
+	}
+}
---
+++ OpenDentBusiness/XmlConverter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace OpenDentBusiness {
+	public class XmlConverter {
+		///<summary>Should accept any type, including simple types, OD types, Arrays, Lists, and arrays of DtoObject.  But not DataTable or DataSet.  If we find a type that isn't supported, then we need to add it.</summary>
+		public static string Serialize<T>(T obj) {
+			StringBuilder strBuild=new StringBuilder();
+			#if DEBUG
+				XmlWriterSettings settings=new XmlWriterSettings();
+				settings.Indent=true;
+				settings.IndentChars="   ";
+				//using the constructor decreases performance and leads to memory leaks.
+				//But it makes the xml much more readable
+				XmlWriter writer=XmlWriter.Create(strBuild,settings);
+			#else
+				XmlWriter writer=XmlWriter.Create(strBuild);
+			#endif
+			XmlSerializer serializer = new XmlSerializer(typeof(T));
+			serializer.Serialize(writer,obj);
+			writer.Close();
+			return strBuild.ToString();
+		}
+
+		///<summary>Should accept any type, including simple types, OD types, Arrays, Lists, and arrays of DtoObject.  But not DataTable or DataSet.  If we find a type that isn't supported, then we need to add it.</summary>
+		public static T Deserialize<T>(string xmlData) {
+			Type type = typeof(T);
+			StringReader strReader=new StringReader(xmlData);
+			XmlReader reader=XmlReader.Create(strReader);
+			XmlSerializer serializer = new XmlSerializer(type);
+			T retVal=(T)serializer.Deserialize(reader);
+			strReader.Close();
+			reader.Close();
+			return retVal;
+		}
+	
+		public static string TableToXml(DataTable table){
+			StringBuilder strBuild=new StringBuilder();
+			XmlWriter xmlWriter=XmlWriter.Create(strBuild);
+			if(table.TableName==""){
+				throw new ApplicationException("OpenDentBusiness.WebServer.XmlConverter.TableToXml requires a tablename matching the type of the collection.");
+			}
+			table.WriteXml(xmlWriter,XmlWriteMode.WriteSchema);
+			xmlWriter.Close();
+			return strBuild.ToString();
+		}
+
+		public static string DsToXml(DataSet ds){
+			StringBuilder strBuild=new StringBuilder();
+			XmlWriter xmlWriter=XmlWriter.Create(strBuild);
+			ds.WriteXml(xmlWriter);
+			xmlWriter.Close();
+			return strBuild.ToString();
+		}
+
+		public static DataTable XmlToTable(string xmlData) {
+			DataTable table=new DataTable();
+			XmlReader xmlReader=XmlReader.Create(new StringReader(xmlData));
+			table.ReadXml(xmlReader);
+			xmlReader.Close();
+			return table;
+		}
+
+		public static DataSet XmlToDs(string xmlData) {
+			DataSet ds=new DataSet();
+			XmlReader xmlReader=XmlReader.Create(new StringReader(xmlData));
+			ds.ReadXml(xmlReader);
+			xmlReader.Close();
+			return ds;
+		}
+		
+
+	}
+}
