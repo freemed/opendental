92ad080c2001d0bf1ef1f69f2d2d5a9d036cbc41
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2009-05-12 20:39:50 +0000

    Added file copy project.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@2679 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ UpdateFileCopier/FormMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UpdateFileCopier {
+	public partial class FormMain:Form {
+		public FormMain() {
+			InitializeComponent();
+		}
+
+		private void FormMain_Load(object sender,EventArgs e) {
+			
+		}
+	}
+}
