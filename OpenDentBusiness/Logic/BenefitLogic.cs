bb3e9c4801ce3ac9355533d77ab62f8b898e4b08
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-02-01 23:34:53 +0000

    Creating a preauthorization for a plan that did not use calendar year was giving an error message.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@3561 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Logic/BenefitLogic.cs
+++ OpenDentBusiness/Logic/BenefitLogic.cs
@@ -6,6 +6,9 @@ namespace OpenDentBusiness {
 	public class BenefitLogic {
 		///<summary>This function is unit tested for accuracy because it has been a repeated source of bugs in the past.</summary>
 		public static DateTime ComputeRenewDate(DateTime asofDate,int monthRenew){
+			if(asofDate.Year<1880) {//this clause is not unit tested.
+				return DateTime.Today;
+			}
 			if(monthRenew==0) {
 				return new DateTime(asofDate.Year,1,1);
 			}
