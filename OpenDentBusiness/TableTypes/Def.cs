7191c6cb273379e4915427f64cb71bb5e401d47e
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-05-20 14:05:37 +0000

    When using web service, edit defs, colors were not showing.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@3915 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/TableTypes/Def.cs
+++ OpenDentBusiness/TableTypes/Def.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace OpenDentBusiness {
 	///<summary>The info in the definition table is used by other tables extensively.  Almost every table in the database links to definition.  Almost all links to this table will be to a DefNum.  Using the DefNum, you can find any of the other fields of interest, usually the ItemName.  Make sure to look at the Defs class to see how the definitions are used.  Loaded into memory ahead of time for speed.  Some types of info such as operatories started out life in this table, but then got moved to their own table when more complexity was needed.</summary>
@@ -17,9 +18,21 @@ namespace OpenDentBusiness {
 		///<summary>This field can be used to store extra info about the item.</summary>
 		public string ItemValue;
 		///<summary>Some categories include a color option.</summary>
+		[XmlIgnore]
 		public Color ItemColor;
 		///<summary>If hidden, the item will not show on any list, but can still be referenced.</summary>
 		public bool IsHidden;
+
+		///<summary>Used only for serialization purposes</summary>
+		[XmlElement("ItemColor",typeof(int))]
+		public int ItemColorXml {
+			get {
+				return ItemColor.ToArgb();
+			}
+			set {
+				ItemColor = Color.FromArgb(value);
+			}
+		}
 
 		///<summary>Returns a copy of the def.</summary>
 		public Def Copy() {
