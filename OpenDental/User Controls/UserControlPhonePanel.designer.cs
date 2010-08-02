9f33b394da8e2012056e1e96f8cd27b05b61e700
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-08-02 18:31:47 +0000

    Phone panel now expands vertically if needed.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4146 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/User Controls/UserControlPhonePanel.designer.cs
+++ OpenDental/User Controls/UserControlPhonePanel.designer.cs
@@ -38,6 +38,7 @@
 			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
 			this.menuItemRinggroupAll = new System.Windows.Forms.ToolStripMenuItem();
 			this.menuItemRinggroupNone = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuItemRinggroupsDefault = new System.Windows.Forms.ToolStripMenuItem();
 			this.menuItemBackup = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
 			this.menuItemLunch = new System.Windows.Forms.ToolStripMenuItem();
@@ -47,7 +48,6 @@
 			this.timerMsgs = new System.Windows.Forms.Timer(this.components);
 			this.butOverride = new OpenDental.UI.Button();
 			this.gridEmp = new OpenDental.UI.ODGrid();
-			this.menuItemRinggroupsDefault = new System.Windows.Forms.ToolStripMenuItem();
 			this.menuNumbers.SuspendLayout();
 			this.menuStatus.SuspendLayout();
 			this.SuspendLayout();
@@ -63,19 +63,19 @@
             this.menuItemManage,
             this.menuItemAdd});
 			this.menuNumbers.Name = "contextMenuStrip1";
-			this.menuNumbers.Size = new System.Drawing.Size(291,48);
+			this.menuNumbers.Size = new System.Drawing.Size(270,48);
 			// 
 			// menuItemManage
 			// 
 			this.menuItemManage.Name = "menuItemManage";
-			this.menuItemManage.Size = new System.Drawing.Size(290,22);
+			this.menuItemManage.Size = new System.Drawing.Size(269,22);
 			this.menuItemManage.Text = "Manage Phone Numbers";
 			this.menuItemManage.Click += new System.EventHandler(this.menuItemManage_Click);
 			// 
 			// menuItemAdd
 			// 
 			this.menuItemAdd.Name = "menuItemAdd";
-			this.menuItemAdd.Size = new System.Drawing.Size(290,22);
+			this.menuItemAdd.Size = new System.Drawing.Size(269,22);
 			this.menuItemAdd.Text = "Attach Phone Number to Current Patient";
 			this.menuItemAdd.Click += new System.EventHandler(this.menuItemAdd_Click);
 			// 
@@ -98,99 +98,106 @@
             this.menuItemHome,
             this.menuItemBreak});
 			this.menuStatus.Name = "menuStatus";
-			this.menuStatus.Size = new System.Drawing.Size(177,324);
+			this.menuStatus.Size = new System.Drawing.Size(167,302);
 			// 
 			// menuItemAvailable
 			// 
 			this.menuItemAvailable.Name = "menuItemAvailable";
-			this.menuItemAvailable.Size = new System.Drawing.Size(176,22);
+			this.menuItemAvailable.Size = new System.Drawing.Size(166,22);
 			this.menuItemAvailable.Text = "Available";
 			this.menuItemAvailable.Click += new System.EventHandler(this.menuItemAvailable_Click);
 			// 
 			// menuItemTraining
 			// 
 			this.menuItemTraining.Name = "menuItemTraining";
-			this.menuItemTraining.Size = new System.Drawing.Size(176,22);
+			this.menuItemTraining.Size = new System.Drawing.Size(166,22);
 			this.menuItemTraining.Text = "Training";
 			this.menuItemTraining.Click += new System.EventHandler(this.menuItemTraining_Click);
 			// 
 			// menuItemTeamAssist
 			// 
 			this.menuItemTeamAssist.Name = "menuItemTeamAssist";
-			this.menuItemTeamAssist.Size = new System.Drawing.Size(176,22);
+			this.menuItemTeamAssist.Size = new System.Drawing.Size(166,22);
 			this.menuItemTeamAssist.Text = "TeamAssist";
 			this.menuItemTeamAssist.Click += new System.EventHandler(this.menuItemTeamAssist_Click);
 			// 
 			// menuItemWrapUp
 			// 
 			this.menuItemWrapUp.Name = "menuItemWrapUp";
-			this.menuItemWrapUp.Size = new System.Drawing.Size(176,22);
+			this.menuItemWrapUp.Size = new System.Drawing.Size(166,22);
 			this.menuItemWrapUp.Text = "WrapUp";
 			this.menuItemWrapUp.Click += new System.EventHandler(this.menuItemWrapUp_Click);
 			// 
 			// menuItemOfflineAssist
 			// 
 			this.menuItemOfflineAssist.Name = "menuItemOfflineAssist";
-			this.menuItemOfflineAssist.Size = new System.Drawing.Size(176,22);
+			this.menuItemOfflineAssist.Size = new System.Drawing.Size(166,22);
 			this.menuItemOfflineAssist.Text = "OfflineAssist";
 			this.menuItemOfflineAssist.Click += new System.EventHandler(this.menuItemOfflineAssist_Click);
 			// 
 			// menuItemUnavailable
 			// 
 			this.menuItemUnavailable.Name = "menuItemUnavailable";
-			this.menuItemUnavailable.Size = new System.Drawing.Size(176,22);
+			this.menuItemUnavailable.Size = new System.Drawing.Size(166,22);
 			this.menuItemUnavailable.Text = "Unavailable";
 			this.menuItemUnavailable.Click += new System.EventHandler(this.menuItemUnavailable_Click);
 			// 
 			// toolStripMenuItem2
 			// 
 			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-			this.toolStripMenuItem2.Size = new System.Drawing.Size(173,6);
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(163,6);
 			// 
 			// menuItemRinggroupAll
 			// 
 			this.menuItemRinggroupAll.Name = "menuItemRinggroupAll";
-			this.menuItemRinggroupAll.Size = new System.Drawing.Size(176,22);
+			this.menuItemRinggroupAll.Size = new System.Drawing.Size(166,22);
 			this.menuItemRinggroupAll.Text = "Ringgroups All";
 			this.menuItemRinggroupAll.Click += new System.EventHandler(this.menuItemRinggroupAll_Click);
 			// 
 			// menuItemRinggroupNone
 			// 
 			this.menuItemRinggroupNone.Name = "menuItemRinggroupNone";
-			this.menuItemRinggroupNone.Size = new System.Drawing.Size(176,22);
+			this.menuItemRinggroupNone.Size = new System.Drawing.Size(166,22);
 			this.menuItemRinggroupNone.Text = "Ringgroups None";
 			this.menuItemRinggroupNone.Click += new System.EventHandler(this.menuItemRinggroupNone_Click);
+			// 
+			// menuItemRinggroupsDefault
+			// 
+			this.menuItemRinggroupsDefault.Name = "menuItemRinggroupsDefault";
+			this.menuItemRinggroupsDefault.Size = new System.Drawing.Size(166,22);
+			this.menuItemRinggroupsDefault.Text = "Ringgroups Default";
+			this.menuItemRinggroupsDefault.Click += new System.EventHandler(this.menuItemRinggroupsDefault_Click);
 			// 
 			// menuItemBackup
 			// 
 			this.menuItemBackup.Name = "menuItemBackup";
-			this.menuItemBackup.Size = new System.Drawing.Size(176,22);
+			this.menuItemBackup.Size = new System.Drawing.Size(166,22);
 			this.menuItemBackup.Text = "Backup";
 			this.menuItemBackup.Click += new System.EventHandler(this.menuItemBackup_Click);
 			// 
 			// toolStripMenuItem1
 			// 
 			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-			this.toolStripMenuItem1.Size = new System.Drawing.Size(173,6);
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(163,6);
 			// 
 			// menuItemLunch
 			// 
 			this.menuItemLunch.Name = "menuItemLunch";
-			this.menuItemLunch.Size = new System.Drawing.Size(176,22);
+			this.menuItemLunch.Size = new System.Drawing.Size(166,22);
 			this.menuItemLunch.Text = "Lunch";
 			this.menuItemLunch.Click += new System.EventHandler(this.menuItemLunch_Click);
 			// 
 			// menuItemHome
 			// 
 			this.menuItemHome.Name = "menuItemHome";
-			this.menuItemHome.Size = new System.Drawing.Size(176,22);
+			this.menuItemHome.Size = new System.Drawing.Size(166,22);
 			this.menuItemHome.Text = "Home";
 			this.menuItemHome.Click += new System.EventHandler(this.menuItemHome_Click);
 			// 
 			// menuItemBreak
 			// 
 			this.menuItemBreak.Name = "menuItemBreak";
-			this.menuItemBreak.Size = new System.Drawing.Size(176,22);
+			this.menuItemBreak.Size = new System.Drawing.Size(166,22);
 			this.menuItemBreak.Text = "Break";
 			this.menuItemBreak.Click += new System.EventHandler(this.menuItemBreak_Click);
 			// 
@@ -227,24 +234,19 @@
 			// gridEmp
 			// 
 			this.gridEmp.AllowSelection = false;
+			this.gridEmp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
 			this.gridEmp.HScrollVisible = false;
 			this.gridEmp.Location = new System.Drawing.Point(0,24);
 			this.gridEmp.Name = "gridEmp";
 			this.gridEmp.ScrollValue = 0;
-			this.gridEmp.Size = new System.Drawing.Size(428,295);
+			this.gridEmp.Size = new System.Drawing.Size(428,299);
 			this.gridEmp.TabIndex = 22;
 			this.gridEmp.Title = "Phones";
 			this.gridEmp.TranslationName = "TableEmpClock";
 			this.gridEmp.WrapText = false;
 			this.gridEmp.CellClick += new OpenDental.UI.ODGridClickEventHandler(this.gridEmp_CellClick);
 			this.gridEmp.MouseUp += new System.Windows.Forms.MouseEventHandler(this.gridEmp_MouseUp);
-			// 
-			// menuItemRinggroupsDefault
-			// 
-			this.menuItemRinggroupsDefault.Name = "menuItemRinggroupsDefault";
-			this.menuItemRinggroupsDefault.Size = new System.Drawing.Size(176,22);
-			this.menuItemRinggroupsDefault.Text = "Ringgroups Default";
-			this.menuItemRinggroupsDefault.Click += new System.EventHandler(this.menuItemRinggroupsDefault_Click);
 			// 
 			// UserControlPhonePanel
 			// 
