1a4e1296e9bc0957dbf09989f78944770d056276
dennis <dennis@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-08-05 16:36:19 +0000

    Code under development.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4169 098d0599-7f2b-0410-95a5-ad30ac85393a

--- WebForms/WebForm1.aspx.cs
+++ WebForms/WebForm1.aspx.cs
@@ -7,108 +7,186 @@ using System.Web.UI.WebControls;
 using System.Drawing;
 using System.Collections;
 
+
 namespace WebForms
 {
 	public partial class WebForm1:System.Web.UI.Page
 	{
 		private Hashtable FormValuesHashTable = new Hashtable();
+		private int DentalOfficeID = 0;
 
 		protected void Page_Load(object sender,EventArgs e)
-		{
-			SetPagePreferences(3);
+		{	
+			try
+			{				
+			int DentalOfficeID = 0;
+				if(Request["DentalOfficeID"] != null)
+				{					DentalOfficeID = Convert.ToInt32(Request["DentalOfficeID"].ToString().Trim());
+					Int32.TryParse(Request["DentalOfficeID"].ToString().Trim(),out DentalOfficeID);
+				}
+			//hard code for testing only
+				DentalOfficeID=1;
+				SetPagePreferences(DentalOfficeID);
+			}
+			catch(Exception ex)
+			{
+				Logger.Information(ex.Message.ToString());
+			}
 		}
 
-		private void SetPagePreferences(int CustomerId)
+		/// <summary>
+		/// sets page specifics like colour etc based on the Dental office preferences form the db
+		/// </summary>
+		/// <param name="DentalOfficeID"></param>
+		private void SetPagePreferences(int DentalOfficeID)
 		{
-			string sqlquery = "SELECT ColorCode,ImageName  FROM cutomerpreferences WHERE CustomerId =" + CustomerId;
-			// DataSet ds = RunQuery(sqlquery);
-			int ColorCode = 16777215; // thi is the code for white
-				
-			/*
-						if (ds != null && ds.Tables[0].Rows.Count > 0)
-						{
-							ColorCode = Convert.ToInt32(ds.Tables[0].Rows[0]["ColorCode"].ToString());
-							ImageName = ds.Tables[0].Rows[0]["ImageName"].ToString();
-						}
-						*/
-
-			//ImageLogo.ImageUrl = "images/" + ImageName;
-
-			Panel1.BackColor = Color.FromArgb(ColorCode);
-			
-			
-
+			try
+			{
+				ODWebServiceEntities db = new ODWebServiceEntities();
+				int ColorCode = 16777215; // this is the code for white
+				string Heading1 = "";
+				string Heading2= "";
+				var PrefObj = from wp in db.webforms_preference where wp.DentalOfficeID==DentalOfficeID
+							  select wp;
+				if(PrefObj.Count() > 0)
+				{
+					ColorCode =PrefObj.First().ColorBorder;
+					Heading1 = PrefObj.First().Heading1;
+					Heading2= PrefObj.First().Heading2;
+				}
+				LabelHeading1.Text=Heading1;
+				LabelHeading2.Text =Heading2;
+				Panel1.BackColor = Color.FromArgb(ColorCode);
+			}
+			catch(Exception ex)
+			{
+				Logger.Information(ex.Message.ToString());
+			}
 
 		}
+		
 		protected void Submit_Click(object sender,EventArgs e)
 		{
-
-
-			ExtractFieldValues(this.Page); 
-
-
+			DentalOfficeID=1;
+			LoopThroughControls(this.Page);
+			SaveFieldValuesInDB(DentalOfficeID);
 		}
 
-		private void ExtractFieldValues(Page p)
+		private void LoopThroughControls(Page p)
 		{
-			foreach(Control c in p.Controls)
+			try
 			{
-				if(c.HasControls())
+				foreach(Control c in p.Controls)
 				{
-					ExtractKeyValue(c);
-					FindControls(c);
-				}
-				else
-				{
-					ExtractKeyValue(c);
+					if(c.HasControls())
+					{
+						ExtractValue(c);
+						FindControls(c);
+					}
+					else
+					{
+						ExtractValue(c);
+					}
 				}
 			}
+			catch(Exception ex)
+			{
+				Logger.Information(ex.Message.ToString());
+			}
 		}
+
 		private void FindControls(Control c)
 		{
-			foreach(Control ctl in c.Controls)
+			try
 			{
-				if(ctl.HasControls())
+				foreach(Control ctl in c.Controls)
 				{
-					ExtractKeyValue(ctl);
-					FindControls(ctl);
-				}
-				else
-				{
-					ExtractKeyValue(ctl);
+					if(ctl.HasControls())
+					{
+						ExtractValue(ctl);
+						FindControls(ctl);
+					}
+					else
+					{
+						ExtractValue(ctl);
+					}
 				}
 			}
+			catch(Exception ex)
+			{
+				Logger.Information(ex.Message.ToString());
+			}
 		}
-		private void ExtractKeyValue(Control c)
+
+		/// <summary>
+		/// Fill the FormValuesHashTable here.
+		/// </summary>
+		/// <param name="c"></param>
+		private void ExtractValue(Control c)
 		{
 
-
-			if(c.GetType() == typeof(TextBox))
+			try
 			{
-				TextBox tbox = ((TextBox)c);
-				FormValuesHashTable.Add(tbox.ID,((TextBox)(tbox)).Text.Trim());
-			}
+				if(c.GetType() == typeof(TextBox))
+				{
+					TextBox tbox = ((TextBox)c);
+					if(tbox.Text.Trim()!="")
+					{
+						FormValuesHashTable.Add(tbox.ID,tbox.Text.Trim());
+					}
+				}
 
-			if(c.GetType() == typeof(RadioButtonList))
+				if(c.GetType() == typeof(RadioButtonList))
+				{
+					RadioButtonList rbl = ((RadioButtonList)c);
+					if(rbl.SelectedIndex!=-1)
+					{
+						FormValuesHashTable.Add(rbl.ID,rbl.SelectedValue);
+					}
+				}
+
+				if(c.GetType() == typeof(CheckBox))
+				{
+					CheckBox cbox = ((CheckBox)c);
+					if(cbox.Checked == true)
+					{
+						FormValuesHashTable.Add(cbox.ID,cbox.Checked.ToString());
+					}
+				}
+
+			}
+			catch(Exception ex)
 			{
-				RadioButtonList rbl = ((RadioButtonList)c);
-				FormValuesHashTable.Add(rbl.ID,((RadioButtonList)(rbl)).SelectedValue);
-				
-				
+				Logger.Information(ex.Message.ToString());
 			}
-
-			if(c.GetType() == typeof(CheckBox))
-			{
-				CheckBox cbox = ((CheckBox)c);
-				FormValuesHashTable.Add(cbox.ID,((CheckBox)(cbox)).Checked.ToString());
-			}
-
-			
 
 		}
 
-		private void SaveFieldvaluesInDB()
+		private void SaveFieldValuesInDB(int DentalOfficeID)
 		{
+			try
+			{
+				ODWebServiceEntities db = new ODWebServiceEntities();
+				webforms_sheet NewSheetObj = new webforms_sheet();
+				var PrefObj = from wp in db.webforms_preference where wp.DentalOfficeID==DentalOfficeID
+							  select wp;
+				NewSheetObj.DateTimeSubmitted= DateTime.Now;
+				foreach(string key in FormValuesHashTable.Keys)
+				{
+					webforms_sheetfield NewSheetfieldObj = new webforms_sheetfield();
+					NewSheetfieldObj.FieldName=key;
+					NewSheetfieldObj.FieldValue=FormValuesHashTable[key].ToString();
+					NewSheetObj.webforms_sheetfield.Add(NewSheetfieldObj);
+				}
+				if(PrefObj.Count() > 0)
+				{
+					PrefObj.First().webforms_sheet.Add(NewSheetObj);
+				}
+			}
+			catch(Exception ex)
+			{
+				Logger.Information(ex.Message.ToString());
+			}
 		}
-}
+	}
 }
