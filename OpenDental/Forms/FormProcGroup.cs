5a14eb69e694c4f9582bd299e0830f1bf6dbed4e
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-09-16 22:11:42 +0000

    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4431 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormProcGroup.cs
@@ -0,0 +1,377 @@
+/*=============================================================================================================
+Open Dental GPL license Copyright (C) 2003  Jordan Sparks, DMD.  http://www.open-dent.com,  www.docsparks.com
+See header in FormOpenDental.cs for complete text.  Redistributions must retain this text.
+===============================================================================================================*/
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Text;
+using Microsoft.Win32;
+using OpenDentBusiness;
+using CodeBase;
+using SparksToothChart;
+using OpenDental.UI;
+
+
+namespace OpenDental{
+///<summary></summary>
+	public class FormProcGroup:System.Windows.Forms.Form {
+		public List<Procedure> ProcList;
+		private System.Windows.Forms.Label label7;
+		private OpenDental.UI.Button butOK;
+		private OpenDental.UI.Button butCancel;
+		private OpenDental.UI.Button butDelete;
+		private ODErrorProvider errorProvider2=new ODErrorProvider();
+		private OpenDental.ODtextBox textNotes;
+		private Patient PatCur;
+		private Family FamCur;
+		private Label label15;
+		private Label label16;
+		private bool SigChanged;
+		private TextBox textUser;
+		private OpenDental.UI.Button buttonUseAutoNote;
+		private List<Claim> ClaimList;
+		private bool allowTopaz;
+		private bool StartedAttachedToClaim;
+		public List<ClaimProcHist> HistList;
+		private ODGrid gridProcs;
+		private SignatureBoxWrapper signatureBoxWrapper;
+		private Label label12;
+		private ValidDate textDateEntry;
+		private Label label26;
+		private ValidDate textProcDate;
+		private Label label2;
+		public Procedure GroupCur;
+
+		public FormProcGroup() {
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		///<summary>Inserts are no longer done within this dialog, but must be done ahead of time from outside.You must specify a procedure to edit, and only the changes that are made in this dialog get saved.  Only used when double click in Account, Chart, TP, and in ContrChart.AddProcedure().  The procedure may be deleted if new, and user hits Cancel.</summary>
+
+		//Constructor from ProcEdit. Lots of this will need to be copied into the new Load function.
+		public FormProcGroup(List<Procedure> procList,Patient patCur,Family famCur) {
+			ProcList=procList;
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		#region Windows Form Designer generated code
+
+		private void InitializeComponent(){
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProcGroup));
+			this.label7 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.textUser = new System.Windows.Forms.TextBox();
+			this.gridProcs = new OpenDental.UI.ODGrid();
+			this.buttonUseAutoNote = new OpenDental.UI.Button();
+			this.textNotes = new OpenDental.ODtextBox();
+			this.butDelete = new OpenDental.UI.Button();
+			this.butCancel = new OpenDental.UI.Button();
+			this.butOK = new OpenDental.UI.Button();
+			this.signatureBoxWrapper = new OpenDental.UI.SignatureBoxWrapper();
+			this.label12 = new System.Windows.Forms.Label();
+			this.textDateEntry = new OpenDental.ValidDate();
+			this.label26 = new System.Windows.Forms.Label();
+			this.textProcDate = new OpenDental.ValidDate();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(39,78);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(73,16);
+			this.label7.TabIndex = 0;
+			this.label7.Text = "&Notes";
+			this.label7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// label15
+			// 
+			this.label15.Location = new System.Drawing.Point(-1,278);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(110,41);
+			this.label15.TabIndex = 79;
+			this.label15.Text = "Signature /\r\nInitials";
+			this.label15.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// label16
+			// 
+			this.label16.Location = new System.Drawing.Point(39,55);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(73,16);
+			this.label16.TabIndex = 80;
+			this.label16.Text = "User";
+			this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textUser
+			// 
+			this.textUser.Location = new System.Drawing.Point(114,52);
+			this.textUser.Name = "textUser";
+			this.textUser.ReadOnly = true;
+			this.textUser.Size = new System.Drawing.Size(119,20);
+			this.textUser.TabIndex = 101;
+			// 
+			// gridProcs
+			// 
+			this.gridProcs.HScrollVisible = true;
+			this.gridProcs.Location = new System.Drawing.Point(12,363);
+			this.gridProcs.Name = "gridProcs";
+			this.gridProcs.ScrollValue = 0;
+			this.gridProcs.SelectionMode = OpenDental.UI.GridSelectionMode.MultiExtended;
+			this.gridProcs.Size = new System.Drawing.Size(673,257);
+			this.gridProcs.TabIndex = 193;
+			this.gridProcs.Title = "Procedures";
+			this.gridProcs.TranslationName = "TableProg";
+			// 
+			// buttonUseAutoNote
+			// 
+			this.buttonUseAutoNote.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.buttonUseAutoNote.Autosize = true;
+			this.buttonUseAutoNote.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.buttonUseAutoNote.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.buttonUseAutoNote.CornerRadius = 4F;
+			this.buttonUseAutoNote.Location = new System.Drawing.Point(483,43);
+			this.buttonUseAutoNote.Name = "buttonUseAutoNote";
+			this.buttonUseAutoNote.Size = new System.Drawing.Size(80,24);
+			this.buttonUseAutoNote.TabIndex = 106;
+			this.buttonUseAutoNote.Text = "Auto Note";
+			this.buttonUseAutoNote.Click += new System.EventHandler(this.buttonUseAutoNote_Click);
+			// 
+			// textNotes
+			// 
+			this.textNotes.AcceptsReturn = true;
+			this.textNotes.AcceptsTab = true;
+			this.textNotes.Location = new System.Drawing.Point(114,72);
+			this.textNotes.Multiline = true;
+			this.textNotes.Name = "textNotes";
+			this.textNotes.QuickPasteType = OpenDentBusiness.QuickPasteType.Procedure;
+			this.textNotes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textNotes.Size = new System.Drawing.Size(450,200);
+			this.textNotes.TabIndex = 1;
+			// 
+			// butDelete
+			// 
+			this.butDelete.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.butDelete.Autosize = true;
+			this.butDelete.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butDelete.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butDelete.CornerRadius = 4F;
+			this.butDelete.Image = global::OpenDental.Properties.Resources.deleteX;
+			this.butDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.butDelete.Location = new System.Drawing.Point(12,626);
+			this.butDelete.Name = "butDelete";
+			this.butDelete.Size = new System.Drawing.Size(83,24);
+			this.butDelete.TabIndex = 8;
+			this.butDelete.Text = "&Delete";
+			this.butDelete.Click += new System.EventHandler(this.butDelete_Click);
+			// 
+			// butCancel
+			// 
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.butCancel.Autosize = true;
+			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCancel.CornerRadius = 4F;
+			this.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.butCancel.Location = new System.Drawing.Point(609,626);
+			this.butCancel.Name = "butCancel";
+			this.butCancel.Size = new System.Drawing.Size(76,24);
+			this.butCancel.TabIndex = 13;
+			this.butCancel.Text = "&Cancel";
+			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
+			// 
+			// butOK
+			// 
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.butOK.Autosize = true;
+			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butOK.CornerRadius = 4F;
+			this.butOK.Location = new System.Drawing.Point(527,626);
+			this.butOK.Name = "butOK";
+			this.butOK.Size = new System.Drawing.Size(76,24);
+			this.butOK.TabIndex = 12;
+			this.butOK.Text = "&OK";
+			this.butOK.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// signatureBoxWrapper
+			// 
+			this.signatureBoxWrapper.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.signatureBoxWrapper.LabelText = null;
+			this.signatureBoxWrapper.Location = new System.Drawing.Point(114,276);
+			this.signatureBoxWrapper.Name = "signatureBoxWrapper";
+			this.signatureBoxWrapper.Size = new System.Drawing.Size(364,81);
+			this.signatureBoxWrapper.TabIndex = 194;
+			// 
+			// label12
+			// 
+			this.label12.Location = new System.Drawing.Point(-11,34);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(125,14);
+			this.label12.TabIndex = 96;
+			this.label12.Text = "Date Entry";
+			this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textDateEntry
+			// 
+			this.textDateEntry.Location = new System.Drawing.Point(114,32);
+			this.textDateEntry.Name = "textDateEntry";
+			this.textDateEntry.ReadOnly = true;
+			this.textDateEntry.Size = new System.Drawing.Size(76,20);
+			this.textDateEntry.TabIndex = 95;
+			// 
+			// label26
+			// 
+			this.label26.Location = new System.Drawing.Point(192,34);
+			this.label26.Name = "label26";
+			this.label26.Size = new System.Drawing.Size(125,18);
+			this.label26.TabIndex = 97;
+			this.label26.Text = "(for security)";
+			this.label26.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// textProcDate
+			// 
+			this.textProcDate.Location = new System.Drawing.Point(114,12);
+			this.textProcDate.Name = "textProcDate";
+			this.textProcDate.ReadOnly = true;
+			this.textProcDate.Size = new System.Drawing.Size(76,20);
+			this.textProcDate.TabIndex = 100;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(16,14);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(96,14);
+			this.label2.TabIndex = 101;
+			this.label2.Text = "Procedure Date";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// FormProcGroup
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
+			this.ClientSize = new System.Drawing.Size(697,658);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textProcDate);
+			this.Controls.Add(this.signatureBoxWrapper);
+			this.Controls.Add(this.label26);
+			this.Controls.Add(this.gridProcs);
+			this.Controls.Add(this.textDateEntry);
+			this.Controls.Add(this.buttonUseAutoNote);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.textUser);
+			this.Controls.Add(this.textNotes);
+			this.Controls.Add(this.label16);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.butDelete);
+			this.Controls.Add(this.butCancel);
+			this.Controls.Add(this.butOK);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FormProcGroup";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Procedure Group";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		#endregion
+
+		//Load function from FormProcEdit (FormProcInfo_Load)... (Single procedure - but form looks like I want)
+		private void FormProcGroup_Load(object sender, System.EventArgs e){
+			textDateEntry.Text=ProcList[0].DateEntryC.ToShortDateString();
+			FillControls();
+		}		
+
+
+		///<summary>ONLY run on startup. Fills the basic controls, except not the ones in the upper left panel which are handled in SetControls.</summary>
+		private void FillControls(){
+			//TODO: Get signaturewrapper key
+			//Security.CurUser;
+
+		}
+
+		private void buttonUseAutoNote_Click(object sender,EventArgs e) {
+			FormAutoNoteCompose FormA=new FormAutoNoteCompose();
+			FormA.ShowDialog();
+			if(FormA.DialogResult==DialogResult.OK) {
+				textNotes.AppendText(FormA.CompletedNote);
+			}
+		}
+
+		private void butDelete_Click(object sender, System.EventArgs e) {
+			return;
+		}		
+
+		private bool EntriesAreValid(){
+			return true;
+		}
+
+		///<summary>MUST call EntriesAreValid first.  Used from OK_Click and from butSetComplete_Click</summary>
+		private void SaveAndClose(){
+			try {
+				SaveSignature();
+			}
+			catch(Exception ex){
+				MessageBox.Show(Lan.g(this,"Error saving signature.")+"\r\n"+ex.Message);
+			}
+			//Save
+			//update//whether or not new, because group was created before opening this form
+
+      DialogResult=DialogResult.OK;
+		}
+
+		private void SaveSignature(){
+			return;
+		}
+
+		public static string GetSignatureKey(Sheet sheet){
+			//No need to check RemotingRole; no call to db
+			StringBuilder strBuild=new StringBuilder();
+			for(int i=0;i<sheet.SheetFields.Count;i++){
+				if(sheet.SheetFields[i].FieldValue==""){
+					continue;
+				}
+				if(sheet.SheetFields[i].FieldType==SheetFieldType.SigBox){
+					continue;
+				}
+				strBuild.Append(sheet.SheetFields[i].FieldValue);
+			}
+			return strBuild.ToString();
+		}
+
+		private void butOK_Click(object sender,System.EventArgs e) {
+			if(EntriesAreValid()) {
+				SaveAndClose();
+			}
+		}
+
+		private void butCancel_Click(object sender,System.EventArgs e) {
+			if(GroupCur.IsNew){
+				//delete
+			}
+			DialogResult=DialogResult.Cancel;
+		}
+		
+		public void FormProcEdit_FormClosing(object sender,System.EventArgs e) {
+			return;
+		}
+
+
+
+	}
+}
