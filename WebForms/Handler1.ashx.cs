390bc79947c508a1717bb4ce0692a8cda9cdca70
dennis <dennis@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-10-05 13:23:51 +0000

    Code changes related to uploading SheetDefs. Added a file
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4579 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ WebForms/Handler1.ashx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenDentBusiness;
+using System.IO;
+
+namespace WebForms {
+	/// <summary>
+	/// Dynamically generates the sheet images which are read from the database.
+	/// </summary>
+	[WebService(Namespace="http://opendental.com/")]
+	[WebServiceBinding(ConformsTo=WsiProfiles.BasicProfile1_1)]
+	public class Handler1:IHttpHandler {
+
+		private long WebSheetFieldDefNum=0;
+
+		public void ProcessRequest(HttpContext context) {
+
+			context.Response.ContentType = "image/gif";
+			ODWebServiceEntities db=new ODWebServiceEntities();
+
+			try {
+
+
+				if(context.Request["WebSheetFieldDefNum"]!=null) {
+					Int64.TryParse(context.Request["WebSheetFieldDefNum"].ToString().Trim(),out WebSheetFieldDefNum);
+				}
+
+				var sfdObj = db.webforms_sheetfielddef.Where(sfd => sfd.WebSheetFieldDefNum==WebSheetFieldDefNum).First();
+				string ImageData=sfdObj.ImageData;
+				Bitmap bitmap=PIn.Bitmap(ImageData);
+				MemoryStream ms = new MemoryStream();
+				// Save to memory using the Jpeg format
+				bitmap.Save(ms,ImageFormat.Jpeg);
+				// read to end
+				byte[] binaryImage = ms.GetBuffer();
+				bitmap.Dispose();
+				ms.Close();
+				context.Response.BinaryWrite(binaryImage);
+			}
+
+			catch(Exception ex) {
+
+				throw ex;
+
+			}
+		}
+
+		public bool IsReusable {
+			get {
+				return false;
+			}
+		}
+	}
+}
