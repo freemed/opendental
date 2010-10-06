2424d212ee24518887f10df122f488e184ed1116
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-10-06 21:05:47 +0000

    Added table TimeCardRule.  Still need to add crud and s class.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4593 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/TimeCardRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness{
+	///<summary></summary>
+	[Serializable]
+	public class TimeCardRule:TableBase{
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long TimeCardRuleNum;
+		///<summary>FK to employee.EmployeeNum. If zero, then this rule applies to all employees.</summary>
+		public long EmployeeNum;
+		///<summary>Typical example is 8.  In California, any work after the first 8 hours is overtime.</summary>
+		public TimeSpan OverHoursPerDay;
+		///<summary>Typical example is 16:00 to indicate that all time worked after 4pm for specific employees is overtime.</summary>
+		public TimeSpan AfterTimeOfDay;
+
+		///<summary></summary>
+		public TimeCardRule Clone() {
+			return (TimeCardRule)this.MemberwiseClone();
+		}
+
+	}
+}
