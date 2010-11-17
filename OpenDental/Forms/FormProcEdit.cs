0414a6e6a5d0eb6f59497e13ef1960e651af3343
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-11-17 14:54:39 +0000

    *Orion* Explanation window pops up for completed procedures now.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4895 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormProcEdit.cs
+++ OpenDental/Forms/FormProcEdit.cs
@@ -4099,7 +4099,7 @@ namespace OpenDental{
 						}
 					}
 					//Must be at least one day old by date.
-					if(OrionProcOld.Status2!=OrionStatus.C && ProcOld.DateTP.Date<MiscData.GetNowDateTime().Date &&
+					if(ProcOld.DateTP.Date<MiscData.GetNowDateTime().Date &&	
 						(OrionProcOld.Status2!=OrionStatus.TP || (OrionProcCur.Status2!=OrionStatus.C && OrionProcCur.Status2!=OrionStatus.CS && OrionProcCur.Status2!=OrionStatus.CR))) {
 						if(FormProcEditExplain.GetChanges(ProcCur,ProcOld,OrionProcCur,OrionProcOld)!=""){//Checks if any changes were made. Also sets static variable Changes.
 							FormProcEditExplain FormP=new FormProcEditExplain();
