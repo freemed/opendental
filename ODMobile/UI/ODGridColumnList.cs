6dff3e5296de05a85a0070d9a0166aa874337c67
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-11-22 04:41:11 +0000

    Deleting old mobile project.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@4913 098d0599-7f2b-0410-95a5-ad30ac85393a

--- ODMobile/UI/ODGridColumnList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenDentMobile.UI {
-	public class ODGridColumnList:List<OpenDentMobile.UI.ODGridColumn> {
-
-	}
-}
