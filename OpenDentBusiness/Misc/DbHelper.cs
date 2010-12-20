d2baea51f2e6dc7977c212b7e00f3e6e86de7af3
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-12-20 20:05:23 +0000

    DbHelper
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5049 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/Misc/DbHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenDentBusiness {
+	///<summary>This class contains methods used to generate database independent SQL.</summary>
+	public class DbHelper {
+
+		public string Limit() {
+			return "";
+		}
+
+
+	}
+}
