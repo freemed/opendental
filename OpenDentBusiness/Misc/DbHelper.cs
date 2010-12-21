f1f852efed841e70cfb5a7f2f5df98d5350a93ae
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-12-21 21:09:33 +0000

    Commenting out unused method. (LimitHaving for GROUP BY.) Added method for Concat.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5072 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Misc/DbHelper.cs
+++ OpenDentBusiness/Misc/DbHelper.cs
@@ -27,6 +27,7 @@ namespace OpenDentBusiness {
 			}
 		}
 
+		/*
 		///<summary>Use when there is a GROUP BY clause in the query. Uses HAVING RowNum... for Oracle. Needs testing. May need to use subquery instead (like with ORDER BY).</summary>
 		public static string LimitHaving(int n) {
 			if(DataConnection.DBtype==DatabaseType.Oracle) {
@@ -35,7 +36,7 @@ namespace OpenDentBusiness {
 			else {
 				return "LIMIT " + n;
 			}
-		}
+		}*/
 
 		///<summary>Use when there is an ORDER BY clause in the query. Uses RowNum... for Oracle.</summary>
 		public static string LimitOrderBy(string query,int n) {
@@ -45,6 +46,12 @@ namespace OpenDentBusiness {
 			else {
 				return query + " LIMIT " + n;
 			}
+		}
+
+		/// <summary>If passing in a literal, surround with single quotes first.</summary>
+		public static string Concat(params string[] values) {
+
+			return "";
 		}
 
 
