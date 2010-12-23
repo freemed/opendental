20be79dc87760b7cfb54fac1167653de9cab431e
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-12-23 17:31:30 +0000

    crud
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5111 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Db/PIn.cs
+++ OpenDentBusiness/Db/PIn.cs
@@ -10,8 +10,24 @@ namespace OpenDentBusiness{
 	
 	/*=========================================================================================
 	=================================== class PIn ===========================================*/
-	///<summary>Converts strings coming in from the database into the appropriate type. "P" is short for Parameter because this class was written specifically to replace parameters in the mysql queries.</summary>
+	///<summary>"P" stands for Parameter.  Converts strings coming in from user input into the appropriate type.</summary>
 	public class PIn{
+		///<summary></summary>
+		public static Bitmap Bitmap(string myString) {
+			if(myString==null || myString.Length<0x32) {//Bitmaps require a minimum length for header info.
+				return null;
+			}
+			try {
+				byte[] rawData=Convert.FromBase64String(myString);
+				MemoryStream stream=new MemoryStream(rawData);
+				System.Drawing.Bitmap image=new System.Drawing.Bitmap(stream);
+				return image;
+			}
+			catch {
+				return null;
+			}
+		}
+
 		///<summary></summary>
 		public static bool Bool (string myString){
 			return myString=="1";
@@ -24,6 +40,21 @@ namespace OpenDentBusiness{
 			}
 			else{
 				return System.Convert.ToByte(myString);
+			}
+		}
+
+		///<summary>Some versions of MySQL return a GROUP_CONCAT as a string, and other versions return it as a byte array.  This method handles either way, making it work smoothly with different versions.</summary>
+		public static string ByteArray(object obj) {
+			if(obj.GetType()==typeof(Byte[])) {
+				Byte[] bytes=(Byte[])obj;
+				StringBuilder strbuild=new StringBuilder();
+				for(int i=0;i<bytes.Length;i++) {
+					strbuild.Append((char)bytes[i]);
+				}
+				return strbuild.ToString();
+			}
+			else {//string
+				return obj.ToString();
 			}
 		}
 
@@ -53,35 +84,7 @@ namespace OpenDentBusiness{
 				return DateTime.MinValue;
 			}
 		}
-
-		public static TimeSpan TimeSpan(string myString) {
-			if (string.IsNullOrEmpty(myString)) {
-				return System.TimeSpan.MinValue;
-			}
-			try {
-				return (System.TimeSpan.Parse(myString));
-			}
-			catch {
-				return System.TimeSpan.MinValue;
-			}
-		}
-
-		///<summary>If blank or invalid, returns 0. Otherwise, parses.</summary>
-		public static double Double (string myString){
-			if(myString==""){
-				return 0;
-			}
-			else{
-				try{
-					return System.Convert.ToDouble(myString);
-				}
-				catch{
-					//MessageBox.Show("Error converting "+myString+" to double");
-					return 0;
-				}
-			}
-		}
-
+		
 		///<summary>If blank or invalid, returns 0. Otherwise, parses.</summary>
 		public static decimal Decimal (string myString){
 			if(myString==""){
@@ -98,13 +101,19 @@ namespace OpenDentBusiness{
 			}
 		}
 
-		///<summary></summary>
-		public static long Long (string myString){
-			if(myString==""){
+		///<summary>If blank or invalid, returns 0. Otherwise, parses.</summary>
+		public static double Double(string myString) {
+			if(myString=="") {
 				return 0;
 			}
-			else{
-				return System.Convert.ToInt64(myString);
+			else {
+				try {
+					return System.Convert.ToDouble(myString);
+				}
+				catch {
+					//MessageBox.Show("Error converting "+myString+" to double");
+					return 0;
+				}
 			}
 		}
 
@@ -119,6 +128,24 @@ namespace OpenDentBusiness{
 		}
 
 		///<summary></summary>
+		public static float Float(string myString) {
+			if(myString=="") {
+				return 0;
+			}
+			return System.Convert.ToSingle(myString);
+		}
+
+		///<summary></summary>
+		public static long Long (string myString){
+			if(myString==""){
+				return 0;
+			}
+			else{
+				return System.Convert.ToInt64(myString);
+			}
+		}
+
+		///<summary></summary>
 		public static short Short(string myString) {
 			if(myString == "") {
 				return 0;
@@ -128,64 +155,8 @@ namespace OpenDentBusiness{
 			}
 		}
 
-		///<summary></summary>
-		public static float Float(string myString){
-			if(myString==""){
-				return 0;
-			}
-			//try{
-				return System.Convert.ToSingle(myString);
-			//}
-			//catch{
-			//	return 0;
-			//}
-		}
-
-		///<summary>Currently does nothing.</summary>
-		public static string String (string myString){
-			return myString;
-		}
-		
-		//<summary></summary>
-		//public static string PTime (string myTime){
-		//	return DateTime.Parse(myTime).ToString("HH:mm:ss");
-		//}
-
-		//<summary></summary>
-		//public static string PBytes (byte[] myBytes){
-		//	return Convert.ToBase64String(myBytes);
-		//}
-
-		/*
-		///<summary></summary>
-		public static Bitmap PBitmap(string myString){
-			if(myString==""){
-				return null;
-			}
-			byte[] rawData=Convert.FromBase64String(myString);
-			MemoryStream stream=new MemoryStream(rawData);
-			Bitmap image=new Bitmap(stream);
-			return image;
-		}*/
-
-		///<summary></summary>
-		public static Bitmap Bitmap(string myString) {
-			if(myString==null || myString.Length<0x32) {//Bitmaps require a minimum length for header info.
-				return null;
-			}
-			try {
-				byte[] rawData=Convert.FromBase64String(myString);
-				MemoryStream stream=new MemoryStream(rawData);
-				System.Drawing.Bitmap image=new System.Drawing.Bitmap(stream);
-				return image;
-			}
-			catch {
-				return null;
-			}
-		}
-
 		///<summary>Saves the string representation of a sound into a .wav file.  The timing of this is different than with the other "P" functions, and is only used by the export button in FormSigElementDefEdit</summary>
-		public static void Sound(string sound, string filename) {
+		public static void Sound(string sound,string filename) {
 			if(!filename.EndsWith(".wav")) {
 				throw new ApplicationException("Filename must end with .wav");
 			}
@@ -193,21 +164,28 @@ namespace OpenDentBusiness{
 			FileStream stream=new FileStream(filename,FileMode.Create,FileAccess.Write);
 			stream.Write(rawData,0,rawData.Length);
 		}
+		
+		///<summary>Currently does nothing.</summary>
+		public static string String (string myString){
+			return myString;
+		}
 
-		///<summary>Some versions of MySQL return a GROUP_CONCAT as a string, and other versions return it as a byte array.  This method handles either way, making it work smoothly with different versions.</summary>
-		public static string ByteArray(object obj){
-			if(obj.GetType()==typeof(Byte[])){
-				Byte[] bytes=(Byte[])obj;
-				StringBuilder strbuild=new StringBuilder();
-				for(int i=0;i<bytes.Length;i++){
-					strbuild.Append((char)bytes[i]);
-				}
-				return strbuild.ToString();
+		public static TimeSpan TimeSpan(string myString) {
+			if (string.IsNullOrEmpty(myString)) {
+				return System.TimeSpan.MinValue;
 			}
-			else{//string
-				return obj.ToString();
+			try {
+				return (System.TimeSpan.Parse(myString));
+			}
+			catch {
+				return System.TimeSpan.MinValue;
 			}
 		}
+		
+	
+		
+
+		
 
 
 	}
---
+++ OpenDentBusiness/Db/PoIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenDentBusiness {
+	///<summary>Parameter object coming in from database.</summary>
+	public class PoIn {
+		///<summary></summary>
+		public static bool Bool(object obj) {
+			try {
+				return ((int)obj)==1;
+			}
+			catch(Exception e) {
+				Type t=obj.GetType();
+			}
+			return false;
+		}
+
+		///<summary></summary>
+		public static byte Byte(object obj) {
+			return (byte)obj;
+		}
+
+		///<summary></summary>
+		public static long Long(object obj) {
+			return (long)obj;
+		}
+
+		///<summary></summary>
+		public static string String(object obj) {
+			//This strategy was tested to take zero ticks on a null.
+			return obj.ToString();
+			//This strategy also takes zero ticks on a null, but does not seem to have any advantage.
+			//bool isNull=(obj.GetType()==typeof(System.DBNull));
+			//if(isNull) {
+			//	return "";
+			//}
+			//return (string)obj;
+		}
+
+	
+
+	}
+}
