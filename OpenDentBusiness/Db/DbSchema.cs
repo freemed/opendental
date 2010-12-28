797600821df42a54897977f7d4cc6d2e6f83ecc5
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2010-12-28 21:53:51 +0000

    DbSchema.OracleValidateDateTStampTriggerHelper added
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5170 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Db/DbSchema.cs
+++ OpenDentBusiness/Db/DbSchema.cs
@@ -15,19 +15,7 @@ namespace OpenDentBusiness {
 			}
 			else {//oracle
 				command = "ALTER TABLE "+tableName+" ADD "+col.ColumnName+" "+GetOracleType(col)+";";
-				if(col.DataType==OdDbType.DateTimeStamp) {
-					Db.NonQ(command);//add column
-					command ="Select * from user_tab_columns where table_name='"+tableName+"';";
-					DataTable tempTable = Db.GetTable(command);//get list of columns
-					command = "CREATE OR REPLACE TRIGGER "+tableName+"_timestamp BEFORE UPDATE ON "+tableName+"FOR EACH ROW"
-										+"BEGIN";
-					for(int i=0;i<tempTable.Rows.Count;i++) {//iterate through each column to see if it was changed
-						command+="	IF :OLD."+tempTable.Rows[i].ToString()+" <> :NEW."+tempTable.Rows[i].ToString()+" THEN"
-										+"	:NEW.DateTStamp := SYSDATE;"
-										+"	END IF;";
-					}
-						command+="END "+tableName+"_timespan;";
-				}
+				OracleValidateDateTStampTriggerHelper(tableName);
 			}
 			Db.NonQ(command);
 		}
@@ -42,9 +30,7 @@ namespace OpenDentBusiness {
 				//query to find the column names
 
 				//Workaround, must create a new table and copy data to add new column anywhere except the end.
-				if(col.DataType==OdDbType.DateTimeStamp) {
-					//Add code for building trigger here.
-				}
+				OracleValidateDateTStampTriggerHelper(tableName);
 			}
 			Db.NonQ(command);
 		}
@@ -57,9 +43,7 @@ namespace OpenDentBusiness {
 			}
 			else {//oracle
 				//Workaround, must create a new table and copy data to add new column anywhere except the end.
-				if(col.DataType==OdDbType.DateTimeStamp) {
-					//Add code for building trigger here.
-				}
+				OracleValidateDateTStampTriggerHelper(tableName);
 			}
 			Db.NonQ(command);
 		}
@@ -72,6 +56,7 @@ namespace OpenDentBusiness {
 			}
 			else {//oracle
 				command= "ALTER TABLE "+tableName+" DROP COLUMN "+columnName+" ;";
+				OracleValidateDateTStampTriggerHelper(tableName);
 //todo: check for existing trigger or index
 			}
 			Db.NonQ(command);
@@ -131,25 +116,18 @@ namespace OpenDentBusiness {
 			switch(col.DataType) {
 				case OdDbType.Bool:
 					return "tinyint";
-					break;
 				case OdDbType.Byte:
 					return "tinyint unsigned";
-					break;
 				case OdDbType.Currency:
 					return "double";
-					break;
 				case OdDbType.Date:
 					return "date";
-					break;
 				case OdDbType.DateTime:
 					return "datetime";
-					break;
 				case OdDbType.DateTimeStamp:
 					return "timestamp";
-					break;
 				case OdDbType.Float:
 					return "float";
-					break;
 				case OdDbType.Int:
 					if(col.IntUseSmallInt){
 						return "smallint";
@@ -157,10 +135,8 @@ namespace OpenDentBusiness {
 					else{
 						return "int";
 					}
-					break;
 				case OdDbType.Long:
 					return "bigint";
-					break;
 				case OdDbType.Text:
 					if(col.TextSize==TextSizeMySqlOracle.Small || col.TextSize==TextSizeMySqlOracle.Medium) {
 						return "text";
@@ -168,16 +144,12 @@ namespace OpenDentBusiness {
 					else {//textSize==TextSizeMySqlOracle.large
 						return "mediumtext";
 					}
-					break;
 				case OdDbType.TimeOfDay:
 					return "time";
-					break;
 				case OdDbType.TimeSpan:
 					return "time";
-					break;
 				case OdDbType.VarChar255:
 					return "varchar(255)";
-					break;
 			}
 			return "";
 		}
@@ -186,32 +158,23 @@ namespace OpenDentBusiness {
 			switch(col.DataType) {
 				case OdDbType.Bool:
 					return "NUMBER(3)";
-					break;
 				case OdDbType.Byte:
 					return "NUMBER(3)";
-					break;
 				case OdDbType.Currency:
 					return "NUMBER(38,8)";
-					break;
 				case OdDbType.Date:
 					return "date";
-					break;
 				case OdDbType.DateTime:
 					return "date";
-					break;
 				case OdDbType.DateTimeStamp:
 					//also requires trigger, trigger code is automatically created above.
 					return "date";
-					break;
 				case OdDbType.Float:
 					return "number(38,8)";
-					break;
 				case OdDbType.Int:
 					return "NUMBER(11)";
-					break;
 				case OdDbType.Long:
 					return "NUMBER(20)";
-					break;
 				case OdDbType.Text:
 					if(col.TextSize==TextSizeMySqlOracle.Small) {
 						return "varchar2(4000)";
@@ -219,22 +182,54 @@ namespace OpenDentBusiness {
 					else {//textSize == medium or large
 						return "clob";
 					}
-					break;
 				case OdDbType.TimeOfDay:
 					return "date";
-					break;
 				case OdDbType.TimeSpan:
 					return "varchar2(255)";
-					break;
 				case OdDbType.VarChar255:
 					return "varchar2(255)";
-					break;
 			}
 			return "";
 		}
 
+		/// <summary>validates any table's dateTStamp triggers for oracle.</summary>
+		private static void OracleValidateDateTStampTriggerHelper(string tableName){
+			bool triggerNeeded = false;
+			bool needDropTrigger = false;
+			string command ="Select TABLE_NAME, COLUMN_NAME from user_tab_columns where table_name='"+tableName.ToUpper()+"';";
+			DataTable tempTable = Db.GetTable(command);//get list of columns
+			for(int i=0;i<tempTable.Rows.Count;i++){
+				if(tempTable.Rows[i][1].ToString()=="DateTStamp"){
+					triggerNeeded=true;
+				}
+			}
+			if(triggerNeeded) {
+				command = "CREATE OR REPLACE TRIGGER "+tableName+"_timestamp BEFORE UPDATE ON "+tableName+"FOR EACH ROW "
+										+"BEGIN";
+				for(int i=0;i<tempTable.Rows.Count;i++) {//iterate through each column to see if it was changed
+					command+="	IF :OLD."+tempTable.Rows[i][1].ToString()+" <> :NEW."+tempTable.Rows[i][1].ToString()+" THEN"
+										+"	:NEW.DateTStamp := SYSDATE;"
+										+"	END IF;";
+				}
+				command+="END "+tableName+"_timespan;";
+			}
+			else {//table needs no DateTStamp trigger
+				command = "Select TRIGGER_NAME from user_triggers WHERE tablename = '"+tableName.ToUpper()+"';";
+				DataTable tempTriggerTable = Db.GetTable(command);
+				for(int i=0;i<tempTriggerTable.Rows.Count;i++) {//check for timestamp triggers before trying to delete
+					if(tempTriggerTable.Rows[i][0].ToString().Contains("_TIMESTAMP")) {
+						needDropTrigger = true;
+					}
+				}
+				if(needDropTrigger) {//Delete timestamp triggers if they exist
+					command = "DROP TRIGGER "+tableName.ToUpper()+"_TIMESTAMP WHERE TABLE_NAME = '"+tableName.ToUpper()+"';";
+					Db.NonQ(command);
+				}
+			}
+		}
+
 		/// <summary>Fills new table by selecting each column before index from old table, creates new column at index, continues to select columns from old table, drops old table, renames new table.</summary>
-		private static void oracleAddAtIndexHelper(string tableName,int indexOfNewColumn) {
+		private static void OracleAddAtIndexHelper(string tableName,int indexOfNewColumn) {
 			//Oracle does not support adding columns to tables anywhere other than the end of the table. Workaround is to create a new table, fill it with data, drop the old table, and rename the new table.
 			//MySql does not need to use this function ever.
 		}
