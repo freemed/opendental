caae2363c0ce84b1d56104703d10819e248d3809
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-01-04 19:38:15 +0000

    Schema Table
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5257 098d0599-7f2b-0410-95a5-ad30ac85393a

--- UnitTests/SchemaTable.cs
+++ UnitTests/SchemaTable.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenDentBusiness;
 
 namespace UnitTests {
 	class SchemaTable {
 		//This defines a test table from which the schema insert table will be generated.
+		public static string TableName;
+		public static List<DbSchemaCol> Cols;
+
+		public SchemaTable(string tabName, List<DbSchemaCol> cols) {
+			TableName = tabName;
+			Cols = cols;
+		}
+
+		public SchemaTable() {
+			TableName = "tempcore";
+			Cols.Add(new DbSchemaCol("colOneInt",OdDbType.Int));
+			Cols.Add(new DbSchemaCol("colTwoText",OdDbType.Text,TextSizeMySqlOracle.Medium));
+			Cols.Add(new DbSchemaCol("colThrDateT",OdDbType.DateTime));
+		}
 
 	}
 }
