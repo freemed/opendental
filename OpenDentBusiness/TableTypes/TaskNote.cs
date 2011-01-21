f89d50ae1040352d6992d336629c53b9589dfdd7
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-01-21 17:05:35 +0000

    New table. Need to be approved, added to crud, S class added, update script generated & reviewed...
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5417 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/TaskNote.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace OpenDentBusiness {
+
+	///<summary>A tasknote is a note that may be added to a task. Many notes may be attached to a task. A user may only edit their own tasknotes within a task.</summary>
+	[Serializable]
+	public class TaskNote:TableBase {
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long TaskNoteNum;
+		///<summary>FK to task. The task this tasknote is attached to.</summary>
+		public long TaskNum;
+		///<summary>FK to user. The user who created this tasknote.</summary>
+		public long UserNum;
+		///<summary>Date and time the note was created (editable).</summary>
+		[CrudColumn(SpecialType=CrudSpecialColType.DateTEntryEditable)]
+		public DateTime DateTimeNote;
+		///<summary>Note. Text that the user wishes to show on the task.</summary>
+		public string Note;
+
+		///<summary></summary>
+		public TaskNote Copy() {
+			return (TaskNote)MemberwiseClone();
+		}
+
+
+
+	}
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
