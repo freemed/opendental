cde8f3bc585ab143bc19dcb9bbdf84e2524f5659
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-01-31 16:56:42 +0000

    Adding table creditcard.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5500 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/Data Interface/CreditCards.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace OpenDentBusiness{
+	///<summary></summary>
+	public class CreditCards{
+		#region CachePattern
+		//This region can be eliminated if this is not a table type with cached data.
+		//If leaving this region in place, be sure to add RefreshCache and FillCache 
+		//to the Cache.cs file with all the other Cache types.
+
+		///<summary>A list of all CreditCards.</summary>
+		private static List<CreditCard> listt;
+
+		///<summary>A list of all CreditCards.</summary>
+		public static List<CreditCard> Listt{
+			get {
+				if(listt==null) {
+					RefreshCache();
+				}
+				return listt;
+			}
+			set {
+				listt=value;
+			}
+		}
+
+		///<summary></summary>
+		public static DataTable RefreshCache(){
+			//No need to check RemotingRole; Calls GetTableRemotelyIfNeeded().
+			string command="SELECT * FROM creditcard ORDER BY ItemOrder";//stub query probably needs to be changed
+			DataTable table=Cache.GetTableRemotelyIfNeeded(MethodBase.GetCurrentMethod(),command);
+			table.TableName="CreditCard";
+			FillCache(table);
+			return table;
+		}
+
+		///<summary></summary>
+		public static void FillCache(DataTable table){
+			//No need to check RemotingRole; no call to db.
+			listt=Crud.CreditCardCrud.TableToList(table);
+		}
+		#endregion
+
+		///<summary></summary>
+		public static List<CreditCard> Refresh(long patNum){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				return Meth.GetObject<List<CreditCard>>(MethodBase.GetCurrentMethod(),patNum);
+			}
+			string command="SELECT * FROM creditcard WHERE PatNum = "+POut.Long(patNum);
+			return Crud.CreditCardCrud.SelectMany(command);
+		}
+
+		///<summary>Gets one CreditCard from the db.</summary>
+		public static CreditCard GetOne(long creditCardNum){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				return Meth.GetObject<CreditCard>(MethodBase.GetCurrentMethod(),creditCardNum);
+			}
+			return Crud.CreditCardCrud.SelectOne(creditCardNum);
+		}
+
+		///<summary></summary>
+		public static long Insert(CreditCard creditCard){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				creditCard.CreditCardNum=Meth.GetLong(MethodBase.GetCurrentMethod(),creditCard);
+				return creditCard.CreditCardNum;
+			}
+			return Crud.CreditCardCrud.Insert(creditCard);
+		}
+
+		///<summary></summary>
+		public static void Update(CreditCard creditCard){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				Meth.GetVoid(MethodBase.GetCurrentMethod(),creditCard);
+				return;
+			}
+			Crud.CreditCardCrud.Update(creditCard);
+		}
+
+		///<summary></summary>
+		public static void Delete(long creditCardNum) {
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
+				Meth.GetVoid(MethodBase.GetCurrentMethod(),creditCardNum);
+				return;
+			}
+			string command= "DELETE FROM creditcard WHERE CreditCardNum = "+POut.Long(creditCardNum);
+			Db.NonQ(command);
+		}
+
+
+
+
+	}
+}
