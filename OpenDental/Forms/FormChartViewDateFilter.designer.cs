777ec20447425cec92555e93fca44ff7a4a88923
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-02-24 18:10:38 +0000

    Added FormChartViewDateFilter
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5623 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormChartViewDateFilter.designer.cs
@@ -0,0 +1,124 @@
+namespace OpenDental{
+	partial class FormChartViewDateFilter {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.butOK = new OpenDental.UI.Button();
+			this.butCancel = new OpenDental.UI.Button();
+			this.validDate1 = new OpenDental.ValidDate();
+			this.textDateStop = new OpenDental.ValidDate();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// butOK
+			// 
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butOK.Autosize = true;
+			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butOK.CornerRadius = 4F;
+			this.butOK.Location = new System.Drawing.Point(142,224);
+			this.butOK.Name = "butOK";
+			this.butOK.Size = new System.Drawing.Size(75,24);
+			this.butOK.TabIndex = 3;
+			this.butOK.Text = "&OK";
+			this.butOK.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// butCancel
+			// 
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butCancel.Autosize = true;
+			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCancel.CornerRadius = 4F;
+			this.butCancel.Location = new System.Drawing.Point(223,224);
+			this.butCancel.Name = "butCancel";
+			this.butCancel.Size = new System.Drawing.Size(75,24);
+			this.butCancel.TabIndex = 2;
+			this.butCancel.Text = "&Cancel";
+			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
+			// 
+			// validDate1
+			// 
+			this.validDate1.Location = new System.Drawing.Point(105,141);
+			this.validDate1.Name = "validDate1";
+			this.validDate1.Size = new System.Drawing.Size(91,20);
+			this.validDate1.TabIndex = 4;
+			// 
+			// textDateStop
+			// 
+			this.textDateStop.Location = new System.Drawing.Point(105,167);
+			this.textDateStop.Name = "textDateStop";
+			this.textDateStop.Size = new System.Drawing.Size(91,20);
+			this.textDateStop.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(19,142);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(84,16);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Start Date";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(19,168);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(84,16);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Stop Date";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// FormChartViewDateFilter
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(316,262);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textDateStop);
+			this.Controls.Add(this.validDate1);
+			this.Controls.Add(this.butOK);
+			this.Controls.Add(this.butCancel);
+			this.Name = "FormChartViewDateFilter";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Progress Notes Date Range Filter";
+			this.Load += new System.EventHandler(this.FormChartViewDateFilter_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private OpenDental.UI.Button butOK;
+		private OpenDental.UI.Button butCancel;
+		private ValidDate validDate1;
+		private ValidDate textDateStop;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
