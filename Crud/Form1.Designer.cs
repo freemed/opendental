cfe6e2d5e951e7043fa2d30d33100d47653986df
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-03-01 19:15:40 +0000

    Crud version
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5657 098d0599-7f2b-0410-95a5-ad30ac85393a

--- Crud/Form1.Designer.cs
+++ Crud/Form1.Designer.cs
@@ -69,7 +69,7 @@
 			this.textDb.Name = "textDb";
 			this.textDb.Size = new System.Drawing.Size(127,20);
 			this.textDb.TabIndex = 5;
-			this.textDb.Text = "development78";
+			this.textDb.Text = "development79";
 			// 
 			// label3
 			// 
