18b9be72b8bd8f0f08b9957e677f1dbc3ea6e2e8
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-03-10 15:18:36 +0000

    Canadian Request for Outstanding Transaction tests 1-3 written, but some parts commented out for now because the Canadian test server is not returning the responses expected, even though our output exactly matches that expected. Moving on to predeterminations for now.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5699 098d0599-7f2b-0410-95a5-ad30ac85393a

--- TestCanada/OutstandingTrans.cs
+++ TestCanada/OutstandingTrans.cs
@@ -17,37 +17,55 @@ namespace TestCanada {
 
 		public static string RunOne() {
 			List<Etrans> etransRequests;
-			Claim claim=Claims.GetClaim(ClaimTC.ClaimNums[0]);
+			//Claim claim=Claims.GetClaim(ClaimTC.ClaimNums[0]);
 			Carrier carrier=Carriers.GetCanadian("666666");
 			carrier.CanadianEncryptionMethod=1;
 			string retval=Run(1,carrier,out etransRequests);
-			//EOB
-			etransRequests[0].PatNum=claim.PatNum;
-			etransRequests[0].PlanNum=claim.PlanNum;
-			etransRequests[0].InsSubNum=claim.InsSubNum;
-			string message=EtransMessageTexts.GetMessageText(etransRequests[0].EtransMessageTextNum);
-			FormCCDPrint FormP=new FormCCDPrint(etransRequests[0],message);//Print the form. 
-			FormP.Print();
-			//Email
-			etransRequests[1].PatNum=claim.PatNum;
-			etransRequests[1].PlanNum=claim.PlanNum;
-			etransRequests[1].InsSubNum=claim.InsSubNum;
-			message=EtransMessageTexts.GetMessageText(etransRequests[1].EtransMessageTextNum);
-			FormP=new FormCCDPrint(etransRequests[1],message);//Print the form. 
-			FormP.Print();
+			////EOB
+			//etransRequests[0].PatNum=claim.PatNum;
+			//etransRequests[0].PlanNum=claim.PlanNum;
+			//etransRequests[0].InsSubNum=claim.InsSubNum;
+			//string message=EtransMessageTexts.GetMessageText(etransRequests[0].EtransMessageTextNum);
+			//FormCCDPrint FormP=new FormCCDPrint(etransRequests[0],message);//Print the form. 
+			//FormP.Print();
+			////Email
+			//etransRequests[1].PatNum=claim.PatNum;
+			//etransRequests[1].PlanNum=claim.PlanNum;
+			//etransRequests[1].InsSubNum=claim.InsSubNum;
+			//message=EtransMessageTexts.GetMessageText(etransRequests[1].EtransMessageTextNum);
+			//FormP=new FormCCDPrint(etransRequests[1],message);//Print the form. 
+			//FormP.Print();
 			return retval;
 		}
 
 		public static string RunTwo() {
-			//Carrier carrier=Carriers.GetCanadian("888888");
-			//return Run(2,carrier);
-			return "";
+			List<Etrans> etransRequests;
+			//Claim claim=Claims.GetClaim(ClaimTC.ClaimNums[6]);
+			Carrier carrier=Carriers.GetCanadian("888888");
+			string retval=Run(2,carrier,out etransRequests);
+			////EOB
+			//etransRequests[0].PatNum=claim.PatNum;
+			//etransRequests[0].PlanNum=claim.PlanNum;
+			//etransRequests[0].InsSubNum=claim.InsSubNum;
+			//string message=EtransMessageTexts.GetMessageText(etransRequests[0].EtransMessageTextNum);
+			//FormCCDPrint FormP=new FormCCDPrint(etransRequests[0],message);//Print the form. 
+			//FormP.Print();
+			return retval;
 		}
 
 		public static string RunThree() {
-			//Carrier carrier=Carriers.GetCanadian("777777");
-			//return Run(3,carrier);
-			return "";
+			List<Etrans> etransRequests;
+			//Claim claim=Claims.GetClaim(ClaimTC.ClaimNums[]);
+			Carrier carrier=Carriers.GetCanadian("777777");
+			string retval=Run(3,carrier,out etransRequests);
+			////EOB
+			//etransRequests[0].PatNum=claim.PatNum;
+			//etransRequests[0].PlanNum=claim.PlanNum;
+			//etransRequests[0].InsSubNum=claim.InsSubNum;
+			//string message=EtransMessageTexts.GetMessageText(etransRequests[0].EtransMessageTextNum);
+			//FormCCDPrint FormP=new FormCCDPrint(etransRequests[0],message);//Print the form. 
+			//FormP.Print();
+			return retval;
 		}
 
 	}
