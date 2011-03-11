c96ad2fb42109628e6e96f53b00b12b45f8346fb
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-03-11 20:59:31 +0000

    label an enum
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5719 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/SheetFramework/SheetEnums.cs
+++ OpenDentBusiness/SheetFramework/SheetEnums.cs
@@ -51,27 +51,27 @@ namespace OpenDentBusiness {
 	}
 
 	public enum SheetFieldType {
-		///<Summary>Pulled from the database to be printed on the sheet.  Or also possibly just generated at runtime even though not pulled from the database.   User still allowed to change the output text as they are filling out the sheet so that it can different from what was initially generated.</Summary>
+		///<Summary>0-Pulled from the database to be printed on the sheet.  Or also possibly just generated at runtime even though not pulled from the database.   User still allowed to change the output text as they are filling out the sheet so that it can different from what was initially generated.</Summary>
 		OutputText,
-		///<Summary>A blank box that the user is supposed to fill in.</Summary>
+		///<Summary>1-A blank box that the user is supposed to fill in.</Summary>
 		InputField,
-		///<Summary>This is text that is defined as part of the sheet and will never change from sheet to sheet.  </Summary>
+		///<Summary>2-This is text that is defined as part of the sheet and will never change from sheet to sheet.  </Summary>
 		StaticText,
-		///<summary>Stores a parameter other than the PatNum.  Not meant to be seen on the sheet.  Only used for SheetField, not SheetFieldDef.</summary>
+		///<summary>3-Stores a parameter other than the PatNum.  Not meant to be seen on the sheet.  Only used for SheetField, not SheetFieldDef.</summary>
 		Parameter,
-		///<Summary>Any image of any size, typically a background image for a form.</Summary>
+		///<Summary>4-Any image of any size, typically a background image for a form.</Summary>
 		Image,
-		///<summary>One sequence of dots that makes a line.  Continuous without any breaks.  Each time the pen is picked up, it creates a new field row in the database.</summary>
+		///<summary>5-One sequence of dots that makes a line.  Continuous without any breaks.  Each time the pen is picked up, it creates a new field row in the database.</summary>
 		Drawing,
-		///<Summary>A simple line drawn from x,y to x+width,y+height.  So for these types, we must allow width and height to be negative or zero.</Summary>
+		///<Summary>6-A simple line drawn from x,y to x+width,y+height.  So for these types, we must allow width and height to be negative or zero.</Summary>
 		Line,
-		///<Summary>A simple rectangle outline.</Summary>
+		///<Summary>7-A simple rectangle outline.</Summary>
 		Rectangle,
-		///<summary>A clickable area on the screen.  It's a form of input, so treated similarly to an InputField.  The X will go from corner to corner of the rectangle specified.  It can also behave like a radio button</summary>
+		///<summary>8-A clickable area on the screen.  It's a form of input, so treated similarly to an InputField.  The X will go from corner to corner of the rectangle specified.  It can also behave like a radio button</summary>
 		CheckBox,
-		///<summary>A signature box, either Topaz pad or directly on the screen with stylus/mouse.  The signature is encrypted based an a hash of all other field values in the entire sheet, excluding other SigBoxes.  The order is critical.</summary>
+		///<summary>9-A signature box, either Topaz pad or directly on the screen with stylus/mouse.  The signature is encrypted based an a hash of all other field values in the entire sheet, excluding other SigBoxes.  The order is critical.</summary>
 		SigBox,
-		///<Summary>An image specific to one patient.</Summary>
+		///<Summary>10-An image specific to one patient.</Summary>
 		PatImage
 		//<summary></summary>
 		//RadioButton
