b6c92e04f97d462ea1c3cb0d72518b300e68744c
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-03-17 20:43:24 +0000

    Vitalsign.cs
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5748 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/TableTypes/Vitalsign.cs
+++ OpenDentBusiness/TableTypes/Vitalsign.cs
@@ -1,9 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace OpenDentBusiness.TableTypes {
-	class Vitalsign {
+namespace OpenDentBusiness {
+	///<summary>For EHR module, one dated vital sign entry.</summary>
+	[Serializable]
+	public class Vitalsign:TableBase {
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long VitalSignNum;
+		///<summary>FK to patient.PatNum.</summary>
+		public long PatNum;
+		///<summary>Height of patient in inches. Fractions might be needed some day.</summary>
+		public float Height;
+		///<summary>Lbs.</summary>
+		public int Weight;
+		///<summary></summary>
+		public int BpSystolic;
+		///<summary></summary>
+		public int BpDiastolic;
+		///<summary>The date that the vitalsigns were taken.</summary>
+		public DateTime DateTaken;
+
+		///<summary></summary>
+		public Vitalsign Copy() {
+			return (Vitalsign)MemberwiseClone();
+		}
+
 	}
 }
