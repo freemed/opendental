72e8c43a1350ee8bded271e8be3d6c8676fd13b8
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-04-13 15:06:57 +0000

    TableType files for DrugManufacturer, DrugUnit, VaccineDef, VaccinePat
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5908 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/VaccineDef.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness {
+	///<summary></summary>
+	[Serializable]
+	public class VaccineDef:TableBase {
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long VaccineDefNum;
+		///<summary></summary>
+		public string CVXCode;
+		///<summary>Name of vaccine.</summary>
+		public string VaccineName;
+		///<summary>FK to drugmanufacturer.DrugManufacturerNum.</summary>
+		public long DrugManufacturerNum;
+
+		///<summary></summary>
+		public VaccineDef Copy() {
+			return (VaccineDef)this.MemberwiseClone();
+		}
+
+	}
+}
---
+++ OpenDentBusiness/TableTypes/VaccinePat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness {
+	///<summary></summary>
+	[Serializable]
+	public class VaccinePat:TableBase {
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long VaccinePatNum;
+		///<summary>FK to vaccinedef.VaccineDefNum.</summary>
+		public long VaccineDefNum;
+		///<summary></summary>
+		public DateTime DateTimeStart;
+		///<summary></summary>
+		public DateTime DateTimeEnd;
+		///<summary>Size of the dose of the vaccine.  999 indicates unknown.</summary>
+		public float AdministeredAmt;
+		///<summary>FK to drugunit.DrugUnitNum. Unit of measurement of the AdministeredAmt.  0 represents null.</summary>
+		public long DrugUnitNum;
+		///<summary></summary>
+		public string LotNumber;
+
+		///<summary></summary>
+		public VaccinePat Copy() {
+			return (VaccinePat)this.MemberwiseClone();
+		}
+
+	}
+}
