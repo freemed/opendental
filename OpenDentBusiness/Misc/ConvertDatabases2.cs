6bee1411cac232fa63b009369f9df00fd1a5df97
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-04-18 13:50:35 +0000

    Changing Assembly versions. Head can now be worked in.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5942 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Misc/ConvertDatabases2.cs
+++ OpenDentBusiness/Misc/ConvertDatabases2.cs
@@ -23,7 +23,7 @@ namespace OpenDentBusiness {
 	//The other file was simply getting too big.  It was bogging down VS speed.
 	///<summary></summary>
 	public partial class ConvertDatabases {
-		public static System.Version LatestVersion=new Version("7.9.0.0");//This value must be changed when a new conversion is to be triggered.
+		public static System.Version LatestVersion=new Version("8.0.0.0");//This value must be changed when a new conversion is to be triggered.
 
 		private static void To6_2_9() {
 			if(FromVersion<new Version("6.2.9.0")) {
@@ -3687,11 +3687,11 @@ VALUES('MercuryDE','"+POut.String(@"C:\MercuryDE\Temp\")+@"','0','','1','','','1
 				command="UPDATE preference SET ValueString = '7.8.5.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
-			To7_9_0();
+			To7_9_1();
 		}
 
-		private static void To7_9_0() {
-			if(FromVersion<new Version("7.9.0.0")) {
+		private static void To7_9_1() {
+			if(FromVersion<new Version("7.9.1.0")) {
 				string command;
 				if(DataConnection.DBtype==DatabaseType.MySql) {
 					command="ALTER TABLE claim ADD CanadaTransRefNum varchar(255) NOT NULL";
@@ -4459,15 +4459,41 @@ VALUES('MercuryDE','"+POut.String(@"C:\MercuryDE\Temp\")+@"','0','','1','','','1
 					+"'FeeSchedulesSetManually', "
 					+"'0')";
 				Db.NonQ32(command);
-
-
-
-
-
-				command="UPDATE preference SET ValueString = '7.9.0.0' WHERE PrefName = 'DataBaseVersion'";
+				command="UPDATE preference SET ValueString = '7.9.1.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
-			//To7_10_0();
+			To8_0_0();
+		}
+
+		private static void To8_0_0() {
+			if(FromVersion<new Version("8.0.0.0")) {
+				string command;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				command="UPDATE preference SET ValueString = '8.0.0.0' WHERE PrefName = 'DataBaseVersion'";
+				Db.NonQ(command);
+			}
+			//To8_1_0();
 		}
 
 
