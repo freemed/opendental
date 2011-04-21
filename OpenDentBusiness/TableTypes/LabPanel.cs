d8a4e25f43f7ddd66781e673335765298ca650cf
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-04-21 14:24:53 +0000

    added LabResults and LabPanel
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@5966 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/LabPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness{
+	///<summary></summary>
+	[Serializable]
+	public class LabPanel:TableBase{
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long LabPanelNum;
+		///<summary>FK to patient.PatNum</summary>
+		public long PatNum;
+		///<summary>FK to medicalorder.MedicalOrderNum.</summary>
+		public long MedicalOrderNum;
+		///<summary>The entire raw HL7 message</summary>
+		public string RawMessage;
+		///<summary>Both name and address in a single field.  From OBR-20</summary>
+		public string Length;
+		///<summary>To be used for synch with web server.</summary>
+		public DateTime DateTStamp;
+
+		///<summary></summary>
+		public LabPanel Copy() {
+			return (LabPanel)this.MemberwiseClone();
+		}
+
+	}
+}
---
+++ OpenDentBusiness/TableTypes/LabResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness{
+	///<summary></summary>
+	[Serializable]
+	public class LabResult:TableBase{
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long LabResultNum;
+		///<summary>FK to labpanel.LabPanelNum.</summary>
+		public long LabPanelNum;
+		///<summary>From OBX-14.</summary>
+		public DateTime DateTest;
+		///<summary>From OBX-3.</summary>
+		public string TestPerformed;
+		///<summary>To be used for synch with web server.</summary>
+		public DateTime DateTStamp;
+
+		///<summary></summary>
+		public LabResult Copy() {
+			return (LabResult)this.MemberwiseClone();
+		}
+
+	}
+}
