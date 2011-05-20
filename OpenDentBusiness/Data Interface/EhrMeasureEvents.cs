0a8ae4db5d6862621e329fe6f1e75a1e531fd6c4
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-05-20 15:10:04 +0000

    EHR Measure Events
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6181 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/EhrMeasureEvents.cs
+++ OpenDentBusiness/Data Interface/EhrMeasureEvents.cs
@@ -7,62 +7,14 @@ using System.Text;
 namespace OpenDentBusiness{
 	///<summary></summary>
 	public class EhrMeasureEvents{
-		#region CachePattern
-		//This region can be eliminated if this is not a table type with cached data.
-		//If leaving this region in place, be sure to add RefreshCache and FillCache 
-		//to the Cache.cs file with all the other Cache types.
-
-		///<summary>A list of all EhrMeasureEvents.</summary>
-		private static List<EhrMeasureEvent> listt;
-
-		///<summary>A list of all EhrMeasureEvents.</summary>
-		public static List<EhrMeasureEvent> Listt{
-			get {
-				if(listt==null) {
-					RefreshCache();
-				}
-				return listt;
-			}
-			set {
-				listt=value;
-			}
-		}
 
 		///<summary></summary>
-		public static DataTable RefreshCache(){
-			//No need to check RemotingRole; Calls GetTableRemotelyIfNeeded().
-			string command="SELECT * FROM ehrmeasureevent ORDER BY ItemOrder";//stub query probably needs to be changed
-			DataTable table=Cache.GetTableRemotelyIfNeeded(MethodBase.GetCurrentMethod(),command);
-			table.TableName="EhrMeasureEvent";
-			FillCache(table);
-			return table;
-		}
-
-		///<summary></summary>
-		public static void FillCache(DataTable table){
-			//No need to check RemotingRole; no call to db.
-			listt=Crud.EhrMeasureEventCrud.TableToList(table);
-		}
-		#endregion
-
-		/*
-		Only pull out the methods below as you need them.  Otherwise, leave them commented out.
-
-		///<summary></summary>
-		public static List<EhrMeasureEvent> Refresh(long patNum){
+		public static List<EhrMeasureEvents> GetByType(EhrMeasureEventType ehrMeasureEventType,long patNum){
 			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
-				return Meth.GetObject<List<EhrMeasureEvent>>(MethodBase.GetCurrentMethod(),patNum);
+				return Meth.GetObject<List<EhrMeasureEvent>>(MethodBase.GetCurrentMethod(),ehrMeasureEventType,patNum);
 			}
-			string command="SELECT * FROM ehrmeasureevent WHERE PatNum = "+POut.Long(patNum);
+			string command="SELECT * FROM ehrmeasureevent WHERE EventType = "+POut.Int((int)ehrMeasureEventType)+" AND PatNum = "+POut.Long(patNum);
 			return Crud.EhrMeasureEventCrud.SelectMany(command);
-		}
-
-		///<summary>Gets one EhrMeasureEvent from the db.</summary>
-		public static EhrMeasureEvent GetOne(long ehrMeasureEventNum){
-			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
-				return Meth.GetObject<EhrMeasureEvent>(MethodBase.GetCurrentMethod(),ehrMeasureEventNum);
-			}
-			return Crud.EhrMeasureEventCrud.SelectOne(ehrMeasureEventNum);
 		}
 
 		///<summary></summary>
@@ -75,15 +27,6 @@ namespace OpenDentBusiness{
 		}
 
 		///<summary></summary>
-		public static void Update(EhrMeasureEvent ehrMeasureEvent){
-			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
-				Meth.GetVoid(MethodBase.GetCurrentMethod(),ehrMeasureEvent);
-				return;
-			}
-			Crud.EhrMeasureEventCrud.Update(ehrMeasureEvent);
-		}
-
-		///<summary></summary>
 		public static void Delete(long ehrMeasureEventNum) {
 			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
 				Meth.GetVoid(MethodBase.GetCurrentMethod(),ehrMeasureEventNum);
@@ -92,6 +35,27 @@ namespace OpenDentBusiness{
 			string command= "DELETE FROM ehrmeasureevent WHERE EhrMeasureEventNum = "+POut.Long(ehrMeasureEventNum);
 			Db.NonQ(command);
 		}
+
+		/*
+		Only pull out the methods below as you need them.  Otherwise, leave them commented out
+
+		///<summary>Gets one EhrMeasureEvent from the db.</summary>
+		public static EhrMeasureEvent GetOne(long ehrMeasureEventNum){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				return Meth.GetObject<EhrMeasureEvent>(MethodBase.GetCurrentMethod(),ehrMeasureEventNum);
+			}
+			return Crud.EhrMeasureEventCrud.SelectOne(ehrMeasureEventNum);
+		}
+
+		///<summary></summary>
+		public static void Update(EhrMeasureEvent ehrMeasureEvent){
+			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb){
+				Meth.GetVoid(MethodBase.GetCurrentMethod(),ehrMeasureEvent);
+				return;
+			}
+			Crud.EhrMeasureEventCrud.Update(ehrMeasureEvent);
+		}
+
 		*/
 
 
--- OpenDental/Forms/FormEduResourceEdit.cs
+++ OpenDental/Forms/FormEduResourceEdit.cs
@@ -190,20 +190,6 @@ namespace OpenDental.Forms {
 				EduResourceCur.LabResultCompare=textCompareValue.Text;
 			}
 			EduResourceCur.ResourceUrl=textUrl.Text;
-			if(!IsProblem) {
-				EduResourceCur.DiseaseDefNum=0;
-			}
-			if(!IsICD9) {
-				EduResourceCur.Icd9Num=0;
-			}
-			if(!IsMedication) {
-				EduResourceCur.MedicationNum=0;
-			}
-			if(!IsLab) {
-				EduResourceCur.LabResultCompare="";
-				EduResourceCur.LabResultID="";
-				EduResourceCur.LabResultName="";
-			}
 			if(IsNew) {
 				EduResources.Insert(EduResourceCur);
 			}
