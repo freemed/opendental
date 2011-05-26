aca2cc59a017abbaa1e352f1f11a85262575ce99
dennis <dennis@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-05-26 19:14:19 +0000

    MobileWeb
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6234 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Mobile/TableTypes/Pharmacym.cs
+++ OpenDentBusiness/Mobile/TableTypes/Pharmacym.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace OpenDentBusiness.Mobile.TableTypes {
+namespace OpenDentBusiness.Mobile {
 	[Serializable()]
 	[CrudTable(IsMobile=true)]
 	class Pharmacym:TableBase {
