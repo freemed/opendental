c3d86fdea66ffb984954c4af15a365685df81174
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-06-03 17:07:02 +0000

    Ortho Chart
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6281 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Data Interface/OrthoCharts.cs
+++ OpenDentBusiness/Data Interface/OrthoCharts.cs
@@ -7,43 +7,6 @@ using System.Text;
 namespace OpenDentBusiness{
 	///<summary></summary>
 	public class OrthoCharts{
-		#region CachePattern
-		//This region can be eliminated if this is not a table type with cached data.
-		//If leaving this region in place, be sure to add RefreshCache and FillCache 
-		//to the Cache.cs file with all the other Cache types.
-
-		///<summary>A list of all OrthoCharts.</summary>
-		private static List<OrthoChart> listt;
-
-		///<summary>A list of all OrthoCharts.</summary>
-		public static List<OrthoChart> Listt{
-			get {
-				if(listt==null) {
-					RefreshCache();
-				}
-				return listt;
-			}
-			set {
-				listt=value;
-			}
-		}
-
-		///<summary></summary>
-		public static DataTable RefreshCache(){
-			//No need to check RemotingRole; Calls GetTableRemotelyIfNeeded().
-			string command="SELECT * FROM orthochart ORDER BY ItemOrder";//stub query probably needs to be changed
-			DataTable table=Cache.GetTableRemotelyIfNeeded(MethodBase.GetCurrentMethod(),command);
-			table.TableName="OrthoChart";
-			FillCache(table);
-			return table;
-		}
-
-		///<summary></summary>
-		public static void FillCache(DataTable table){
-			//No need to check RemotingRole; no call to db.
-			listt=Crud.OrthoChartCrud.TableToList(table);
-		}
-		#endregion
 
 		/*
 		Only pull out the methods below as you need them.  Otherwise, leave them commented out.
--- OpenDentBusiness/TableTypes/OrthoChart.cs
+++ OpenDentBusiness/TableTypes/OrthoChart.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Drawing;
 
 namespace OpenDentBusiness {
-	///<summary>TODO_InputThisDescription</summary>
+	///<summary>For the orthochart feature, each row in this table is one cell in that grid.  An empty cell corresponds to a missing db table row.</summary>
 	[Serializable]
 	public class OrthoChart:TableBase {
 		///<summary>Primary key.</summary>
 		[CrudColumn(IsPriKey=true)]
 		public long OrthoChartNum;
-		///<summary>FK to patient.PatNum </summary>
+		///<summary>FK to patient.PatNum.</summary>
 		public long PatNum;
 		///<summary></summary>
 		public DateTime DateService;
