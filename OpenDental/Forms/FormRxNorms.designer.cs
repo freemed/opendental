6ec958b840dbdb936b7d970aa8fd4b697334be1f
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-06-06 18:16:53 +0000

    FormRxNorms added.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6292 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormRxNorms.designer.cs
@@ -0,0 +1,114 @@
+namespace OpenDental{
+	partial class FormRxNorms {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.butOK = new OpenDental.UI.Button();
+			this.butCancel = new OpenDental.UI.Button();
+			this.button1 = new OpenDental.UI.Button();
+			this.odGrid1 = new OpenDental.UI.ODGrid();
+			this.SuspendLayout();
+			// 
+			// butOK
+			// 
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butOK.Autosize = true;
+			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butOK.CornerRadius = 4F;
+			this.butOK.Location = new System.Drawing.Point(284,484);
+			this.butOK.Name = "butOK";
+			this.butOK.Size = new System.Drawing.Size(75,24);
+			this.butOK.TabIndex = 3;
+			this.butOK.Text = "&OK";
+			this.butOK.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// butCancel
+			// 
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butCancel.Autosize = true;
+			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCancel.CornerRadius = 4F;
+			this.butCancel.Location = new System.Drawing.Point(365,484);
+			this.butCancel.Name = "butCancel";
+			this.butCancel.Size = new System.Drawing.Size(75,24);
+			this.butCancel.TabIndex = 2;
+			this.butCancel.Text = "&Cancel";
+			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
+			// 
+			// button1
+			// 
+			this.button1.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Autosize = true;
+			this.button1.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.button1.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.button1.CornerRadius = 4F;
+			this.button1.Location = new System.Drawing.Point(26,484);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75,24);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "&None";
+			this.button1.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// odGrid1
+			// 
+			this.odGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.odGrid1.HScrollVisible = false;
+			this.odGrid1.Location = new System.Drawing.Point(26,34);
+			this.odGrid1.Name = "odGrid1";
+			this.odGrid1.ScrollValue = 0;
+			this.odGrid1.Size = new System.Drawing.Size(414,432);
+			this.odGrid1.TabIndex = 4;
+			this.odGrid1.Title = "RxNorm Codes";
+			this.odGrid1.TranslationName = null;
+			// 
+			// FormRxNorms
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(465,535);
+			this.Controls.Add(this.odGrid1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.butOK);
+			this.Controls.Add(this.butCancel);
+			this.Name = "FormRxNorms";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "RxNorms";
+			this.Load += new System.EventHandler(this.BasicTemplate_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private OpenDental.UI.Button butOK;
+		private OpenDental.UI.Button butCancel;
+		private UI.Button button1;
+		private UI.ODGrid odGrid1;
+	}
+}
