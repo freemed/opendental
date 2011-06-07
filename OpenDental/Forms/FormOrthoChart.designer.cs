e974e8fa640d7f7e35357e24dd862cb879eb234e
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-06-07 13:19:13 +0000

    FormOrthoChart.designer.cs
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6301 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormOrthoChart.designer.cs
@@ -0,0 +1,134 @@
+namespace OpenDental{
+	partial class FormOrthoChart {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.butAdd = new OpenDental.UI.Button();
+			this.butOK = new OpenDental.UI.Button();
+			this.butCancel = new OpenDental.UI.Button();
+			this.gridPat = new OpenDental.UI.ODGrid();
+			this.gridMain = new OpenDental.UI.ODGrid();
+			this.SuspendLayout();
+			// 
+			// butAdd
+			// 
+			this.butAdd.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.butAdd.Autosize = true;
+			this.butAdd.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butAdd.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butAdd.CornerRadius = 4F;
+			this.butAdd.Location = new System.Drawing.Point(10,634);
+			this.butAdd.Name = "butAdd";
+			this.butAdd.Size = new System.Drawing.Size(75,23);
+			this.butAdd.TabIndex = 9;
+			this.butAdd.Text = "Add";
+			this.butAdd.UseVisualStyleBackColor = true;
+			this.butAdd.Click += new System.EventHandler(this.butAdd_Click);
+			// 
+			// butOK
+			// 
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butOK.Autosize = true;
+			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butOK.CornerRadius = 4F;
+			this.butOK.Location = new System.Drawing.Point(770,634);
+			this.butOK.Name = "butOK";
+			this.butOK.Size = new System.Drawing.Size(75,23);
+			this.butOK.TabIndex = 8;
+			this.butOK.Text = "OK";
+			this.butOK.UseVisualStyleBackColor = true;
+			this.butOK.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// butCancel
+			// 
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butCancel.Autosize = true;
+			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCancel.CornerRadius = 4F;
+			this.butCancel.Location = new System.Drawing.Point(851,634);
+			this.butCancel.Name = "butCancel";
+			this.butCancel.Size = new System.Drawing.Size(75,23);
+			this.butCancel.TabIndex = 7;
+			this.butCancel.Text = "Cancel";
+			this.butCancel.UseVisualStyleBackColor = true;
+			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
+			// 
+			// gridPat
+			// 
+			this.gridPat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gridPat.HScrollVisible = false;
+			this.gridPat.Location = new System.Drawing.Point(10,9);
+			this.gridPat.Name = "gridPat";
+			this.gridPat.ScrollValue = 0;
+			this.gridPat.Size = new System.Drawing.Size(916,194);
+			this.gridPat.TabIndex = 6;
+			this.gridPat.Title = "Patient Fields";
+			this.gridPat.TranslationName = null;
+			// 
+			// gridMain
+			// 
+			this.gridMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gridMain.HScrollVisible = false;
+			this.gridMain.Location = new System.Drawing.Point(10,209);
+			this.gridMain.Name = "gridMain";
+			this.gridMain.ScrollValue = 0;
+			this.gridMain.Size = new System.Drawing.Size(916,418);
+			this.gridMain.TabIndex = 5;
+			this.gridMain.Title = "Ortho Chart";
+			this.gridMain.TranslationName = null;
+			// 
+			// FormOrthoChart
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(938,665);
+			this.Controls.Add(this.butAdd);
+			this.Controls.Add(this.butOK);
+			this.Controls.Add(this.butCancel);
+			this.Controls.Add(this.gridPat);
+			this.Controls.Add(this.gridMain);
+			this.Name = "FormOrthoChart";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Ortho Chart";
+			this.Load += new System.EventHandler(this.FormOrthoChart_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private UI.Button butAdd;
+		private UI.Button butOK;
+		private UI.Button butCancel;
+		private UI.ODGrid gridPat;
+		private UI.ODGrid gridMain;
+
+	}
+}
