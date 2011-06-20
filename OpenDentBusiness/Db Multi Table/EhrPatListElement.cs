0f76bee321fb6c729dd07f351227b53db25f8a24
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-06-20 19:33:40 +0000

    EHR pat list work.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6396 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/Db Multi Table/EhrPatListElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenDentBusiness {
+	///<summary>Used by ehr "Generate Patient Lists".  This object represents one row in the grid when building the report.  Multiple such elements will be ANDed together to automatically generate a query.</summary>
+	public class EhrPatListElement {
+		///<summary>Birthdate, Disease, Medication, or LabResult</summary>
+		public EhrRestrictionType Restriction;
+		///<summary>For all 4 types, what to compare against.  Examples:  Birthdate: '50', Disease: '4140' (icd9 code will be followed by wildcard), Medication: 'Lisinopril' (not case sensitive, surrounded by wildcards), LabResult: 'HDL-cholesterol'.</summary>
+		public string CompareString;
+		///<summary>gt, lt, or equal.  Only used for lab and birthdate.</summary>
+		public EhrOperand Operand;
+		///<summary>Only used for Lab.  Usually a number.</summary>
+		public string LabValue;
+		///<summary></summary>
+		public bool OrderBy;
+	}
+
+	public enum EhrRestrictionType {
+		Birthdate,
+		Disease,
+		Medication,
+		LabResult
+	}
+
+	public enum EhrOperand {
+		GreaterThan,
+		LessThan,
+		Equals
+	}
+}
---
+++ OpenDentBusiness/Db Multi Table/EhrPatListElements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace OpenDentBusiness {
+	///<summary>Used in Ehr patient lists.</summary>
+	public class EhrPatListElements {
+
+	}
+}
