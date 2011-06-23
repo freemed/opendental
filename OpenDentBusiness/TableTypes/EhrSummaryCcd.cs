080d6260b4a0aa955eef0f806d23e025a0aadc76
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-06-23 16:56:05 +0000

    Added table EhrSummaryCcd
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6409 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/EhrSummaryCcd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness {
+	///<summary>Can also be a CCR.  Sent CCDs are not saved here, only received CCDs/CCRs.  To display a saved Ccd, it is combined with an internal stylesheet.</summary>
+	[Serializable]
+	public class EhrSummaryCcd:TableBase {
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long EhrSummaryCcdNum;
+		///<summary>FK to patient.PatNum.</summary>
+		public long PatNum;
+		///<summary>Date that this Ccd was received.</summary>
+		public DateTime DateSummary;
+		///<summary>The xml content of the received text file.</summary>
+		[CrudColumn(SpecialType=CrudSpecialColType.TextIsClob)]
+		public string ContentSummary;
+
+		///<summary></summary>
+		public EhrSummaryCcd Copy() {
+			return (EhrSummaryCcd)MemberwiseClone();
+		}
+
+	}
+
+	
+
+}
