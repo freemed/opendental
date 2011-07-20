e3b76af9b42d6081400a37b7438f3741b9053f65
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-07-20 14:45:39 +0000

    Adding TopazT.cs
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6578 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ UnitTests/TopazT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using OpenDentBusiness;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace UnitTests {
+	public class TopazT {
+		
+		/// <summary></summary>
+		public static string RunAll() {
+			string retVal="";
+			string dataToSign="The signature will be encrypted to this data.  I can add as much as I like.";
+			string sigNew="";
+			string sigOld="";
+
+			//send 2 different sigs to the wrapper.  Both should work.
+			
+
+
+
+			retVal+="Topaz test done.\r\n";
+			return retVal;
+		}
+
+		
+
+	}
+}
