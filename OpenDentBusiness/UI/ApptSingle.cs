94f432c4cf688f207e4a9f417a748bd763d403ca
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-08-05 16:42:50 +0000

    Added ApptSingle.cs
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6674 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/UI/ApptSingle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace OpenDentBusiness.UI{
+	///<summary>A container class that holds info about one appointment for drawing purposes.</summary>
+	public class ApptSingle {
+
+
+		public Bitmap GetImage() {
+			return null;
+		}
+
+	}
+}
