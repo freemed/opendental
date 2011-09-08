01ee47d0c6f54d092dae3ed1db0695b8ebcae063
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-09-08 13:33:59 +0000

    Checkboxes on reports setup are now filled.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6903 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormReportSetup.cs
+++ OpenDental/Forms/FormReportSetup.cs
@@ -17,7 +17,9 @@ namespace OpenDental {
 		}
 
 		private void FormReportSetup_Load(object sender,EventArgs e) {
-//todo: set checkboxes
+			checkReportsProcDate.Checked=PrefC.GetBool(PrefName.ReportsPPOwriteoffDefaultToProcDate);
+			checkReportsShowPatNum.Checked=PrefC.GetBool(PrefName.ReportsShowPatNum);
+			checkReportProdWO.Checked=PrefC.GetBool(PrefName.ReportPandIschedProdSubtractsWO);
 			//TEMP------------------------------------------------------------------------------------------
 			//butAgg.Visible=false;//TODO: THIS IS TEMPORARY. DELETE THIS LINE WHEN THE AGGREGATOR IS WORKING.
 			//TEMP------------------------------------------------------------------------------------------
