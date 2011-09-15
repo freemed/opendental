9e2c92cab8463c715ebf019d77e5e7a66b2102f0
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-09-15 17:53:46 +0000

    Added background for UDS 3A report
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@6949 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Properties/Resources.Designer.cs
+++ OpenDental/Properties/Resources.Designer.cs
@@ -471,6 +471,13 @@ namespace OpenDental.Properties {
             }
         }
         
+        internal static System.Drawing.Bitmap UDS3a {
+            get {
+                object obj = ResourceManager.GetObject("UDS3a", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;
         ///&lt;Report Name=&quot;&quot;&gt;
