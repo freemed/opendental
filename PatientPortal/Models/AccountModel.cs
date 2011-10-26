d80e43753f810b616c06794b3b1a068640143696
dennis <dennis@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-10-26 17:38:29 +0000

    Patient Portal
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@7174 098d0599-7f2b-0410-95a5-ad30ac85393a

--- PatientPortal/Models/AccountModel.cs
+++ PatientPortal/Models/AccountModel.cs
@@ -31,27 +31,20 @@ namespace PatientPortalMVC.Models {
 		public List<Statementm> statementmList;
 
 		public AccountModel(Patientm patm) {
+			String formatstring="{0:0.00}";
+			BalTotal="$"+String.Format(formatstring,Math.Abs(patm.BalTotal));
+			InsEst="$"+String.Format(formatstring,Math.Abs(patm.InsEst));
+			AfterIns="$"+String.Format(formatstring,Math.Abs(patm.BalTotal-patm.InsEst));
 			if(patm.BalTotal<0) {
-				BalTotal="-$" + Math.Abs(patm.BalTotal);
-			}
-			else {
-				BalTotal="$" + Math.Abs(patm.BalTotal);
+				BalTotal="-"+BalTotal;
 			}
 			if(patm.InsEst<0) {
-				InsEst="-$" + Math.Abs(patm.InsEst);
-			}
-			else {
-				InsEst="$" + Math.Abs(patm.InsEst);
+				InsEst="-"+InsEst;
 			}
 			if((patm.BalTotal-patm.InsEst)<0) {
-				AfterIns="-$" + Math.Abs(patm.BalTotal-patm.InsEst);
+				AfterIns="-"+AfterIns;
 			}
-			else {
-				AfterIns="$" + Math.Abs(patm.BalTotal-patm.InsEst);
-			}
-
 			statementmList=Statementms.GetStatementms(patm.CustomerNum,patm.PatNum);
-
 		}
 
 	}
