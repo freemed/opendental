0138e90b71d8c1e21db0563be6727aaaedfee386
dennis <dennis@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-11-10 22:31:22 +0000

    Patient Portal
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@7228 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/Mobile/Data Interface/PrefmC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace OpenDentBusiness.Mobile {
+	public class PrefmC {
+		///<summary>Dennis ToDo: this class has to be revised</summary>
+		public Dictionary<string,Prefm> Dict=new Dictionary<string,Prefm>();// cannot have a static variable here because we want something unique for each patient.
+		///<summary>Gets a pref of type string.</summary>
+		public string GetString(PrefmName prefmName) {
+			if(Dict==null) {
+				Prefms.LoadPreferences(3);
+			}
+			if(!Dict.ContainsKey(prefmName.ToString())) {
+				throw new Exception(prefmName+" is an invalid pref name.");
+			}
+			return Dict[prefmName.ToString()].ValueString;
+		}
+
+
+	}
+}
+
+
+
+	
