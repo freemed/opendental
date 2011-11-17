4163816af73c3765abb965eeaadcda1547cb939e
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-11-17 22:35:33 +0000

    Changed head to 12.0
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@7257 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Properties/AssemblyInfo.cs
+++ OpenDental/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("11.2.0.0")]
+[assembly: AssemblyVersion("12.0.0.0")]
 
 //[assembly: AssemblyFileVersion("1.0.0.0")]
