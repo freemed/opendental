5d32d068028952426daf445e7d4f5c38fec91c8e
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-11-18 22:08:04 +0000

    ConvertDatabases2.cs changing 11.2 to 12.0
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@7265 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Misc/ConvertDatabases2.cs
+++ OpenDentBusiness/Misc/ConvertDatabases2.cs
@@ -23,7 +23,7 @@ namespace OpenDentBusiness {
 	//The other file was simply getting too big.  It was bogging down VS speed.
 	///<summary></summary>
 	public partial class ConvertDatabases {
-		public static System.Version LatestVersion=new Version("11.2.0.0");//This value must be changed when a new conversion is to be triggered.
+		public static System.Version LatestVersion=new Version("12.0.0.0");//This value must be changed when a new conversion is to be triggered.
 
 		private static void To6_2_9() {
 			if(FromVersion<new Version("6.2.9.0")) {
@@ -6825,12 +6825,12 @@ VALUES('MercuryDE','"+POut.String(@"C:\MercuryDE\Temp\")+@"','0','','1','','','1
 				command="UPDATE preference SET ValueString = '11.1.9.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
-			To11_2_0();
+			To12_0_0();
 		}
 		
 		///<summary></summary>
-		private static void To11_2_0() {
-			if(FromVersion<new Version("11.2.0.0")) {
+		private static void To12_0_0() {
+			if(FromVersion<new Version("12.0.0.0")) {
 				string command;
 				if(DataConnection.DBtype==DatabaseType.MySql) {
 					command="INSERT INTO preference(PrefName,ValueString) VALUES('BillingEmailSubject','Statement for account [PatNum]')";
@@ -7026,10 +7026,10 @@ VALUES('MercuryDE','"+POut.String(@"C:\MercuryDE\Temp\")+@"','0','','1','','','1
 
 
 
-				command="UPDATE preference SET ValueString = '11.2.0.0' WHERE PrefName = 'DataBaseVersion'";
+				command="UPDATE preference SET ValueString = '12.0.0.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
-			//To11_3_0();
+			//To12_1_0();
 		}
 
 
