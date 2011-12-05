b21a3c8d81d0cf4c02c6f40e12c7a751694fea7b
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2011-12-05 22:09:32 +0000

    Added Central Manager project.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@7329 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ CentralManager/FormCentralManager.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace CentralManager {
+	partial class FormCentralManager {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.SuspendLayout();
+			// 
+			// FormCentralManager
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F,13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292,273);
+			this.Name = "FormCentralManager";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Central Manager";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
