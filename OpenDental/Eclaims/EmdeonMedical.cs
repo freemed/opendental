df19408a83180a08d14304c1b85cdec1c46d21cc
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-01-06 00:03:47 +0000

    Emdeon Medical bridge enhancements. Not showing in UI yet, but otherwise ready for testing.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@7450 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Eclaims/EmdeonMedical.cs
+++ OpenDental/Eclaims/EmdeonMedical.cs
@@ -23,49 +23,107 @@ namespace OpenDental.Eclaims
 			EmdeonITSTest.ITSWS itsws=new EmdeonITSTest.ITSWS();
 			itsws.Url="https://cert.its.emdeon.com/ITS/ITSWS.asmx";//test url
 			//itsws.Url="https://its.emdeon.com/ITS/ITSWS.asmx";//production url
-			try{
-			  if(!Directory.Exists(clearhouse.ExportPath)){
-			    throw new Exception("Clearinghouse export path is invalid.");
-			  }
-			  if(!Directory.Exists(clearhouse.ResponsePath)){
-			    throw new Exception("Clearinghouse response path is invalid.");
-			  }
-				string[] files=Directory.GetFiles(clearhouse.ExportPath);
-				for(int i=0;i<files.Length;i++) {
-					byte[] fileBytes=File.ReadAllBytes(files[i]);
-					string fileBytesBase64=Convert.ToBase64String(fileBytes);
-					string messageType="MCT";//medical
-					if(medType==EnumClaimMedType.Institutional) {
-						messageType="HCT";
+			string messageType="";
+			if(batchNum!=0) { //batchNum will not be zero when a batch is being sent but would be zero when getting reports.
+				string batchFile="";
+				Etrans etrans=null;
+				try {
+					if(!Directory.Exists(clearhouse.ExportPath)) {
+						throw new Exception("Clearinghouse export path is invalid.");
 					}
-					else if(medType==EnumClaimMedType.Dental) {
-						messageType="DCT";//not used yet, but planned for future.
-					}
-					EmdeonITSTest.ITSReturn response=itsws.PutFileExt(clearhouse.LoginID,clearhouse.Password,messageType,Path.GetFileName(files[i]),fileBytesBase64);
-					if(response.ErrorCode==0) { //Submission successful.
-						string successDetails=response.Response;//todo: store in db
-						File.Delete(files[i]);//delete uploaded claims so they don't get resent later.
-					}
-					else { //Submission error.
-						MessageBox.Show(Lan.g("EmdeonMedical","Claim submission failed. Batch file")+" "+files[i]+" "
-							+Lan.g("EmdeonMedical","will not be deleted so it can be resent. Error message from Emdeon")+": "+response.Response);
+					string[] files=Directory.GetFiles(clearhouse.ExportPath);
+					if(files.Length>0) {
+						//We make sure to only send the X12 batch file for the current batch, so that if there is a failure, then we know
+						//for sure that we need to reverse the batch. This will also help us avoid any exterraneous/old batch files in the
+						//same directory which might be left if there is a permission issue when trying to delete the batch files after processing.
+						for(int i=0;i<files.Length;i++) {
+							if(Path.GetFileName(files[i])=="claims"+batchNum+".txt") {
+								batchFile=files[i];
+								break;
+							}
+						}
+						byte[] fileBytes=File.ReadAllBytes(batchFile);
+						string fileBytesBase64=Convert.ToBase64String(fileBytes);
+						messageType="MCT";//medical
+						if(medType==EnumClaimMedType.Institutional) {
+							messageType="HCT";
+						}
+						else if(medType==EnumClaimMedType.Dental) {
+							messageType="DCT";//not used/tested yet, but planned for future.
+						}
+						EmdeonITSTest.ITSReturn response=itsws.PutFileExt(clearhouse.LoginID,clearhouse.Password,messageType,Path.GetFileName(batchFile),fileBytesBase64);
+						etrans=new Etrans();
+						etrans.BatchNumber=batchNum;
+						etrans.ClearingHouseNum=clearhouse.ClearinghouseNum;
+						etrans.DateTimeTrans=DateTime.Now;
+						etrans.Etype=EtransType.TextReport;
+						etrans.Note=response.Response;
+						if(response.ErrorCode==0) { //Batch submission successful.
+							etrans.AckCode="A";
+							Etranss.Insert(etrans);
+						}
+						else { //Batch submission rejection/error.
+							etrans.AckCode="R";
+							Etranss.Insert(etrans);
+							throw new Exception("Emdeon rejected all claims in the current batch file "+batchFile+" for the following reason: "+response.Response);
+						}
 					}
 				}
-			//  //rename the downloaded mail files to end with txt
-			//  files=Directory.GetFiles(clearhouse.ResponsePath);
-			//  for(int i=0;i<files.Length;i++){
-			//    //string t=files[i];
-			//    if(Path.GetExtension(files[i])!=".txt"){
-			//      File.Move(files[i],files[i]+".txt");
-			//    }
-			//  }
+				catch(Exception e) {
+					MessageBox.Show(e.Message);
+					x837Controller.Rollback(clearhouse,batchNum);
+					return false;
+				}
+				finally {
+					if(batchFile!="") {
+						try {
+							File.Delete(batchFile);
+						}
+						catch {
+							MessageBox.Show("Failed to remove batch file "+batchFile+". This is probably due to a permission issue. Check folder permissions and delete this file manually.");
+						}
+					}
+				}
 			}
-			catch(Exception e){
-			  MessageBox.Show(e.Message);
-			  if(batchNum!=0){
-			    x837Controller.Rollback(clearhouse,batchNum);
-			  }
-			  return false;
+			else {//batchNum=0, therefore getting reports.
+				if(!Directory.Exists(clearhouse.ResponsePath)) {
+					throw new Exception("Clearinghouse response path is invalid.");
+				}
+				try {
+					//Download the most up to date reports, but do not delete them from the server yet.
+					messageType="MCTG";//medical
+					if(medType==EnumClaimMedType.Institutional) {
+						messageType="HCTG";
+					}
+					else if(medType==EnumClaimMedType.Dental) {
+						messageType="DCTG";
+					}
+					EmdeonITSTest.ITSReturn response=itsws.GetFile(clearhouse.LoginID,clearhouse.Password,messageType);
+					if(response.ErrorCode==0) { //Report retrieval successful.
+						string reportFileDataBase64=response.Response;
+						byte[] reportFileDataBytes=Convert.FromBase64String(reportFileDataBase64);
+						string reportFilePath=CodeBase.ODFileUtils.CreateRandomFile(clearhouse.ResponsePath,".zip");
+						File.WriteAllBytes(reportFilePath,reportFileDataBytes);
+						//Now that the file has been saved, remove the report file from the Emdeon server.
+						messageType="MCTD";//medical
+						if(medType==EnumClaimMedType.Institutional) {
+							messageType="HCTD";
+						}
+						else if(medType==EnumClaimMedType.Dental) {
+							messageType="DCTD";
+						}
+						//If deleting the report fails, we don't care because that will simply mean that we download it again next time.
+						//Thus we don't need to check the status after this next call.
+						itsws.GetFile(clearhouse.LoginID,clearhouse.Password,messageType);
+					}
+					else { //Report retrieval failure.
+						throw new Exception("Failed to get reports from Emdeon. Error message from Emdeon: "+response.Response);
+					}
+				}
+				catch(Exception ex) {
+					MessageBox.Show(ex.Message);
+					return false;
+				}
 			}
 			return true;
 		}
