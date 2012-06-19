ebf87a7a6f3c0da220902bcccf015513c6161346
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-06-19 22:11:48 +0000

    CaptureLink bridge will not be available until we get specifications.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8077 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Bridges/CaptureLink.cs
+++ OpenDental/Bridges/CaptureLink.cs
@@ -41,3 +41,55 @@ namespace OpenDental.Bridges{
 
 	}
 }
+
+//WE NEED TO ADD THE FOLLOWING CODE TO ConvertDatabases2.cs WHEN WE CAN VERIFY THE FUNCTIONALITY OF THIS BRIDGE WITH SPECIFICATIONS (WHICH WE DO NOT HAVE YET).
+////Insert CaptureLink Bridge
+//if(DataConnection.DBtype==DatabaseType.MySql) {
+//  command="INSERT INTO program (ProgName,ProgDesc,Enabled,Path,CommandLine,Note"
+//    +") VALUES("
+//    +"'CaptureLink', "
+//    +"'CaptureLink from www.henryschein.ca', "
+//    +"'0', "
+//    +"'"+POut.String(@"CaptureLink.exe")+"',"
+//    +"'', "
+//    +"'')";
+//  long programNum=Db.NonQ(command,true);
+//  command="INSERT INTO programproperty (ProgramNum,PropertyDesc,PropertyValue"
+//    +") VALUES("
+//    +"'"+POut.Long(programNum)+"', "
+//    +"'Enter 0 to use PatientNum, or 1 to use ChartNum', "
+//    +"'0')";
+//  Db.NonQ(command);
+//  command="INSERT INTO toolbutitem (ProgramNum,ToolBar,ButtonText) "
+//    +"VALUES ("
+//    +"'"+POut.Long(programNum)+"', "
+//    +"'"+POut.Int(((int)ToolBarsAvail.ChartModule))+"', "
+//    +"'CaptureLink')";
+//  Db.NonQ(command);
+//}
+//else {//oracle
+//  command="INSERT INTO program (ProgramNum,ProgName,ProgDesc,Enabled,Path,CommandLine,Note"
+//    +") VALUES("
+//    +"(SELECT MAX(ProgramNum)+1 FROM program),"
+//    +"'CaptureLink', "
+//    +"'CaptureLink from www.henryschein.ca', "
+//    +"'0', "
+//    +"'"+POut.String(@"CaptureLink.exe")+"',"
+//    +"'', "
+//    +"'')";
+//  long programNum=Db.NonQ(command,true);
+//  command="INSERT INTO programproperty (ProgramPropertyNum,ProgramNum,PropertyDesc,PropertyValue"
+//    +") VALUES("
+//    +"(SELECT MAX(ProgramPropertyNum+1) FROM programproperty),"
+//    +"'"+POut.Long(programNum)+"', "
+//    +"'Enter 0 to use PatientNum, or 1 to use ChartNum', "
+//    +"'0')";
+//  Db.NonQ(command);
+//  command="INSERT INTO toolbutitem (ToolButItemNum,ProgramNum,ToolBar,ButtonText) "
+//    +"VALUES ("
+//    +"(SELECT MAX(ToolButItemNum)+1 FROM toolbutitem),"
+//    +"'"+POut.Long(programNum)+"', "
+//    +"'"+POut.Int(((int)ToolBarsAvail.ChartModule))+"', "
+//    +"'CaptureLink')";
+//  Db.NonQ(command);
+//}//end CaptureLink bridge
