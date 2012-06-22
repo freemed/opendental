b43834aa41a5a69fb2b3c547c1f3adc1bdaa56a4
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-06-22 22:52:38 +0000

    Tscan bridge updates.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8092 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Bridges/Tscan.cs
+++ OpenDental/Bridges/Tscan.cs
@@ -20,47 +20,51 @@ namespace OpenDental.Bridges{
 		///<summary>Launches the program using a combination of command line characters and the patient.Cur data.</summary>
 		public static void SendData(Program ProgramCur, Patient pat){
 			string path=Programs.GetProgramPath(ProgramCur);
-			ArrayList ForProgram=ProgramProperties.GetForProgram(ProgramCur.ProgramNum);
-			if(pat==null){
+			if(pat==null) {
 				try {
 					Process.Start(path);//should start Tscan without bringing up a pt.
 				}
 				catch {
 					MessageBox.Show(path+" is not available.");
 				}
+				return;
 			}
-			else {
-				string info=" -f"+Regex.Replace(pat.FName,"[^a-zA-Z0-9]","");//First name can only be alpha-numeric, so we remove non-alpha-numeric characters.
-				if(Regex.Replace(pat.MiddleI,"[^a-zA-Z0-9]","")!="") {
-					info+=" -m"+Regex.Replace(pat.MiddleI,"[^a-zA-Z0-9]","");//Middle name can only be alpha-numeric, so we remove non-alpha-numeric characters.
-				}
-				info+=" -l"+Regex.Replace(pat.LName,"[^a-zA-Z0-9]","");//Last name can only be alpha-numeric, so we remove non-alpha-numeric characters.
-				//Patient id can only be aplha numeric, but they will almost always be so we don't check that here.
-				ProgramProperty PPCur=ProgramProperties.GetCur(ForProgram, "Enter 0 to use PatientNum, or 1 to use ChartNum");
-				if(PPCur.PropertyValue=="0"){
-					info+=" -i"+pat.PatNum.ToString();
-				}
-				else{
-					info+=" -i"+Regex.Replace(pat.ChartNumber,"[^a-zA-Z0-9]","");//Spaces not allowed.
-				}
-				if(pat.Birthdate.Year>1880) {
-					info+=" -d"+pat.Birthdate.Day.ToString().PadLeft(2,'0');
-					info+=" -j"+pat.Birthdate.Month.ToString().PadLeft(2,'0');
-					info+=" -y"+pat.Birthdate.Year.ToString();
-				}
-				if(pat.Gender==PatientGender.Female) {
-					info+=" -g1";
-				}
-				else if(pat.Gender==PatientGender.Male) { 
-					info+=" -g2";
-				}
-				try{
-					Process.Start(path,ProgramCur.CommandLine+info);
-				}
-				catch{
-					MessageBox.Show(path+" is not available, or there is an error in the command line options.");
-				}
+			string info="-f"+Tidy(pat.FName)+" ";//First name can only be alpha-numeric, so we remove non-alpha-numeric characters.
+			if(Tidy(pat.MiddleI)!="") {//Only send middle name if available, since it is optional.
+				info+="-m"+Tidy(pat.MiddleI)+" ";//Middle name can only be alpha-numeric, so we remove non-alpha-numeric characters.
 			}
+			info+="-l"+Tidy(pat.LName)+" ";//Last name can only be alpha-numeric, so we remove non-alpha-numeric characters.
+			if(ProgramProperties.GetPropVal(ProgramCur.ProgramNum,"Enter 0 to use PatientNum, or 1 to use ChartNum")=="0"){
+				info+="-i"+pat.PatNum.ToString()+" ";
+			}
+			else{
+				info+="-i"+Tidy(pat.ChartNumber)+" ";//Patient id only alpha-numeric as required.
+			}
+			//Birthdate is optional, so we only send if valid.
+			if(pat.Birthdate.Year>1880) {
+				info+="-d"+pat.Birthdate.Day.ToString().PadLeft(2,'0')+" ";//The example in specification shows days with two digits, so we pad.
+				info+="-j"+pat.Birthdate.Month.ToString().PadLeft(2,'0')+" ";//The example in specification shows months with two digits, so we pad.
+				info+="-y"+pat.Birthdate.Year.ToString()+" ";//The example specification shows years 4 digits long.
+			}
+			//Gender is optional, so we only send if not Unknown.
+			if(pat.Gender==PatientGender.Female) {
+				info+="-g1 ";
+			}
+			else if(pat.Gender==PatientGender.Male) {
+				info+="-g2 ";
+			}
+			try {
+				Process.Start(path,info);
+			}
+			catch(Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		///<summary>Removes non-alpha-numeric characters.</summary>
+		private static string Tidy(string input) {
+			string retVal=Regex.Replace(input,"[^a-zA-Z0-9]","");//get rid of any non-alpha-numeric characters.
+			return retVal;
 		}
 
 	}
