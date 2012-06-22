9cf8e4325397f0ab241d582fa2449301c5256d02
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-06-22 22:53:09 +0000

    ClioSoft bridge updates.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8093 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Bridges/ClioSoft.cs
+++ OpenDental/Bridges/ClioSoft.cs
@@ -19,39 +19,46 @@ namespace OpenDental.Bridges{
 		///<summary>Launches the program using a combination of command line characters and the patient.Cur data.</summary>
 		public static void SendData(Program ProgramCur, Patient pat){
 			string path=Programs.GetProgramPath(ProgramCur);
-			ArrayList ForProgram=ProgramProperties.GetForProgram(ProgramCur.ProgramNum);
-			if(pat==null){
-				try{
+			if(pat==null) {
+				try {
 					Process.Start(path);//should start ClioSoft without bringing up a pt.
 				}
-				catch{
+				catch {
 					MessageBox.Show(path+" is not available.");
 				}
+				return;
+			}
+			string info="\"";
+			//Patient id can be any string format
+			if(ProgramProperties.GetPropVal(ProgramCur.ProgramNum,"Enter 0 to use PatientNum, or 1 to use ChartNum")=="0") {
+				info+="-"+pat.PatNum.ToString()+";";
+			}
+			else{
+				info+="-"+Tidy(pat.ChartNumber)+";";
+			}
+			info+=Tidy(pat.FName)+";";
+			info+=Tidy(pat.LName)+";";
+			//Birthdate is optional, so we only send if valid.
+			if(pat.Birthdate.Year>1880) {
+				info+=pat.Birthdate.ToString("MM/dd/yyyy")+";";
 			}
 			else {
-				string info=" \"-";
-				//Patient id can be any string format
-				ProgramProperty PPCur=ProgramProperties.GetCur(ForProgram, "Enter 0 to use PatientNum, or 1 to use ChartNum");
-				if(PPCur.PropertyValue=="0"){
-					info+=pat.PatNum.ToString();
-				}
-				else{
-					info+=pat.ChartNumber;
-				}
-				//We remove double-quotes from the first and last name of the patient so extra double-quotes don't
-				//cause confusion in the command line parameters for ClioSoft.
-				info+=";"+pat.FName.Replace("\"","")+";"+pat.LName.Replace("\"","")+";";
-				if(pat.Birthdate.Year>1880) {
-					info+=pat.Birthdate.ToString("MM/dd/yyyy");
-				}
-				info+=";"+pat.SSN+";\"";
-				try{
-					Process.Start(path,ProgramCur.CommandLine+info);
-				}
-				catch{
-					MessageBox.Show(path+" is not available, or there is an error in the command line options.");
-				}
+				info+=";";
 			}
+			info+=Tidy(pat.SSN)+";";//They don't care about dashes and there is no length restriction, just not semi-colons.
+			info+="\"";
+			try {
+				Process.Start(path,info);
+			}
+			catch(Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		///<summary>Removes semi-colons.</summary>
+		private static string Tidy(string input) {
+			string retVal=input.Replace(";","");//get rid of any semicolons.
+			return retVal;
 		}
 
 	}
