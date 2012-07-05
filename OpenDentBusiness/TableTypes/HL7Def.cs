a3821adaa037ed5c4124caa1cd5fe5f01f271bf7
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-07-05 21:10:46 +0000

    HL7Def and HL7DefField tables added.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8142 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/HL7Def.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness{
+	///<summary>.</summary>
+	[Serializable()]
+	public class HL7Def:TableBase{
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long HL7DefNum;
+		///<summary></summary>
+		public string Description;
+		///<summary>Enum:ModeTxHL7 File, TcpIp</summary>
+		public string ModeTx;
+		///<summary>Only used for File mode</summary>
+		public string IncomingFolder;
+		///<summary>Only used for File mode</summary>
+		public string OutgoingFolder;
+		///<summary>Only used for tcpip mode. Example: 1461</summary>
+		public string IncomingPort;
+		///<summary>Only used for tcpip mode. Example: 192.168.0.23:1462</summary>
+		public string OutgoingIpPort;
+		///<summary>Only relevant for outgoing. Incoming field separators are defined in MSH. Default |.</summary>
+		public string FieldSeparator;
+		///<summary>Only relevant for outgoing. Incoming field separators are defined in MSH. Default ^.</summary>
+		public string ComponentSeparator;
+		///<summary>Only relevant for outgoing. Incoming field separators are defined in MSH. Default &.</summary>
+		public string SubcomponentSeparator;
+		///<summary>Only relevant for outgoing. Incoming field separators are defined in MSH. Default ~.</summary>
+		public string RepetitionSeparator;
+		///<summary>Only relevant for outgoing. Incoming field separators are defined in MSH. Default \.</summary>
+		public string EscapeCharacter;
+		///<summary>If this is set, then there will be no child tables. Internal types are fully defined within the C# code rather than in the database.</summary>
+		public bool IsInternal;
+		///<summary>This will always have a value because we always start with a copy of some internal type.</summary>
+		public string InternalType;
+		///<summary>Example: 12.2.14. This will be empty if IsInternal. This records the version at which they made their copy. We might have made significant improvements since their copy.</summary>
+		public string InternalTypeVersion;
+		///<summary>.</summary>
+		public bool IsEnabled;
+		///<summary></summary>
+		[CrudColumn(SpecialType=CrudSpecialColType.TextIsClob)]
+		public string Note;
+
+		///<summary></summary>
+		public HL7Def Clone() {
+			return (HL7Def)this.MemberwiseClone();
+		}
+
+	}
+
+	
+}
---
+++ OpenDentBusiness/TableTypes/HL7DefField.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness{
+	///<summary>Multiple fields per segment.</summary>
+	[Serializable()]
+	public class HL7DefField:TableBase{
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long HL7DefFieldNum;
+		///<summary>FK to HL7DefSegment.HL7DefSegmentNum</summary>
+		public long HL7DefSegmentNum;
+		///<summary>Position within the segment.</summary>
+		public int OrdinalPos;
+		///<summary>HL7 table Id, if applicable. Example: 0234. Example: 1234/2345</summary>
+		public string TableId;
+		///<summary>The DataTypeHL7 enum will be unlinked from the db by storing as string in db. As it's loaded into OD, it will become an enum.</summary>
+		public string DataType;
+		///<summary>User will get to pick from a list of fields that we will maintain. For example, guarantor.LName, provider.Abbr, or patient.addressFull</summary>
+		public string FieldName;
+
+		///<summary></summary>
+		public HL7DefField Clone() {
+			return (HL7DefField)this.MemberwiseClone();
+		}
+
+	}
+
+	
+}
