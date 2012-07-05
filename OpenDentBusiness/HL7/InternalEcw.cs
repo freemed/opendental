ad07fb2f48692509bf5ca907465ee8381feb13ab
jordansparks <jordansparks@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-07-05 19:56:41 +0000

    Added HL7/InternalEcw.cs
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8140 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/HL7/InternalEcw.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDentBusiness.HL7 {
+	///<summary></summary>
+	public class InternalEcw {
+		//public static HL7Def GetHL7Def(
+			//build the objects here 
+
+		//}
+
+
+	}
+}
