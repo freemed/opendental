dfaa2185581d17a301b29063c592758442ca024a
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-07-06 18:26:04 +0000

    Save commit for new eCW internal def.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8146 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/HL7/InternalEcw.cs
+++ OpenDentBusiness/HL7/InternalEcw.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace OpenDentBusiness.HL7 {
 	///<summary></summary>
 	public class InternalEcw {
-		//public static HL7Def GetHL7Def(
-			//build the objects here 
 
-		//}
+		public static HL7Def GetHL7Def() {
+			HL7Def def=new HL7Def();
+			def.Description="eClinicalWorks";
+			def.ModeTx="File";//TODO: Convert to enum.
+			def.IncomingFolder=@"C:\HL7\In";
+			def.OutgoingFolder=@"C:\HL7\Out";
+			def.IncomingPort="";
+			def.OutgoingIpPort="";
+			def.FieldSeparator="|";
+			def.ComponentSeparator="^";
+			def.SubcomponentSeparator="&";
+			def.RepetitionSeparator="~";
+			def.EscapeCharacter=@"\";
+			def.IsInternal=true;
+			def.InternalType="eCW";
+			def.InternalTypeVersion=Assembly.GetAssembly(typeof(Db)).GetName().Version.ToString();
+			def.IsEnabled=false;
+			def.Note="";
+			return def;
+		}
 
 
 	}
