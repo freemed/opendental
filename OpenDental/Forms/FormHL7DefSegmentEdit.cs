db035e2b22543464ee2a8d94b5ab6ecbbba5e615
cameron <cameron@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-07-13 21:46:48 +0000

    Added HL7DefSegmentEdit form.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8165 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormHL7DefSegmentEdit.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+
+namespace OpenDental {
+	public partial class FormHL7DefSegmentEdit:Form {
+		public HL7DefSegment HL7DefSeg;
+
+		public FormHL7DefSegmentEdit() {
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		private void butOK_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.OK;
+		}
+
+		private void butCancel_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.Cancel;
+		}
+	}
+}
