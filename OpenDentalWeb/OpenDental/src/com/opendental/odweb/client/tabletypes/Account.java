3ee725c3920c3450467938c8a19f7a0e9d7a4833
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-09-07 21:19:05 +0000

    Added escaping of xml characters from serialized objects.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8393 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentalWeb/OpenDental/src/com/opendental/odweb/client/tabletypes/Account.java
+++ OpenDentalWeb/OpenDental/src/com/opendental/odweb/client/tabletypes/Account.java
@@ -2,6 +2,7 @@ package com.opendental.odweb.client.tabletypes;
 
 import com.google.gwt.xml.client.Document;
 import com.google.gwt.xml.client.XMLParser;
+import com.opendental.odweb.client.remoting.SerializeStringEscapes;
 
 public class Account {
 		/** Primary key. */
@@ -34,9 +35,9 @@ public class Account {
 			StringBuilder sb=new StringBuilder();
 			sb.append("<Account>");
 			sb.append("<AccountNum>").append(AccountNum).append("</AccountNum>");
-			sb.append("<Description>").append(Description).append("</Description>");
+			sb.append("<Description>").append(SerializeStringEscapes.EscapeForXml(Description)).append("</Description>");
 			sb.append("<AcctType>").append(AcctType.ordinal()).append("</AcctType>");
-			sb.append("<BankNumber>").append(BankNumber).append("</BankNumber>");
+			sb.append("<BankNumber>").append(SerializeStringEscapes.EscapeForXml(BankNumber)).append("</BankNumber>");
 			sb.append("<Inactive>").append((Inactive)?1:0).append("</Inactive>");
 			sb.append("<AccountColor>").append(AccountColor).append("</AccountColor>");
 			sb.append("</Account>");
@@ -46,7 +47,7 @@ public class Account {
 		/** Sets the variables for this object based on the values from the XML.
 		 * @param xml The XML passed in must be valid and contain a node for every variable on this object.
 		 * @throws Exception Deserialize is encased in a try catch and will pass any thrown exception on. */
-		public void Deserialize(String xml) throws Exception {
+		public void DeserializeFromXml(String xml) throws Exception {
 			try {
 				Document doc=XMLParser.parse(xml);
 				AccountNum=Integer.valueOf(doc.getElementsByTagName("AccountNum").item(0).getFirstChild().getNodeValue());
