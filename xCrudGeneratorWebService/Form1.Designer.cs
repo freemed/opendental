699115dc561241553e0a579c66c688b4e3fb5a55
cameron <cameron@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-10-31 22:11:58 +0000

    Web Service crud generator, initial commit.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8610 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ xCrudGeneratorWebService/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace xCrudGeneratorWebService {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.butRun = new System.Windows.Forms.Button();
+			this.label10 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(135,9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(421,54);
+			this.label1.TabIndex = 0;
+			this.label1.Text = resources.GetString("label1.Text");
+			// 
+			// butRun
+			// 
+			this.butRun.Location = new System.Drawing.Point(38,32);
+			this.butRun.Name = "butRun";
+			this.butRun.Size = new System.Drawing.Size(75,23);
+			this.butRun.TabIndex = 1;
+			this.butRun.Text = "Run";
+			this.butRun.UseVisualStyleBackColor = true;
+			this.butRun.Click += new System.EventHandler(this.butRun_Click);
+			// 
+			// label10
+			// 
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif",8.25F,System.Drawing.FontStyle.Bold,System.Drawing.GraphicsUnit.Point,((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(135,71);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(421,33);
+			this.label10.TabIndex = 24;
+			this.label10.Text = "This CrudGenerator project MUST be set to compile each time it\'s run even if chan" +
+    "ges were only to OpenDentBusiness.";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F,13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(607,143);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.butRun);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Crud Generator Web Service";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button butRun;
+		private System.Windows.Forms.Label label10;
+	}
+}
+
---
+++ xCrudGeneratorWebService/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+
+namespace xCrudGeneratorWebService {
+	public partial class Form1:Form {
+		private string SerialDir;
+		private const string rn="\r\n";
+		private const string t="\t";
+		private const string t2="\t\t";
+		private const string t3="\t\t\t";
+		private const string t4="\t\t\t\t";
+		private const string t5="\t\t\t\t\t";
+		private const string t6="\t\t\t\t\t\t";
+		private const string t7="\t\t\t\t\t\t\t";
+		private List<Type> TableTypes;
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender,EventArgs e) {
+			SerialDir=@"..\..\..\OpenDentalWebService\Serializing";
+			TableTypes=new List<Type>();
+			Type typeTableBase=typeof(TableBase);
+			Assembly assembly=Assembly.GetAssembly(typeTableBase);
+			foreach(Type typeClass in assembly.GetTypes()){
+				if(typeClass.BaseType==typeTableBase) {
+					if(IsMobile(typeClass)) {
+						continue;
+					}
+					TableTypes.Add(typeClass);	
+				}
+			}
+			TableTypes.Sort(CompareTypesByName);
+		}
+
+		private int CompareTypesByName(Type x, Type y){
+			return x.Name.CompareTo(y.Name);
+		}
+
+		///<summary>This will allow us to skip the mobile types.</summary>
+		private bool IsMobile(Type typeClass) {
+			object[] attributes = typeClass.GetCustomAttributes(typeof(CrudTableAttribute),true);
+			if(attributes.Length==0) {
+				return false;
+			}
+			for(int i=0;i<attributes.Length;i++) {
+				if(attributes[i].GetType()!=typeof(CrudTableAttribute)) {
+					continue;
+				}
+				if(((CrudTableAttribute)attributes[i]).IsMobile) {
+					return true;
+				}
+			}
+			//couldn't find any.
+			return false;
+		}
+
+		private void butRun_Click(object sender,EventArgs e) {
+			Cursor=Cursors.WaitCursor;
+			StringBuilder strb;
+			string className;
+			for(int i=0;i<TableTypes.Count;i++) {
+				className=TableTypes[i].Name;
+				FieldInfo[] fields=null;
+				fields=TableTypes[i].GetFields();
+				strb=new StringBuilder();
+				WriteAll(strb,className,fields);
+				File.WriteAllText(Path.Combine(SerialDir,className+".cs"),strb.ToString());
+				Application.DoEvents();
+			}
+			Cursor=Cursors.Default;
+			MessageBox.Show("Done");
+		}
+
+		///<summary>Example of className is 'Account' or 'Patient'.</summary>
+		private void WriteAll(StringBuilder strb,string className,FieldInfo[] fields) {
+			#region class header
+			strb.Append(@"using System;"+rn
+				+"using System.IO;"+rn
+				+"using System.Text;"+rn
+				+"using System.Xml;"+rn
+				+"using System.Drawing;"+rn
+				+rn+"namespace OpenDentalWebService {"+rn
+				+t+"///<summary>This file is generated automatically by the crud, do not make any changes to this file because they will get overwritten.</summary>"+rn
+				+t+"public class "+className+" {"+rn);
+			#endregion class header
+			#region serialize
+			strb.Append(rn+t2+"///<summary></summary>"+rn
+				+t2+"public static string Serialize(OpenDentBusiness."+className+" "+className.ToLower()+") {"+rn
+				+t3+"StringBuilder sb=new StringBuilder();"+rn
+				+t3+"sb.Append(\"<"+className+">\");"+rn);
+			GetSerialize(strb,className,fields);
+			strb.Append(t3+"sb.Append(\"</"+className+">\");"+rn
+				+t3+"return sb.ToString();"+rn+t2+"}"+rn);
+			#endregion serialize
+			#region deserialize
+			strb.Append(rn+t2+"///<summary></summary>"+rn
+				+t2+"public static OpenDentBusiness."+className+" Deserialize(string xml) {"+rn
+				+t3+"OpenDentBusiness."+className+" "+className.ToLower()+"=new OpenDentBusiness."+className+"();"+rn
+				+t3+"using(XmlReader reader=XmlReader.Create(new StringReader(xml))) {"+rn
+				+t4+"reader.MoveToContent();"+rn
+				+t4+"while(reader.Read()) {"+rn
+				+t5+"//Only detect start elements."+rn
+				+t5+"if(!reader.IsStartElement()) {"+rn
+				+t6+"continue;"+rn
+				+t5+"}"+rn
+				+t5+"switch(reader.Name) {"+rn);
+			GetDeserialize(strb,className,fields);
+			strb.Append(t5+"}"+rn+t4+"}"+rn+t3+"}"+rn+t3+"return "+className.ToLower()+";"+rn+t2+"}"+rn);
+			#endregion deserialize
+			#region footer
+			strb.Append(rn);
+			strb.Append(@"
+	}
+}");
+			#endregion footer
+		}
+
+		///<summary>Fill Serialize</summary>
+		private StringBuilder GetSerialize(StringBuilder strb,string className,FieldInfo[] fields) {
+			foreach(FieldInfo field in fields) {
+				if(IsNotDbColumn(field)) {//if not a db column, skip
+					continue;
+				}
+				strb.Append(t3+"sb.Append(\"<"+field.Name+">\").Append(");
+				if(field.FieldType.IsEnum) {
+					strb.Append("(int)"+className.ToLower()+"."+field.Name+").Append(\"</"+field.Name+">\");"+rn);
+					continue;
+				}
+				switch(field.FieldType.Name) {
+					case "Byte":
+					case "Int32": //int
+					case "Int64": //long
+					case "Single": //float
+					case "Double":
+					case "Interval": //intervals are stored in db as int
+						strb.Append(className.ToLower()+"."+field.Name+").Append(\"</"+field.Name+">\");"+rn);
+						continue;
+					case "String":
+						strb.Append("SerializeStringEscapes.EscapeForXml("+className.ToLower()+"."+field.Name+")).Append(\"</"+field.Name+">\");"+rn);
+						continue;
+					case "Boolean":
+						strb.Append("("+className.ToLower()+"."+field.Name+")?1:0).Append(\"</"+field.Name+">\");"+rn);
+						continue;
+					case "Color":
+						strb.Append(className.ToLower()+"."+field.Name+".ToArgb()).Append(\"</"+field.Name+">\");"+rn);
+						continue;
+					case "TimeSpan":
+						strb.Append(className.ToLower()+"."+field.Name+".ToString()).Append(\"</"+field.Name+">\");"+rn);
+						continue;
+					case "DateTime":
+						strb.Append(className.ToLower()+"."+field.Name+".ToLongDateString()).Append(\"</"+field.Name+">\");"+rn);
+						continue;
+					default:
+						continue;
+				}
+			}
+			return strb;
+		}
+
+		///<summary>Normally false</summary>
+		private bool IsNotDbColumn(FieldInfo field) {
+			object[] attributes = field.GetCustomAttributes(typeof(CrudColumnAttribute),true);
+			if(attributes.Length==0) {
+				return false;
+			}
+			return ((CrudColumnAttribute)attributes[0]).IsNotDbColumn;
+		}
+
+		///<summary></summary>
+		private StringBuilder GetDeserialize(StringBuilder strb,string className,FieldInfo[] fields) {
+			foreach(FieldInfo field in fields) {
+				if(IsNotDbColumn(field)) {//if not a db column, skip
+					continue;
+				}
+				strb.Append(t6+"case \""+field.Name+"\":"+rn
+					+t7+className.ToLower()+"."+field.Name+"=");
+				if(field.FieldType.IsEnum) {
+					strb.Append("(OpenDentBusiness."+field.FieldType.Name+")reader.ReadContentAsInt();"+rn
+						+t7+"break;"+rn);
+					continue;
+				}
+				switch(field.FieldType.Name) {
+					case "Byte":
+						strb.Append("(byte)reader.ReadContentAsInt();"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Interval":
+						strb.Append("new OpenDentBusiness.Interval(reader.ReadContentAsInt());"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Int32": //int
+						strb.Append("reader.ReadContentAsInt();"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Int64": //long
+						strb.Append("reader.ReadContentAsLong();"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Single": //float
+						strb.Append("reader.ReadContentAsFloat();"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Double":
+						strb.Append("reader.ReadContentAsDouble();"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "String":
+						strb.Append("reader.ReadContentAsString();"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Boolean":
+						strb.Append("reader.ReadContentAsString()!=\"0\";"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "Color":
+						strb.Append("Color.FromArgb(reader.ReadContentAsInt());"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "TimeSpan":
+						strb.Append("TimeSpan.Parse(reader.ReadContentAsString());"+rn
+							+t7+"break;"+rn);
+						continue;
+					case "DateTime":
+						strb.Append("DateTime.Parse(reader.ReadContentAsString());"+rn
+							+t7+"break;"+rn);
+						continue;
+					default:
+						continue;
+				}
+			}
+			return strb;
+		}
+
+	}
+}
