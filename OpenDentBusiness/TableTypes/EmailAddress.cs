06926b5efb03a0a94e9dba9fcfde7ca9686df41e
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2012-12-17 17:05:30 +0000

    Started work on email addresses for clinics.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8768 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/TableTypes/EmailAddress.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace OpenDentBusiness {
+	///<summary>Stores all the connection info for one email address.  Linked to clinic by clinic.EmailAddressNum.  Sends email based on patient's clinic.</summary>
+	[Serializable()]
+	public class EmailAddress:TableBase {
+		///<summary>Primary key.</summary>
+		[CrudColumn(IsPriKey=true)]
+		public long EmailAddressNum;
+		///<summary>For example smtp.gmail.com</summary>
+		public string SMPTserver;
+		///<summary>.</summary>
+		public string EmailUsername;
+		///<summary>.</summary>
+		public string EmailPassword;
+		///<summary>Usually 587, sometimes 25</summary>
+		public int ServerPort;
+		///<summary>.</summary>
+		public bool UseSSL;
+		///<summary>The email address of the sender as it should appear to the recipient.</summary>
+		public string SenderAddress;
+
+		///<summary></summary>
+		public EmailAddress Clone() {
+			return (EmailAddress)this.MemberwiseClone();
+		}
+
+	}
+
+
+}
