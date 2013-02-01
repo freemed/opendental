25bf81ba82c3fa144639b63ae31affaee56024d1
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-02-01 01:24:55 +0000

    Added a loading bar.  Also altered deserializing to use a RepeatingCommand which periodically notifies the browser that it's still doing something so that the warning message of the script failing doesn't show.  I'm embarrassed to commit this code but there's only so many asynchronous documents I'm willing to read before I go insane.  Until javascript becomes multi threaded, deferring the commands will have to suffice.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8977 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentalWeb/OpenDental/src/com/opendental/odweb/client/remoting/Db.java
+++ OpenDentalWeb/OpenDental/src/com/opendental/odweb/client/remoting/Db.java
@@ -5,24 +5,49 @@ import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.opendental.odweb.client.remoting.Serializing.DeserializeCallbackResult;
 import com.opendental.odweb.client.ui.MsgBox;
 
 /** This class is used to initiate communication to the server.  It's more like a helper class because communicating with the server gets ugly, especially since we do it so often. */
 public class Db {
 	/** This is the callback that every call to the database should listen to.  Even void requests should instantiate this callback in case an error occurs. */
 	private static RequestCallbackResult RequestCallback;
+	/** This is a callback that will call the RequestCallback.  Sounds redundant but it is necessary because deserializing might take a while and so we need to use a RepeatingCommand.
+	 *  We have to use a repeating command within the Serialize class due to computation that has the possibility to tie up the main thread for more than 10 seconds.  */
+	private static DeserializeCallbackResult DeserializeCallback;
+	/** Simple popup that will have a loading bar so the user knows the program hasn't frozen on them. */
+	private static PopupPanel Popup;
+	
+	/** Simple popup panel showing a loading bar.  The loading spinner was too jittery when deserializing so I went with the bar. */
+	private static void showLoading() {
+		Popup=new PopupPanel();
+		Popup.add(new Image("images/loadingBar.gif"));
+		Popup.setGlassEnabled(true);
+		Popup.center();
+		Popup.show();
+	}
+	
+	private static void hideLoading() {
+		if(Popup!=null) {
+			Popup.hide();
+			Popup=null;
+		}
+	}
 	
 	/** Every call to the database calls this method.  It will pack up the passed in dto request into a RequestBuilder and send it off to the server.
 	 *  If the response from the server is successful, it will call the onSuccess method in the RequestCallbackResult. 
 	 *  @param dtoRequest Pass in any serialized DtoGet request. */
 	public static void sendRequest(String dtoRequest,RequestCallbackResult requestCallback) {
 		RequestCallback=requestCallback;
+		DeserializeCallback=new Deserialize_Callback();
 		RequestBuilder builder=RemotingClient.getRequestBuilder(dtoRequest);
 		try {//Try catch is required around http request.
 			builder.sendRequest(null, new GetRequest());
 		}
 		catch (RequestException e) {
-			MsgBox.show("Error: "+e.getMessage());
+			MsgBox.show("Send request error: "+e.getMessage());
 		}
 	}
 	
@@ -30,15 +55,16 @@ public class Db {
 		public void onResponseReceived(Request request, Response response) {
 			if(response.getStatusCode()==200) {
 				try {
-					// TODO Make sure this works with DtoGetVoid.
-					Object obj=Serializing.getDeserializedObject(response.getText());
-					RequestCallback.onSuccess(obj);
+					showLoading();
+					Serializing.getDeserializedObject(response.getText(),DeserializeCallback);
 				}
 				catch (Exception e) {
+					hideLoading();
 					MsgBox.show(e.getMessage());//This will be a more specific error.
 				}
 			}
 			else {
+				hideLoading();
 				MsgBox.show("Error status text: "+response.getStatusText()
       			+"\r\nError status code: "+Integer.toString(response.getStatusCode())
       			+"\r\nError text: "+response.getText());
@@ -48,6 +74,14 @@ public class Db {
 		public void onError(Request request, Throwable exception) {
 			MsgBox.show(exception.getMessage());
 		}
+	}
+	
+	/** Hides the loading bar and calls the request callback's onSuccess method. */
+	private static class Deserialize_Callback implements DeserializeCallbackResult {
+		public void onComplete(Object obj) {
+			hideLoading();
+			RequestCallback.onSuccess(obj);
+		}		
 	}
 	
 	/** Every request sent to the database will result in a call being made to this callback.  This is the callback that every window should listen to when making a call to the database for their result. */
