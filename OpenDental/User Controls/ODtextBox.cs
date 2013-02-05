ca72c016d1418a20a249a3cba60c02bf285c2dda
cameron <cameron@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-02-05 22:26:24 +0000

    Save commit of spell check feature.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@8992 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/User Controls/ODtextBox.cs
+++ OpenDental/User Controls/ODtextBox.cs
@@ -9,19 +9,23 @@ using NHunspell;
 using System.Text.RegularExpressions;
 using OpenDental.UI;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace OpenDental
 {
 	/// <summary>This is used instead of a regular textbox when quickpaste functionality is needed.</summary>
 	public class ODtextBox : RichTextBox{//System.ComponentModel.Component
 		private System.Windows.Forms.ContextMenu contextMenu;
-		private System.ComponentModel.Container components = null;// Required designer variable.
+		private IContainer components;// Required designer variable.
 		private static Hunspell HunspellGlobal;//We create this object one time for every instance of this textbox control within the entire program.
 		private QuickPasteType quickPasteType;
 		private List<string> ListCorrect;
 		private List<string> ListIncorrect;
-		private Bitmap BitmapOverlay;//TODO: dispose when control is destroyed.
-		private Graphics BufferGraphics;//TODO: dispose when control is destroyed.
+		private Graphics BufferGraphics;
+		private Timer timer1;
+		private Point PositionOfClick;
+		private ReplaceWord ReplWord;
+
 		/*public ODtextBox(System.ComponentModel.IContainer container)
 		{
 			///
@@ -45,6 +49,14 @@ namespace OpenDental
 			ListIncorrect=new List<string>();
 			EventHandler onClick=new EventHandler(menuItem_Click);
 			MenuItem menuItem;
+			contextMenu.MenuItems.Add("",onClick);//These five menu items will hold the suggested spelling for misspelled words.  If no misspelled words, they will not be visible.
+			contextMenu.MenuItems.Add("",onClick);
+			contextMenu.MenuItems.Add("",onClick);
+			contextMenu.MenuItems.Add("",onClick);
+			contextMenu.MenuItems.Add("",onClick);
+			contextMenu.MenuItems.Add("-");
+			contextMenu.MenuItems.Add("Add to Dictionary",onClick);
+			contextMenu.MenuItems.Add("-");
 			menuItem=new MenuItem(Lan.g(this,"Insert Date"),onClick,Shortcut.CtrlD);
 			contextMenu.MenuItems.Add(menuItem);
 			menuItem=new MenuItem(Lan.g(this,"Insert Quick Note"),onClick,Shortcut.CtrlQ);
@@ -71,10 +83,6 @@ namespace OpenDental
 					BufferGraphics.Dispose();
 					BufferGraphics=null;
 				}
-				if(BitmapOverlay!=null) {
-					BitmapOverlay.Dispose();
-					BitmapOverlay=null;
-				}
 				//We do not dispose the hunspell object because it will be automatially disposed of when the program closes.
 			}
 			base.Dispose( disposing );
@@ -88,18 +96,27 @@ namespace OpenDental
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.contextMenu = new System.Windows.Forms.ContextMenu();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
 			// 
 			// contextMenu
 			// 
 			this.contextMenu.Popup += new System.EventHandler(this.contextMenu_Popup);
 			// 
+			// timer1
+			// 
+			this.timer1.Interval = 500;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
 			// ODtextBox
 			// 
-			//this.AcceptsReturn = true;
 			this.ContextMenu = this.contextMenu;
-			this.Multiline = true;
 			this.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+			this.VScroll += new System.EventHandler(this.ODtextBox_VScroll);
+			this.ResumeLayout(false);
+
 		}
 		#endregion
 
@@ -115,14 +132,91 @@ namespace OpenDental
 		}
 
 		private void contextMenu_Popup(object sender, System.EventArgs e) {
-			if(SelectionLength==0){
-				contextMenu.MenuItems[3].Enabled=false;//cut
-				contextMenu.MenuItems[4].Enabled=false;//copy
+			if(SelectionLength==0) {
+				contextMenu.MenuItems[11].Enabled=false;//cut
+				contextMenu.MenuItems[12].Enabled=false;//copy
 			}
-			else{
-				contextMenu.MenuItems[3].Enabled=true;
-				contextMenu.MenuItems[4].Enabled=true;
+			else {
+				contextMenu.MenuItems[11].Enabled=true;
+				contextMenu.MenuItems[12].Enabled=true;
 			}
+			if(!IsOnMisspelled(PositionOfClick)) {//did not click on a misspelled word
+				contextMenu.MenuItems[0].Visible=false;//suggestion 1
+				contextMenu.MenuItems[1].Visible=false;//suggestion 2
+				contextMenu.MenuItems[2].Visible=false;//suggestion 3
+				contextMenu.MenuItems[3].Visible=false;//suggestion 4
+				contextMenu.MenuItems[4].Visible=false;//suggestion 5
+				contextMenu.MenuItems[5].Visible=false;//contextMenu separator
+				contextMenu.MenuItems[6].Visible=false;//Add to Dictionary
+				contextMenu.MenuItems[7].Visible=false;//separator
+			}
+			else if(IsOnMisspelled(PositionOfClick)) {//clicked on or near a misspelled word
+				List<string> suggestions=SpellSuggest();
+				if(suggestions.Count==0) {//no suggestions
+					contextMenu.MenuItems[0].Text=Lan.g(this,"No Spelling Suggestions");
+					contextMenu.MenuItems[0].Visible=true;
+					contextMenu.MenuItems[0].Enabled=false;//suggestion 1 set to "No Spelling Suggestions"
+					contextMenu.MenuItems[1].Visible=false;//suggestion 2
+					contextMenu.MenuItems[2].Visible=false;//suggestion 3
+					contextMenu.MenuItems[3].Visible=false;//suggestion 4
+					contextMenu.MenuItems[4].Visible=false;//suggestion 5
+				}
+				else {
+					for(int i=0;i<5;i++) {//Only display first 5 suggestions if available
+						if(i>=suggestions.Count) {
+							contextMenu.MenuItems[i].Visible=false;
+							continue;
+						}
+						contextMenu.MenuItems[i].Text=suggestions[i];
+						contextMenu.MenuItems[i].Visible=true;
+						contextMenu.MenuItems[i].Enabled=true;
+					}
+				}
+				contextMenu.MenuItems[5].Visible=true;//contextMenu separator, will display whether or not there is a suggestion for the misspelled word
+				contextMenu.MenuItems[6].Visible=true;
+				contextMenu.MenuItems[7].Visible=true;
+			}
+		}
+
+		///<summary>Determines whether the right click was on a misspelled word.</summary>
+		private bool IsOnMisspelled(Point PositionOfClick) {
+			string[] words=Regex.Split(Text,"([\\s])");
+			if(words.Length==0) {//no text so just return false, not on a misspelled word
+				return false;
+			}
+			int charIndex=this.GetCharIndexFromPosition(PositionOfClick);
+			Point charLocation=this.GetPositionFromCharIndex(charIndex);
+			if(PositionOfClick.Y<charLocation.Y-2 || PositionOfClick.Y>charLocation.Y+this.FontHeight+2) {//this is the closest char but they were not very close when they right clicked
+				return false;
+			}
+			char c=this.GetCharFromPosition(PositionOfClick);
+			if(c=='\n') {//if closest char is a new line char, then assume not on a misspelled word
+				return false;
+			}
+			int startIndex=0;
+			ReplWord=new ReplaceWord();
+			for(int i=0;i<words.Length;i++) {//after this for loop we should always have the word closest to where the user clicked.
+				if(words[i].Length==0) {
+					continue;
+				}
+				int endIndex=startIndex+words[i].Length-1;
+				if(charIndex>=startIndex && charIndex<=endIndex) {//this is our word
+					ReplWord.word=words[i];
+					ReplWord.startIndex=startIndex;
+					ReplWord.endIndex=endIndex;
+					break;
+				}
+				startIndex=startIndex+words[i].Length;
+			}
+			if(ListIncorrect.Contains(ReplWord.word)) {
+				return true;
+			}
+			return false;
+		}
+
+		private List<string> SpellSuggest() {
+			List<string> suggestions=HunspellGlobal.Suggest(ReplWord.word);
+			return suggestions;
 		}
 
 		protected override void OnMouseDown(MouseEventArgs e) {
@@ -130,28 +224,58 @@ namespace OpenDental
 				this.Focus();
 			}
 			base.OnMouseDown(e);
+			PositionOfClick=new Point(e.X,e.Y);
 		}
 
 		private void menuItem_Click(object sender,System.EventArgs e) {
 			switch(contextMenu.MenuItems.IndexOf((MenuItem)sender)){
 				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+					int originalCaret=this.SelectionStart;
+					this.Text=this.Text.Remove(ReplWord.startIndex,ReplWord.word.Length);
+					this.Text=this.Text.Insert(ReplWord.startIndex,contextMenu.MenuItems[contextMenu.MenuItems.IndexOf((MenuItem)sender)].Text);
+					if(this.Text.Length<=originalCaret) {
+						this.SelectionStart=this.Text.Length;
+					}
+					else {
+						this.SelectionStart=originalCaret;
+					}
+					timer1.Start();
+					break;
+				//case 5 is separator
+				case 6:
+					string newWord=Regex.Replace(ReplWord.word,"[\\s]|[\\p{P}\\p{S}-['-]]","");//don't allow words with spaces or punctuation except ' and - in them
+					//we don't need to make sure it is not already in the custom dict, since if it was it would not be underlined and this menu item could not be selected
+					DictCustom word=new DictCustom();
+					word.WordText=newWord;
+					DictCustoms.Insert(word);
+					DataValid.SetInvalid(InvalidType.DictCustoms);
+					ListIncorrect.Remove(ReplWord.word);
+					ListCorrect.Add(ReplWord.word);
+					timer1.Start();
+					break;
+				//case 7 is separator
+				case 8:
 					InsertDate();
 					break;
-				case 1://Insert Quick Note
+				case 9://Insert Quick Note
 					ShowFullDialog();
 					break;
-				//case 2 is separator
-				case 3://cut
+				//case 10 is separator
+				case 11://cut
 					Clipboard.SetDataObject(SelectedText);
 					int caretPos=SelectionStart;
 					Text=Text.Remove(SelectionStart,SelectionLength);
 					SelectionStart=caretPos;
 					SelectionLength=0;
 					break;
-				case 4://copy
+				case 12://copy
 					Clipboard.SetDataObject(SelectedText);
 					break;
-				case 5://paste
+				case 13://paste
 					if(!Clipboard.ContainsText()) {
 					  MsgBox.Show(this,"There is no text on the clipboard.");
 					  break;
@@ -165,13 +289,56 @@ namespace OpenDental
 					string strPaste=(string)iData.GetData(DataFormats.Text); 
 					Text=Text.Insert(caret,strPaste);
 					SelectionStart=caret+strPaste.Length;
+					timer1.Start();
 					break;
 			}
 		}
 
+		private void timer1_Tick(object sender,EventArgs e) {
+			timer1.Stop();
+			CheckSpelling();
+		}
+
+		private void ODtextBox_VScroll(object sender,EventArgs e) {
+			timer1.Stop();
+			timer1.Start();
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e) {
+			base.OnKeyDown(e);
+			Bitmap BitmapOverlay=new Bitmap(this.Width,this.Height);
+			BufferGraphics=Graphics.FromImage(BitmapOverlay);
+			BufferGraphics.Clear(Color.Transparent);//We don't want to overwrite the text in the rich text box.
+			ClearWavyLines(BufferGraphics);
+			Graphics graphicsTextBox=Graphics.FromHwnd(this.Handle);
+			graphicsTextBox.DrawImageUnscaled(BitmapOverlay,0,0);
+			graphicsTextBox.Dispose();
+			BitmapOverlay.Dispose();
+		}
+
+		private void ClearWavyLines(Graphics bufferGraphics) {
+			string[] words=Regex.Split(Text,"([\\s])");
+			int startInd=0;
+			for(int i=0;i<words.Length;i++) {//go through each word and use GetPositionFromCharIndex to determine the y position to draw our white box
+				Point start=this.GetPositionFromCharIndex(startInd);//get pos of character at startInd, y could be negative if scroll bar active
+				start.Y=start.Y+this.FontHeight;
+				if(start.Y<0) {
+					startInd=startInd+words[i].Length;
+					continue;
+				}
+				if(start.Y>=this.Height) {
+					break;
+				}
+				Rectangle wavyLineArea=new Rectangle(start.X,start.Y,this.Width,2);
+			  bufferGraphics.FillRectangle(Brushes.White,wavyLineArea);
+				startInd=startInd+words[i].Length;
+			}
+		}
+		
 		///<summary></summary>
 		protected override void OnKeyUp(KeyEventArgs e) {
 			base.OnKeyUp(e);
+			timer1.Stop();
 			int originalLength=base.Text.Length;
 			int originalCaret=base.SelectionStart;
 			string newText=QuickPasteNotes.Substitute(Text,quickPasteType);
@@ -180,42 +347,25 @@ namespace OpenDental
 				SelectionStart=originalCaret+Text.Length-originalLength;
 			}
 			//then CtrlQ
-			if(e.KeyCode==Keys.Q && e.Modifiers==Keys.Control){
+			if(e.KeyCode==Keys.Q && e.Modifiers==Keys.Control) {
 				ShowFullDialog();
 			}
-			if(this.BitmapOverlay==null) {
-				this.BitmapOverlay=new Bitmap(this.Width,this.Height);
-				BufferGraphics=Graphics.FromImage(this.BitmapOverlay);
-				BufferGraphics.Clear(Color.Transparent);//We don't want to overwrite the text in the rich text box.
-			}			
+			timer1.Start();
+		}
+
+		private void CheckSpelling() {
+			Bitmap BitmapOverlay=new Bitmap(this.Width,this.Height);
+			BufferGraphics=Graphics.FromImage(BitmapOverlay);
+			BufferGraphics.Clear(Color.Transparent);//We don't want to overwrite the text in the rich text box.
 			ClearWavyLines(BufferGraphics);
-			checkSpelling();
+			SpellCheck();
 			Graphics graphicsTextBox=Graphics.FromHwnd(this.Handle);
-			Application.DoEvents();
 			graphicsTextBox.DrawImageUnscaled(BitmapOverlay,0,0);
 			graphicsTextBox.Dispose();
+			BitmapOverlay.Dispose();
 		}
 
-		private void ShowFullDialog(){
-			FormQuickPaste FormQ=new FormQuickPaste();
-			FormQ.TextToFill=this;
-			FormQ.QuickType=quickPasteType;
-			FormQ.ShowDialog();
-		}
-
-		private void ClearWavyLines(Graphics bufferGraphics) {
-			Point start=this.GetPositionFromCharIndex(0);
-			int numLines=(this.Height/this.FontHeight)+1;
-			int y=start.Y+this.FontHeight;//Start below the first line of text.
-			for(int i=0;i<numLines;i++) {
-				Rectangle wavyLineArea=new Rectangle(start.X,y,this.Width,3);
-				bufferGraphics.FillRectangle(Brushes.White,wavyLineArea);
-				y+=this.FontHeight;
-			}
-		}
-
-		private void checkSpelling() {
-			int curPos=base.SelectionStart;
+		private void SpellCheck() {
 			string[] words=Regex.Split(Text,"([\\s])");
 			if(words.Length==0) {//deleted all text, just return
 				return;
@@ -224,19 +374,20 @@ namespace OpenDental
 			for(int i=0;i<words.Length;i++) {
 				noPunctWords[i]=Regex.Replace(words[i],"[\\p{P}\\p{S}-['-]]","");
 			}
-			Clear();
+			int startPos=0;
 			for(int i=0;i<noPunctWords.Length;i++) {
-				int textLength=Text.Length;//length before appending word
-				this.AppendText(words[i]);
 				bool correct=false;
-				if(noPunctWords[i].Length==0) {
+				if(noPunctWords[i].Length==0) {//noPunctWord's length might be zero but the length with punctuation could be more than zero, so move startPos forward the length of word
+					startPos=startPos+words[i].Length;
 					continue;
 				}
 				if(ListCorrect.Contains(words[i])) {
+					startPos=startPos+words[i].Length;
 					continue;
 				}
 				if(ListIncorrect.Contains(words[i])) {
-					CustomPaint(textLength,textLength+words[i].Length);
+					DrawWave(startPos,startPos+words[i].Length);
+					startPos=startPos+words[i].Length;
 					continue;
 				}
 				for(int w=0;w<DictCustoms.Listt.Count;w++) {//compare to custom word list
@@ -249,32 +400,31 @@ namespace OpenDental
 					correct=HunspellGlobal.Spell(noPunctWords[i]);
 				}
 				if(!correct) {
-					CustomPaint(textLength,textLength+words[i].Length);
+					DrawWave(startPos,startPos+words[i].Length);
 					ListIncorrect.Add(words[i]);
 				}
 				else {//if it gets here, the word was spelled correctly, determined by comparing to the custom word list and/or the hunspell dict
 					ListCorrect.Add(words[i]);
 				}
+				startPos=startPos+words[i].Length;
 			}
-			base.SelectionStart=curPos;
 			return;
 		}
 
-		private void CustomPaint(int startIndex,int endIndex) {
-			Point start=this.GetPositionFromCharIndex(startIndex);
-			Point end=this.GetPositionFromCharIndex(endIndex);
-			start.Y=start.Y+this.FontHeight;
-			end.Y=start.Y;
-			DrawWave(start,end);
-		}
-
-		private void DrawWave(Point start,Point end) {
+		private void DrawWave(int startIndex,int endIndex) {
+			Point start=this.GetPositionFromCharIndex(startIndex);//accounts for scroll position
+			Point end=this.GetPositionFromCharIndex(endIndex);//accounts for scroll position
+			start.Y=start.Y+this.FontHeight;//move from top of line to bottom of line
+			end.Y=start.Y;//move from top of line to bottom of line
+			if(start.Y<=4 || start.Y>=this.Height) {//Don't draw lines for text which is currently not visible.
+				return;
+			}
 			Pen pen=Pens.Red;
 			if((end.X-start.X)>4) {
 				ArrayList pl=new ArrayList();
 				for(int i=start.X;i<=(end.X-2);i=i+4) {
 					pl.Add(new Point(i,start.Y));
-					pl.Add(new Point(i+2,start.Y+2));
+					pl.Add(new Point(i+2,start.Y+1));
 				}
 				Point[] p=(Point[])pl.ToArray(typeof(Point));
 				BufferGraphics.DrawLines(pen,p);
@@ -284,6 +434,13 @@ namespace OpenDental
 			}
 		}
 
+		private void ShowFullDialog(){
+			FormQuickPaste FormQ=new FormQuickPaste();
+			FormQ.TextToFill=this;
+			FormQ.QuickType=quickPasteType;
+			FormQ.ShowDialog();
+		}
+
 		private void InsertDate(){
 			int caret=SelectionStart;
 			string strPaste=DateTime.Today.ToShortDateString();
@@ -291,10 +448,11 @@ namespace OpenDental
 			SelectionStart=caret+strPaste.Length;
 		}
 
-		
-
-
-
+		private class ReplaceWord {
+			public string word="";
+			public int startIndex=0;
+			public int endIndex=0;
+		}
 
 	}
 }
