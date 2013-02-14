803746d3b8cdcb0e181199b63e7646c521c72794
cameron <cameron@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-02-14 16:58:04 +0000

    Enhancement, wiki pages can now hold 1,000,000 characters.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9042 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/User Controls/TextBoxWiki.Designer.cs
+++ OpenDental/User Controls/TextBoxWiki.Designer.cs
@@ -31,13 +31,14 @@
 			this.textBoxMain.AcceptsReturn = true;
 			this.textBoxMain.AcceptsTab = true;
 			this.textBoxMain.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.textBoxMain.Font = new System.Drawing.Font("Courier New",9F,System.Drawing.FontStyle.Regular,System.Drawing.GraphicsUnit.Point,((byte)(0)));
+			this.textBoxMain.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBoxMain.HideSelection = false;
-			this.textBoxMain.Location = new System.Drawing.Point(0,0);
+			this.textBoxMain.Location = new System.Drawing.Point(0, 0);
+			this.textBoxMain.MaxLength = 1000000;
 			this.textBoxMain.Multiline = true;
 			this.textBoxMain.Name = "textBoxMain";
 			this.textBoxMain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.textBoxMain.Size = new System.Drawing.Size(150,150);
+			this.textBoxMain.Size = new System.Drawing.Size(150, 150);
 			this.textBoxMain.TabIndex = 0;
 			this.textBoxMain.Text = "this is a test";
 			this.textBoxMain.TextChanged += new System.EventHandler(this.textBoxMain_TextChanged);
