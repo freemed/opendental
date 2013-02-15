0b41626eba5f0ec69b76d8744227f510ee1ff1b7
cameron <cameron@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-02-15 22:19:39 +0000

    Pressing the enter key inside a text box for a new line will cause forms to close sometimes. Also enhanced spell checking to parse words more accurately and underline accurately when misspelled word spanned more than one line.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9048 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/User Controls/ODtextBox.cs
+++ OpenDental/User Controls/ODtextBox.cs
@@ -24,8 +24,8 @@ namespace OpenDental
 		private Graphics BufferGraphics;
 		private Timer timer1;
 		private Point PositionOfClick;
-		private ReplaceWord ReplWord;
-		private bool spellCheckIsEnabled;
+		private Group ReplWord;
+		private bool spellCheckIsEnabled;//set to true in constructor
 
 		///<summary>Set true to enable spell checking in this control.</summary>
 		[Category("Behavior"),Description("Set true to enable spell checking.")]
@@ -53,6 +53,8 @@ namespace OpenDental
 		public ODtextBox(){
 			InitializeComponent();// Required for Windows.Forms Class Composition Designer support
 			spellCheckIsEnabled=true;
+			this.AcceptsTab=true;//Causes CR to not also trigger OK button on a form when that button is set as AcceptButton on the form.
+			this.DetectUrls=false;
 			if(System.ComponentModel.LicenseManager.UsageMode!=System.ComponentModel.LicenseUsageMode.Designtime
 				&& HunspellGlobal==null)
 			{
@@ -71,6 +73,7 @@ namespace OpenDental
 			contextMenu.MenuItems.Add("",onClick);
 			contextMenu.MenuItems.Add("-");
 			contextMenu.MenuItems.Add("Add to Dictionary",onClick);
+			contextMenu.MenuItems.Add("Disable Spell Check",onClick);
 			contextMenu.MenuItems.Add("-");
 			menuItem=new MenuItem(Lan.g(this,"Insert Date"),onClick,Shortcut.CtrlD);
 			contextMenu.MenuItems.Add(menuItem);
@@ -148,14 +151,14 @@ namespace OpenDental
 
 		private void contextMenu_Popup(object sender, System.EventArgs e) {
 			if(SelectionLength==0) {
-				contextMenu.MenuItems[11].Enabled=false;//cut
-				contextMenu.MenuItems[12].Enabled=false;//copy
+				contextMenu.MenuItems[12].Enabled=false;//cut
+				contextMenu.MenuItems[13].Enabled=false;//copy
 			}
 			else {
-				contextMenu.MenuItems[11].Enabled=true;
 				contextMenu.MenuItems[12].Enabled=true;
+				contextMenu.MenuItems[13].Enabled=true;
 			}
-			if(!IsOnMisspelled(PositionOfClick)) {//did not click on a misspelled word
+			if(!IsOnMisspelled(PositionOfClick) || !PrefC.GetBool(PrefName.SpellCheckIsEnabled)) {//did not click on a misspelled word OR spell check is disabled
 				contextMenu.MenuItems[0].Visible=false;//suggestion 1
 				contextMenu.MenuItems[1].Visible=false;//suggestion 2
 				contextMenu.MenuItems[2].Visible=false;//suggestion 3
@@ -163,9 +166,10 @@ namespace OpenDental
 				contextMenu.MenuItems[4].Visible=false;//suggestion 5
 				contextMenu.MenuItems[5].Visible=false;//contextMenu separator
 				contextMenu.MenuItems[6].Visible=false;//Add to Dictionary
-				contextMenu.MenuItems[7].Visible=false;//separator
+				contextMenu.MenuItems[7].Visible=false;//Disable Spell Check
+				contextMenu.MenuItems[8].Visible=false;//separator
 			}
-			else if(IsOnMisspelled(PositionOfClick)) {//clicked on or near a misspelled word
+			else if(IsOnMisspelled(PositionOfClick) && PrefC.GetBool(PrefName.SpellCheckIsEnabled)) {//clicked on or near a misspelled word AND spell check is enabled
 				List<string> suggestions=SpellSuggest();
 				if(suggestions.Count==0) {//no suggestions
 					contextMenu.MenuItems[0].Text=Lan.g(this,"No Spelling Suggestions");
@@ -188,49 +192,41 @@ namespace OpenDental
 					}
 				}
 				contextMenu.MenuItems[5].Visible=true;//contextMenu separator, will display whether or not there is a suggestion for the misspelled word
-				contextMenu.MenuItems[6].Visible=true;
-				contextMenu.MenuItems[7].Visible=true;
+				contextMenu.MenuItems[6].Visible=true;//Add to Dictionary
+				contextMenu.MenuItems[7].Visible=true;//Disable Spell Check
+				contextMenu.MenuItems[8].Visible=true;//contextMenu separator
 			}
 		}
 
-		///<summary>Determines whether the right click was on a misspelled word.</summary>
+		///<summary>Determines whether the right click was on a misspelled word.  Also sets the start and end index of chars to be replaced in text.</summary>
 		private bool IsOnMisspelled(Point PositionOfClick) {
-			string[] words=Regex.Split(Text,"([\\s])");
-			if(words.Length==0) {//no text so just return false, not on a misspelled word
-				return false;
-			}
+			MatchCollection words=GetWords();
 			int charIndex=this.GetCharIndexFromPosition(PositionOfClick);
 			Point charLocation=this.GetPositionFromCharIndex(charIndex);
 			if(PositionOfClick.Y<charLocation.Y-2 || PositionOfClick.Y>charLocation.Y+this.FontHeight+2) {//this is the closest char but they were not very close when they right clicked
-				return false;
+			  return false;
 			}
 			char c=this.GetCharFromPosition(PositionOfClick);
 			if(c=='\n') {//if closest char is a new line char, then assume not on a misspelled word
-				return false;
+			  return false;
 			}
-			int startIndex=0;
-			ReplWord=new ReplaceWord();
-			for(int i=0;i<words.Length;i++) {//after this for loop we should always have the word closest to where the user clicked.
-				if(words[i].Length==0) {
-					continue;
-				}
-				int endIndex=startIndex+words[i].Length-1;
-				if(charIndex>=startIndex && charIndex<=endIndex) {//this is our word
-					ReplWord.Word=words[i];
-					ReplWord.StartIndex=startIndex;
-					ReplWord.EndIndex=endIndex;
+			foreach(Match m in words) {
+			  Group word=m.Groups[1];//Group 0 is the entire match, group 1 is our word, group 2 is our word without the first character (determined by parentheses).
+			  int startIndex=word.Index;//word.Index is relative to Text.
+			  int endIndex=startIndex+word.Length-1;
+			  if(charIndex>=startIndex && charIndex<=endIndex) {//this is our word
+					ReplWord=word;
 					break;
-				}
-				startIndex=startIndex+words[i].Length;
+			  }
 			}
-			if(ListIncorrect.Contains(ReplWord.Word)) {
+			if(ListIncorrect.Contains(ReplWord.Value)) {
 				return true;
 			}
 			return false;
 		}
 
 		private List<string> SpellSuggest() {
-			List<string> suggestions=HunspellGlobal.Suggest(ReplWord.Word);
+			List<string> suggestions=HunspellGlobal.Suggest(ReplWord.Value);
 			return suggestions;
 		}
 
@@ -250,8 +246,8 @@ namespace OpenDental
 				case 3:
 				case 4:
 					int originalCaret=this.SelectionStart;
-					this.Text=this.Text.Remove(ReplWord.StartIndex,ReplWord.Word.Length);
-					this.Text=this.Text.Insert(ReplWord.StartIndex,contextMenu.MenuItems[contextMenu.MenuItems.IndexOf((MenuItem)sender)].Text);
+					this.Text=this.Text.Remove(ReplWord.Index,ReplWord.Value.Length);
+					this.Text=this.Text.Insert(ReplWord.Index,contextMenu.MenuItems[contextMenu.MenuItems.IndexOf((MenuItem)sender)].Text);
 					if(this.Text.Length<=originalCaret) {
 						this.SelectionStart=this.Text.Length;
 					}
@@ -262,35 +258,50 @@ namespace OpenDental
 					break;
 				//case 5 is separator
 				case 6://Add to dict
-					string newWord=Regex.Replace(ReplWord.Word,"[\\s]|[\\p{P}\\p{S}-['-]]","");//don't allow words with spaces or punctuation except ' and - in them
+					string newWord=ReplWord.Value;
 					//guaranteed to not already exist in custom dictionary, or it wouldn't be underlined.
 					DictCustom word=new DictCustom();
 					word.WordText=newWord;
 					DictCustoms.Insert(word);
 					DataValid.SetInvalid(InvalidType.DictCustoms);
-					ListIncorrect.Remove(ReplWord.Word);
-					ListCorrect.Add(ReplWord.Word);
+					ListIncorrect.Remove(ReplWord.Value);
+					ListCorrect.Add(ReplWord.Value);
 					timer1.Start();
 					break;
-				//case 7 is separator
-				case 8:
+				case 7://Disable spell check
+					if(!MsgBox.Show(this,MsgBoxButtons.OKCancel,"This will disable spell checking.  To re-enable, go to Setup | Spell Check and check the \"Spell Check Enabled\" box.")) {
+						break;
+					}
+					Prefs.UpdateBool(PrefName.SpellCheckIsEnabled,false);
+					DataValid.SetInvalid(InvalidType.Prefs);
+					Bitmap BitmapOverlay=new Bitmap(this.Width,this.Height);//Clear wavy lines
+					BufferGraphics=Graphics.FromImage(BitmapOverlay);
+					BufferGraphics.Clear(Color.Transparent);//We don't want to overwrite the text in the rich text box.
+					ClearWavyLines(BufferGraphics);
+					Graphics graphicsTextBox=Graphics.FromHwnd(this.Handle);
+					graphicsTextBox.DrawImageUnscaled(BitmapOverlay,0,0);
+					graphicsTextBox.Dispose();
+					BitmapOverlay.Dispose();
+					break;
+				//case 8 is separator
+				case 9:
 					InsertDate();
 					break;
-				case 9://Insert Quick Note
+				case 10://Insert Quick Note
 					ShowFullDialog();
 					break;
-				//case 10 is separator
-				case 11://cut
+				//case 11 is separator
+				case 12://cut
 					Clipboard.SetDataObject(SelectedText);
 					int caretPos=SelectionStart;
 					Text=Text.Remove(SelectionStart,SelectionLength);
 					SelectionStart=caretPos;
 					SelectionLength=0;
 					break;
-				case 12://copy
+				case 13://copy
 					Clipboard.SetDataObject(SelectedText);
 					break;
-				case 13://paste
+				case 14://paste
 					if(!Clipboard.ContainsText()) {
 					  MsgBox.Show(this,"There is no text on the clipboard.");
 					  break;
@@ -324,6 +335,9 @@ namespace OpenDental
 
 		protected override void OnKeyDown(KeyEventArgs e) {
 			base.OnKeyDown(e);
+			if(!PrefC.GetBool(PrefName.SpellCheckIsEnabled)) {//Only spell check if enabled
+				return;
+			}
 			Bitmap BitmapOverlay=new Bitmap(this.Width,this.Height);
 			BufferGraphics=Graphics.FromImage(BitmapOverlay);
 			BufferGraphics.Clear(Color.Transparent);//We don't want to overwrite the text in the rich text box.
@@ -372,6 +386,9 @@ namespace OpenDental
 		}
 
 		private void CheckSpelling() {
+			if(!PrefC.GetBool(PrefName.SpellCheckIsEnabled)) {//Only spell check if enabled
+				return;
+			}
 			Bitmap BitmapOverlay=new Bitmap(this.Width,this.Height);
 			BufferGraphics=Graphics.FromImage(BitmapOverlay);
 			BufferGraphics.Clear(Color.Transparent);//We don't want to overwrite the text in the rich text box.
@@ -383,72 +400,95 @@ namespace OpenDental
 			BitmapOverlay.Dispose();
 		}
 
+		///<summary>Performs spell checking against indiviudal words against the English USA dictionary.</summary>
 		private void SpellCheck() {
-			string[] words=Regex.Split(Text,"([\\s])");
-			if(words.Length==0) {//deleted all text, just return
-				return;
-			}
-			string[] noPunctWords=new string[words.Length];
-			for(int i=0;i<words.Length;i++) {
-				noPunctWords[i]=Regex.Replace(words[i],"[\\p{P}\\p{S}-['-]]","");
-			}
-			int startPos=0;
-			for(int i=0;i<noPunctWords.Length;i++) {
+			//Matches a word containing at least one alpha-numeric and might include punctuation in the middle of the word.
+			MatchCollection words=GetWords();
+			foreach(Match m in words) {
+				Group word=m.Groups[1];//Group 0 is the entire match, group 1 is our word, group 2 is our word without the first character (determined by parentheses).
 				bool correct=false;
-				if(noPunctWords[i].Length==0) {//noPunctWord's length might be zero but the length with punctuation could be more than zero, so move startPos forward the length of word
-					startPos=startPos+words[i].Length;
-					continue;
+				if(ListCorrect.Contains(word.Value)) {
+					continue;//Spelled correctly
 				}
-				if(ListCorrect.Contains(words[i])) {
-					startPos=startPos+words[i].Length;
-					continue;
-				}
-				if(ListIncorrect.Contains(words[i])) {
-					DrawWave(startPos,startPos+words[i].Length);
-					startPos=startPos+words[i].Length;
-					continue;
+				int startIndex=word.Index;//word.Index is relative to Text.
+				int endIndex=startIndex+word.Length;//One spot past the end of the word, because DrawWave() draws to the beginning of the character of the endIndex.
+				if(ListIncorrect.Contains(word.Value)) {
+					DrawWave(startIndex,endIndex);
+					continue;//Spelled incorrectly
 				}
 				for(int w=0;w<DictCustoms.Listt.Count;w++) {//compare to custom word list
-					if(DictCustoms.Listt[w].WordText.ToLower()==noPunctWords[i].ToLower()) {//convert to lower case before comparing
+					if(DictCustoms.Listt[w].WordText.ToLower()==word.Value.ToLower()) {//convert to lower case before comparing
 						correct=true;
 						break;
 					}
 				}
 				if(!correct) {//Not in custom dictionary, so spell check
-					correct=HunspellGlobal.Spell(noPunctWords[i]);
+					correct=HunspellGlobal.Spell(word.Value);
 				}
 				if(!correct) {
-					DrawWave(startPos,startPos+words[i].Length);
-					ListIncorrect.Add(words[i]);
+					DrawWave(startIndex,endIndex);
+					ListIncorrect.Add(word.Value);
 				}
 				else {//if it gets here, the word was spelled correctly, determined by comparing to the custom word list and/or the hunspell dict
-					ListCorrect.Add(words[i]);
+					ListCorrect.Add(word.Value);
 				}
-				startPos=startPos+words[i].Length;
 			}
-			return;
+		}
+
+		///<summary>Determines individual words by matching regular expression pattern.  Pattern is 0 or more non word characters, followed by a word character (defined as [a-zA-Z0-9_]), followed by 0 or 1 (non-white space chars followed by a word char), followed by 0 or more non-word chars.  So a word has to begin and end with a word character (could be the same char) and in between those chars there can be any number of chars as long as they are not white space.</summary>
+		private MatchCollection GetWords() {
+			return Regex.Matches(Text,@"\W*(\w([\S-[/\\]]*\w)?)\W*",RegexOptions.Multiline|RegexOptions.CultureInvariant);
 		}
 
 		private void DrawWave(int startIndex,int endIndex) {
 			Point start=this.GetPositionFromCharIndex(startIndex);//accounts for scroll position
 			Point end=this.GetPositionFromCharIndex(endIndex);//accounts for scroll position
 			start.Y=start.Y+this.FontHeight;//move from top of line to bottom of line
-			end.Y=start.Y;//move from top of line to bottom of line
+			end.Y=end.Y+this.FontHeight;//move from top of line to bottom of line
 			if(start.Y<=4 || start.Y>=this.Height) {//Don't draw lines for text which is currently not visible.
 				return;
 			}
 			Pen pen=Pens.Red;
-			if((end.X-start.X)>4) {
-				ArrayList pl=new ArrayList();
-				for(int i=start.X;i<=(end.X-2);i=i+4) {
-					pl.Add(new Point(i,start.Y));
-					pl.Add(new Point(i+2,start.Y+1));
+			if(end.Y>start.Y) {//Mispelled word spans multiple lines
+				Point tempEnd=start;
+				tempEnd.X=this.Width;
+				while(tempEnd.Y<=end.Y) {
+					if((tempEnd.X-start.X)>4) {//Only draw wavy line if mispelled word is at least 4 pixels wide, otherwise draw straight line
+						ArrayList pl=new ArrayList();
+						for(int i=start.X;i<=(tempEnd.X-2);i=i+4) {
+							pl.Add(new Point(i,start.Y));
+							pl.Add(new Point(i+2,start.Y+1));
+						}
+						Point[] p=(Point[])pl.ToArray(typeof(Point));
+						BufferGraphics.DrawLines(pen,p);
+					}
+					else {
+						BufferGraphics.DrawLine(pen,start,end);
+					}
+					start.X=1;
+					start.Y=start.Y+this.FontHeight;
+					tempEnd.Y=start.Y;
+					if(tempEnd.Y==end.Y) {//We incremented to the next line and this is the last line of the mispelled word
+						tempEnd.X=end.X;
+					}
+					else {//not the last line of mispelled word, so draw wavy line to end of this line
+						tempEnd.X=this.Width;
+					}
 				}
-				Point[] p=(Point[])pl.ToArray(typeof(Point));
-				BufferGraphics.DrawLines(pen,p);
 			}
 			else {
-				BufferGraphics.DrawLine(pen,start,end);
+				if((end.X-start.X)>4) {
+					ArrayList pl=new ArrayList();
+					for(int i=start.X;i<=(end.X-2);i=i+4) {
+						pl.Add(new Point(i,start.Y));
+						pl.Add(new Point(i+2,start.Y+1));
+					}
+					Point[] p=(Point[])pl.ToArray(typeof(Point));
+					BufferGraphics.DrawLines(pen,p);
+				}
+				else {
+					BufferGraphics.DrawLine(pen,start,end);
+				}
 			}
 		}
 
@@ -464,12 +504,6 @@ namespace OpenDental
 			string strPaste=DateTime.Today.ToShortDateString();
 			Text=Text.Insert(caret,strPaste);
 			SelectionStart=caret+strPaste.Length;
-		}
-
-		private class ReplaceWord {
-			public string Word="";
-			public int StartIndex=0;
-			public int EndIndex=0;
 		}
 
 	}
