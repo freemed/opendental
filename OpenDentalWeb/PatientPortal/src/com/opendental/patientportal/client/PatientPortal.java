2d90c8f88df7937b3a6a5ec5609bd5553e7845ea
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-03-11 23:20:09 +0000

    Added the patient portal gwt project.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9141 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentalWeb/PatientPortal/src/com/opendental/patientportal/client/PatientPortal.java
@@ -0,0 +1,12 @@
+package com.opendental.patientportal.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class PatientPortal implements EntryPoint {
+
+	public void onModuleLoad() {
+		
+	}
+	
+	
+}
