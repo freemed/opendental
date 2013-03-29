e2ce9da092101e9774d56cb19669d045bcd62a57
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-03-29 19:21:04 +0000

    wiki, added wiki list headers edit.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9256 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormWikiListHeaders.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Windows.Forms;
+using OpenDentBusiness;
+using OpenDental.UI;
+using CodeBase;
+using System.Text.RegularExpressions;
+
+namespace OpenDental {
+	public partial class FormWikiListHeaders:Form {
+		public List<string> ColNames;
+		///<summary>Widths must always be specified.  Not optional.</summary>
+		public List<int> ColWidths;
+
+		public FormWikiListHeaders() {
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		private void FormWikiTableHeaders_Load(object sender,EventArgs e) {
+			gridMain.BeginUpdate();
+			gridMain.Columns.Clear();
+			ODGridColumn col;
+			for(int i=1;i<ColNames.Count+1;i++) {
+				col=new ODGridColumn("",75,true);
+				gridMain.Columns.Add(col);
+			}
+			gridMain.Rows.Clear();
+			ODGridRow row0=new ODGridRow();
+			ODGridRow row1=new ODGridRow();
+			for(int i=0;i<ColNames.Count;i++) {
+				row0.Cells.Add(ColNames[i]);
+				row1.Cells.Add(ColWidths[i].ToString());
+			}
+			gridMain.Rows.Add(row0);
+			gridMain.Rows.Add(row1);
+			gridMain.EndUpdate();
+		}
+
+		private void butOK_Click(object sender,EventArgs e) {
+			for(int i=0;i<ColNames.Count;i++) {
+				ColNames[i]=gridMain.Rows[0].Cells[i].Text;
+				try {
+					ColWidths[i]=PIn.Int(gridMain.Rows[1].Cells[i].Text);
+				}
+				catch {
+					MsgBox.Show(this,"Please enter only positive integer widths in the 2nd row");
+					return;
+				}
+				if(ColWidths[i]<1) {
+					MsgBox.Show(this,"Please enter only positive integer widths in the 2nd row");
+					return;
+				}
+			}
+			DialogResult=DialogResult.OK;
+		}
+
+		private void butCancel_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.Cancel;
+		}
+
+		
+
+		
+
+		
+
+		
+
+		
+	
+
+	
+
+		
+
+		
+
+		
+
+	
+
+	}
+}
---
+++ OpenDental/Forms/FormWikiListHeaders.designer.cs
@@ -0,0 +1,99 @@
+namespace OpenDental{
+	partial class FormWikiListHeaders {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.butOK = new OpenDental.UI.Button();
+			this.butCancel = new OpenDental.UI.Button();
+			this.gridMain = new OpenDental.UI.ODGrid();
+			this.SuspendLayout();
+			// 
+			// butOK
+			// 
+			this.butOK.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butOK.Autosize = true;
+			this.butOK.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butOK.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butOK.CornerRadius = 4F;
+			this.butOK.Location = new System.Drawing.Point(698, 172);
+			this.butOK.Name = "butOK";
+			this.butOK.Size = new System.Drawing.Size(75, 24);
+			this.butOK.TabIndex = 21;
+			this.butOK.Text = "&OK";
+			this.butOK.Click += new System.EventHandler(this.butOK_Click);
+			// 
+			// butCancel
+			// 
+			this.butCancel.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butCancel.Autosize = true;
+			this.butCancel.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butCancel.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butCancel.CornerRadius = 4F;
+			this.butCancel.Location = new System.Drawing.Point(779, 172);
+			this.butCancel.Name = "butCancel";
+			this.butCancel.Size = new System.Drawing.Size(75, 24);
+			this.butCancel.TabIndex = 20;
+			this.butCancel.Text = "Cancel";
+			this.butCancel.Click += new System.EventHandler(this.butCancel_Click);
+			// 
+			// gridMain
+			// 
+			this.gridMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.gridMain.HScrollVisible = true;
+			this.gridMain.Location = new System.Drawing.Point(12, 12);
+			this.gridMain.Name = "gridMain";
+			this.gridMain.ScrollValue = 0;
+			this.gridMain.SelectionMode = OpenDental.UI.GridSelectionMode.OneCell;
+			this.gridMain.Size = new System.Drawing.Size(842, 137);
+			this.gridMain.TabIndex = 26;
+			this.gridMain.Title = "";
+			this.gridMain.TranslationName = "";
+			// 
+			// FormWikiListHeaders
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(868, 223);
+			this.Controls.Add(this.gridMain);
+			this.Controls.Add(this.butOK);
+			this.Controls.Add(this.butCancel);
+			this.Name = "FormWikiListHeaders";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Edit Wiki List Headers";
+			this.Load += new System.EventHandler(this.FormWikiTableHeaders_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private UI.Button butOK;
+		private UI.Button butCancel;
+		private UI.ODGrid gridMain;
+
+
+	}
+}
