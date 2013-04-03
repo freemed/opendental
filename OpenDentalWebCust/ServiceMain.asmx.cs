74a57b6bc9502a434e431b6d16c7bb6e384de0bf
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-04-03 19:29:17 +0000

    Added a shell for the OpenDentalWebCust web service project.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9265 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentalWebCust/ServiceMain.asmx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace OpenDentalWebCust {
+	/// <summary>Summary description for Service1</summary>
+	[WebService(Namespace = "http://www.opendental.com/OpenDentalWebCust/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[ToolboxItem(false)]
+	public class ServiceMain:System.Web.Services.WebService {
+
+		[WebMethod]
+		public string HelloWorld() {
+			return "Hello World";
+		}
+	}
+}
