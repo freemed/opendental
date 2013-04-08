f681bb238dd4c4d422e85994a9233dc87936a65d
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-04-08 23:49:13 +0000

    Account module would take a while to load when family had 400+ members.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9279 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Misc/ConvertDatabases2.cs
+++ OpenDentBusiness/Misc/ConvertDatabases2.cs
@@ -11964,6 +11964,23 @@ a.PageNotExists:hover {
 				command="UPDATE preference SET ValueString = '13.1.14.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
+			To13_1_19();
+		}
+
+		private static void To13_1_19() {
+			if(FromVersion<new Version("13.1.19.0")) {
+				string command;
+				if(DataConnection.DBtype==DatabaseType.MySql) {
+					command="ALTER TABLE adjustment ADD INDEX (ProcNum)";
+					Db.NonQ(command);
+				}
+				else {//oracle
+					command=@"CREATE INDEX adjustment_ProcNum ON adjustment (ProcNum)";
+					Db.NonQ(command);
+				}
+				command="UPDATE preference SET ValueString = '13.1.19.0' WHERE PrefName = 'DataBaseVersion'";
+				Db.NonQ(command);
+			}
 			To13_2_0();
 		}
 
