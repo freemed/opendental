fe4fc4088a224da035dec6ad7ca1bf8cc0a0b06e
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-04-09 15:45:23 +0000

    Wiki lists, column width table was missing from database.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9280 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Misc/ConvertDatabases2.cs
+++ OpenDentBusiness/Misc/ConvertDatabases2.cs
@@ -11978,6 +11978,30 @@ a.PageNotExists:hover {
 					command=@"CREATE INDEX adjustment_ProcNum ON adjustment (ProcNum)";
 					Db.NonQ(command);
 				}
+				if(DataConnection.DBtype==DatabaseType.MySql) {
+					command="DROP TABLE IF EXISTS wikilistheaderwidth";
+					Db.NonQ(command);
+					command=@"CREATE TABLE wikilistheaderwidth (
+						WikiListHeaderWidthNum bigint NOT NULL auto_increment PRIMARY KEY,
+						ListName varchar(255) NOT NULL,
+						ColName varchar(255) NOT NULL,
+						ColWidth int NOT NULL
+						) DEFAULT CHARSET=utf8";
+					Db.NonQ(command);
+				}
+				else {//oracle
+					//WikiLists Not Supported in Oracle. but we're still adding this table here for consistency of the schema.  Also, we might turn on this feature for Oracle some day.
+					command="BEGIN EXECUTE IMMEDIATE 'DROP TABLE wikilistheaderwidth'; EXCEPTION WHEN OTHERS THEN NULL; END;";
+					Db.NonQ(command);
+					command=@"CREATE TABLE wikilistheaderwidth (
+						WikiListHeaderWidthNum number(20) NOT NULL,
+						ListName varchar2(255),
+						ColName varchar2(255),
+						ColWidth number(11) NOT NULL,
+						CONSTRAINT wikilistheaderwidth_WikiListHe PRIMARY KEY (WikiListHeaderWidthNum)
+						)";
+					Db.NonQ(command);
+				}
 				command="UPDATE preference SET ValueString = '13.1.19.0' WHERE PrefName = 'DataBaseVersion'";
 				Db.NonQ(command);
 			}
--- OpenDentBusiness/Misc/ConvertDatabases3.cs
+++ OpenDentBusiness/Misc/ConvertDatabases3.cs
@@ -30,30 +30,7 @@ namespace OpenDentBusiness {
 						Db.NonQ(command);
 					}
 				}
-				if(DataConnection.DBtype==DatabaseType.MySql) {
-					command="DROP TABLE IF EXISTS wikilistheaderwidth";
-					Db.NonQ(command);
-					command=@"CREATE TABLE wikilistheaderwidth (
-						WikiListHeaderWidthNum bigint NOT NULL auto_increment PRIMARY KEY,
-						ListName varchar(255) NOT NULL,
-						ColName varchar(255) NOT NULL,
-						ColWidth int NOT NULL
-						) DEFAULT CHARSET=utf8";
-					Db.NonQ(command);
-				}
-				else {//oracle
-					//WikiLists Not Supported in Oracle. but we're still adding this table here for consistency of the schema.  Also, we might turn on this feature for Oracle some day.
-					command="BEGIN EXECUTE IMMEDIATE 'DROP TABLE wikilistheaderwidth'; EXCEPTION WHEN OTHERS THEN NULL; END;";
-					Db.NonQ(command);
-					command=@"CREATE TABLE wikilistheaderwidth (
-						WikiListHeaderWidthNum number(20) NOT NULL,
-						ListName varchar2(255),
-						ColName varchar2(255),
-						ColWidth number(11) NOT NULL,
-						CONSTRAINT wikilistheaderwidth_WikiListHe PRIMARY KEY (WikiListHeaderWidthNum)
-						)";
-					Db.NonQ(command);
-				}
+		
 
 
 
