6b332674c2491b3f4444523fc715691238c7cab0
michael <michael@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-07-02 17:24:38 +0000

    EmailSentOrReceived enum updated to support patient portal WebMail messages.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9584 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/TableTypes/EmailMessage.cs
+++ OpenDentBusiness/TableTypes/EmailMessage.cs
@@ -43,14 +43,24 @@ namespace OpenDentBusiness {
 		}
 	}
 
-	///<summary>0=neither, 1=sent, 2=received.</summary>
+	///<summary>0=Neither, 1=Sent, 2=Receivedceived. 3=Read, 4=WebMailReceived, 5=WebMailRecdRead, 6=WebMailSent, 7=WebMailSentRead</summary>
 	public enum EmailSentOrReceived {
 		///<summary>0</summary>
 		Neither,
 		///<summary>1</summary>
 		Sent,
-		///<summary>2</summary>
-		Received
+		///<summary>2 For regular email and email encrypted using Direct.  Shows in Inbox.  Once it's attached to a patient it will also show in Chart module.</summary>
+		Received,
+		///<summary>3 For regular email and email encrypted using Direct.  Has been read.  Shows in Inbox.  Once it's attached to a patient it will also show in Chart module.</summary>
+		Read,
+		///<summary>4 WebMail received from patient portal.  Shows in OD Inbox and in pt Chart module.  Also shows in PP as a sent and unread WebMail msg.</summary>
+		WebMailReceived,
+		///<summary>5 WebMail received from patient portal that has been marked read.  Shows in the OD Inbox and in pt Chart module.  Also shows in PP as a sent and read WebMail.</summary>
+		WebMailRecdRead,
+		///<summary>6 Webmail sent from provider to patient.  Shows in Chart module and also shows in PP as a received and unread WebMail msg.</summary>
+		WebMailSent,
+		///<summary>7 Webmail sent from provider to patient and read by patient.  Shows in Chart module and also shows in PP as a received and read WebMail msg.</summary>
+		WebMailSentRead
 	}
 
 
