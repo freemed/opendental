a507a33f5c2d92290bee52490f8a6230ca2c926b
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-09-13 16:44:28 +0000

    Updated summary for SOP tabletype.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9851 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/TableTypes/SOP.cs
+++ OpenDentBusiness/TableTypes/SOP.cs
@@ -3,15 +3,15 @@ using System.Collections;
 using System.Drawing;
 
 namespace OpenDentBusiness {
-	///<summary>System of Payment Typology.  Used by EHR.  Examples: Medicaid, MedicaidPPO, NoFee, etc.  About 100 defined by govt.  Other tables generally use the SopCode as their foreign key.</summary>
+	///<summary>Source of Payment Typology.  Used by EHR.  Examples: Medicaid, MedicaidPPO, NoFee, etc.  About 100 defined by govt.  Other tables generally use the SopCode as their foreign key.</summary>
 	[Serializable]
 	public class Sop:TableBase {
 		///<summary>Primary key. .</summary>
 		[CrudColumn(IsPriKey=true)]
 		public long SopNum;
-		///<summary>Sop code. Not allowed to edit this column once saved in the database.</summary>
+		///<summary>Sop code. Not allowed to edit this column once saved in the database.  Examples: 121, 3115, etc.</summary>
 		public string SopCode;
-		///<summary>Description provided by Sop documentation.</summary>
+		///<summary>Description provided by Sop documentation.  Examples: Medicare FFS, TRICARE Reserve Select</summary>
 		public string Description;
 
 
