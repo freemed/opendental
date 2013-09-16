cb1ec9c4fb966d2048cf5c8d86987dbbca781046
tgriswold <tgriswold@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-09-16 19:17:51 +0000

    EHR, Added new FormMedicationUpdate form to be implemented by Ryan/Cameron.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9863 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormMedicationUpdate.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+
+namespace OpenDental {
+	public partial class FormMedicationUpdate:Form {
+		public FormMedicationUpdate() {
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		private void butOK_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.OK;
+		}
+
+		private void butCancel_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.Cancel;
+		}
+	}
+}
