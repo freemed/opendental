23a067a663527458219ad3922a006e8f39448923
allen <allen@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-09-23 18:49:57 +0000

    Updated IsNew summary.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@9888 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/CrudFramework/TableBase.cs
+++ OpenDentBusiness/CrudFramework/TableBase.cs
@@ -9,7 +9,7 @@ namespace OpenDentBusiness {
 	/// <summary>The base class for classes that correspond to a table in the database.  Make sure to mark each derived class [Serializable].</summary>
 	abstract public class TableBase {
 		private bool isNew;
-		///<summary>Always false by default.  Will only be true if explicitly set to true by user.</summary>
+		///<summary>Not a db column.  Always false by default.  Will only be true if explicitly set to true by programmer.  When CRUD grabs a table from db, it is naturally set to False.  Once set, this value is not used by the CRUD in any manner.  Just used by the programmer for making decisions about whether to Insert or Update.</summary>
 		public bool IsNew {
 			get { return isNew; }
 			set { isNew = value; }
