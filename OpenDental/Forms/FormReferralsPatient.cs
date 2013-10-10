46a6bcb263b889745e383fc0a65b913480ea6723
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-10-10 18:13:49 +0000

    Email column added to display grid of FormReferralsPatient (where referrals can be added and the referral list is displayed).  This change is in preparation for adding FormEhrReferralPick, which will be used when the user wants to generate a CCD message.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@10003 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/FormReferralsPatient.cs
+++ OpenDental/Forms/FormReferralsPatient.cs
@@ -59,11 +59,11 @@ namespace OpenDental{
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReferralsPatient));
 			this.gridMain = new OpenDental.UI.ODGrid();
+			this.checkShowAll = new System.Windows.Forms.CheckBox();
+			this.butAddTo = new OpenDental.UI.Button();
+			this.butSlip = new OpenDental.UI.Button();
 			this.butAddFrom = new OpenDental.UI.Button();
 			this.butClose = new OpenDental.UI.Button();
-			this.butSlip = new OpenDental.UI.Button();
-			this.butAddTo = new OpenDental.UI.Button();
-			this.checkShowAll = new System.Windows.Forms.CheckBox();
 			this.SuspendLayout();
 			// 
 			// gridMain
@@ -72,93 +72,93 @@ namespace OpenDental{
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.gridMain.HScrollVisible = false;
-			this.gridMain.Location = new System.Drawing.Point(12,42);
+			this.gridMain.Location = new System.Drawing.Point(12, 42);
 			this.gridMain.Name = "gridMain";
 			this.gridMain.ScrollValue = 0;
 			this.gridMain.SelectionMode = OpenDental.UI.GridSelectionMode.MultiExtended;
-			this.gridMain.Size = new System.Drawing.Size(719,261);
+			this.gridMain.Size = new System.Drawing.Size(839, 261);
 			this.gridMain.TabIndex = 74;
 			this.gridMain.Title = "Referrals Attached";
 			this.gridMain.TranslationName = "TableRefList";
 			this.gridMain.CellDoubleClick += new OpenDental.UI.ODGridClickEventHandler(this.gridMain_CellDoubleClick);
 			// 
-			// butAddFrom
+			// checkShowAll
 			// 
-			this.butAddFrom.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butAddFrom.Autosize = true;
-			this.butAddFrom.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butAddFrom.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butAddFrom.CornerRadius = 4F;
-			this.butAddFrom.Image = global::OpenDental.Properties.Resources.Add;
-			this.butAddFrom.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.butAddFrom.Location = new System.Drawing.Point(12,10);
-			this.butAddFrom.Name = "butAddFrom";
-			this.butAddFrom.Size = new System.Drawing.Size(109,24);
-			this.butAddFrom.TabIndex = 72;
-			this.butAddFrom.Text = "Referred From";
-			this.butAddFrom.Click += new System.EventHandler(this.butAddFrom_Click);
-			// 
-			// butClose
-			// 
-			this.butClose.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.butClose.Autosize = true;
-			this.butClose.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butClose.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butClose.CornerRadius = 4F;
-			this.butClose.Location = new System.Drawing.Point(656,317);
-			this.butClose.Name = "butClose";
-			this.butClose.Size = new System.Drawing.Size(75,24);
-			this.butClose.TabIndex = 0;
-			this.butClose.Text = "Close";
-			this.butClose.Click += new System.EventHandler(this.butClose_Click);
-			// 
-			// butSlip
-			// 
-			this.butSlip.AdjustImageLocation = new System.Drawing.Point(0,0);
-			this.butSlip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.butSlip.Autosize = true;
-			this.butSlip.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
-			this.butSlip.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
-			this.butSlip.CornerRadius = 4F;
-			this.butSlip.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.butSlip.Location = new System.Drawing.Point(12,317);
-			this.butSlip.Name = "butSlip";
-			this.butSlip.Size = new System.Drawing.Size(86,24);
-			this.butSlip.TabIndex = 90;
-			this.butSlip.Text = "Referral Slip";
-			this.butSlip.Click += new System.EventHandler(this.butSlip_Click);
+			this.checkShowAll.Location = new System.Drawing.Point(560, 18);
+			this.checkShowAll.Name = "checkShowAll";
+			this.checkShowAll.Size = new System.Drawing.Size(162, 20);
+			this.checkShowAll.TabIndex = 92;
+			this.checkShowAll.Text = "Show All";
+			this.checkShowAll.UseVisualStyleBackColor = true;
+			this.checkShowAll.Click += new System.EventHandler(this.checkShowAll_Click);
 			// 
 			// butAddTo
 			// 
-			this.butAddTo.AdjustImageLocation = new System.Drawing.Point(0,0);
+			this.butAddTo.AdjustImageLocation = new System.Drawing.Point(0, 0);
 			this.butAddTo.Autosize = true;
 			this.butAddTo.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
 			this.butAddTo.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
 			this.butAddTo.CornerRadius = 4F;
 			this.butAddTo.Image = global::OpenDental.Properties.Resources.Add;
 			this.butAddTo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.butAddTo.Location = new System.Drawing.Point(127,10);
+			this.butAddTo.Location = new System.Drawing.Point(127, 10);
 			this.butAddTo.Name = "butAddTo";
-			this.butAddTo.Size = new System.Drawing.Size(94,24);
+			this.butAddTo.Size = new System.Drawing.Size(94, 24);
 			this.butAddTo.TabIndex = 91;
 			this.butAddTo.Text = "Refer To";
 			this.butAddTo.Click += new System.EventHandler(this.butAddTo_Click);
 			// 
-			// checkShowAll
+			// butSlip
 			// 
-			this.checkShowAll.Location = new System.Drawing.Point(560,18);
-			this.checkShowAll.Name = "checkShowAll";
-			this.checkShowAll.Size = new System.Drawing.Size(162,20);
-			this.checkShowAll.TabIndex = 92;
-			this.checkShowAll.Text = "Show All";
-			this.checkShowAll.UseVisualStyleBackColor = true;
-			this.checkShowAll.Click += new System.EventHandler(this.checkShowAll_Click);
+			this.butSlip.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butSlip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.butSlip.Autosize = true;
+			this.butSlip.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butSlip.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butSlip.CornerRadius = 4F;
+			this.butSlip.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.butSlip.Location = new System.Drawing.Point(12, 317);
+			this.butSlip.Name = "butSlip";
+			this.butSlip.Size = new System.Drawing.Size(86, 24);
+			this.butSlip.TabIndex = 90;
+			this.butSlip.Text = "Referral Slip";
+			this.butSlip.Click += new System.EventHandler(this.butSlip_Click);
+			// 
+			// butAddFrom
+			// 
+			this.butAddFrom.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butAddFrom.Autosize = true;
+			this.butAddFrom.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butAddFrom.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butAddFrom.CornerRadius = 4F;
+			this.butAddFrom.Image = global::OpenDental.Properties.Resources.Add;
+			this.butAddFrom.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.butAddFrom.Location = new System.Drawing.Point(12, 10);
+			this.butAddFrom.Name = "butAddFrom";
+			this.butAddFrom.Size = new System.Drawing.Size(109, 24);
+			this.butAddFrom.TabIndex = 72;
+			this.butAddFrom.Text = "Referred From";
+			this.butAddFrom.Click += new System.EventHandler(this.butAddFrom_Click);
+			// 
+			// butClose
+			// 
+			this.butClose.AdjustImageLocation = new System.Drawing.Point(0, 0);
+			this.butClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.butClose.Autosize = true;
+			this.butClose.BtnShape = OpenDental.UI.enumType.BtnShape.Rectangle;
+			this.butClose.BtnStyle = OpenDental.UI.enumType.XPStyle.Silver;
+			this.butClose.CornerRadius = 4F;
+			this.butClose.Location = new System.Drawing.Point(776, 317);
+			this.butClose.Name = "butClose";
+			this.butClose.Size = new System.Drawing.Size(75, 24);
+			this.butClose.TabIndex = 0;
+			this.butClose.Text = "Close";
+			this.butClose.Click += new System.EventHandler(this.butClose_Click);
 			// 
 			// FormReferralsPatient
 			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5,13);
-			this.ClientSize = new System.Drawing.Size(743,352);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(863, 352);
 			this.Controls.Add(this.checkShowAll);
 			this.Controls.Add(this.butAddTo);
 			this.Controls.Add(this.butSlip);
@@ -168,6 +168,7 @@ namespace OpenDental{
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(871, 200);
 			this.Name = "FormReferralsPatient";
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -212,6 +213,8 @@ namespace OpenDental{
 			gridMain.Columns.Add(col);
 			col=new ODGridColumn(Lan.g("TableRefList","Note"),200);
 			gridMain.Columns.Add(col);
+			col=new ODGridColumn(Lan.g("TableRefList","Email"),120);
+			gridMain.Columns.Add(col);
 			gridMain.Rows.Clear();
 			ODGridRow row;
 			//Referral referral;
@@ -241,6 +244,8 @@ namespace OpenDental{
 					row.Cells.Add(str);
 				}
 				row.Cells.Add(RefAttachList[i].Note);
+				Referral referral=Referrals.GetReferral(RefAttachList[i].ReferralNum);
+				row.Cells.Add(referral.EMail);
 				gridMain.Rows.Add(row);
 			}
 			gridMain.EndUpdate();
