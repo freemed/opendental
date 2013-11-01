bc9748691d9ee28ae035c7f5ab874e58c483724d
grahamde <grahamde@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-11-01 13:46:44 +0000

    Put all but one unused references back in to BasicTemplate.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@10178 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDental/Forms/BasicTemplate.cs
+++ OpenDental/Forms/BasicTemplate.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
+using OpenDentBusiness;
 
 namespace OpenDental {
 	public partial class BasicTemplate:Form {
