40c4f994337c25ae0078dfd7466e0f99aa9992de
Ryan <Ryan@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-11-12 20:40:14 +0000

    Added FormCDSIntervention
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@10225 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormCDSIntervention.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+
+namespace OpenDental {
+	public partial class FormCDSIntervention:Form {
+
+		public FormCDSIntervention() {
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		private void FormCDSIntervention_Load(object sender,EventArgs e) {
+
+		}
+
+		private void butOK_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.OK;
+		}
+
+		private void butCancel_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.Cancel;
+		}
+
+	}
+}
