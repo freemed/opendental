7174dc59cda8664253facf340d593f0297bce39a
tgriswold <tgriswold@098d0599-7f2b-0410-95a5-ad30ac85393a>
2013-12-24 00:20:47 +0000

    Added form to select HCPCS codes for encounters.
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/head@10427 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDental/Forms/FormHcpcs.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+using System.IO;
+using OpenDental.UI;
+
+namespace OpenDental {
+	public partial class FormHcpcses:Form {
+		public bool IsSelectionMode;
+		public Hcpcs SelectedHcpcs;
+		private List<Hcpcs> listHcpcses;
+		private bool changed;
+
+		public FormHcpcses() {
+			InitializeComponent();
+			Lan.F(this);
+		}
+
+		private void FormHcpcses_Load(object sender,EventArgs e) {
+			if(IsSelectionMode) {
+				butClose.Text=Lan.g(this,"Cancel");
+			}
+			else {
+				butOK.Visible=false;
+			}
+			ActiveControl=textCode;
+		}
+		
+		private void butSearch_Click(object sender,EventArgs e) {
+			FillGrid();
+		}
+
+		private void FillGrid() {
+			gridMain.BeginUpdate();
+			gridMain.Columns.Clear();
+			ODGridColumn col;
+			col=new ODGridColumn("HCPCS Code",100);
+			gridMain.Columns.Add(col);
+			col=new ODGridColumn("Description",500);
+			gridMain.Columns.Add(col);
+			gridMain.Rows.Clear();
+			ODGridRow row;
+			listHcpcses=Hcpcses.GetBySearchText(textCode.Text);
+			for(int i=0;i<listHcpcses.Count;i++) {
+				row=new ODGridRow();
+				row.Cells.Add(listHcpcses[i].HcpcsCode);
+				row.Cells.Add(listHcpcses[i].DescriptionShort);
+				row.Tag=listHcpcses[i];
+				gridMain.Rows.Add(row);
+			}
+			gridMain.EndUpdate();
+		}
+
+		private void gridMain_CellDoubleClick(object sender,ODGridClickEventArgs e) {
+			if(IsSelectionMode) {
+				SelectedHcpcs=(Hcpcs)gridMain.Rows[e.Row].Tag;
+				DialogResult=DialogResult.OK;
+				return;
+			}
+		}
+
+		private void butOK_Click(object sender,EventArgs e) {
+			//not even visible unless IsSelectionMode
+			if(gridMain.GetSelectedIndex()==-1) {
+				MsgBox.Show(this,"Please select an item first.");
+				return;
+			}
+			SelectedHcpcs=(Hcpcs)gridMain.Rows[gridMain.GetSelectedIndex()].Tag;
+			DialogResult=DialogResult.OK;
+		}
+
+		private void butCancel_Click(object sender,EventArgs e) {
+			DialogResult=DialogResult.Cancel;
+		}
+
+	
+
+	}
+}
